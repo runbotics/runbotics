4fdf35c57104b0ba6da2bb567ee7f7415072b90b
preludium <szymczak.mikolaj08@gmail.com>
2022-08-26 13:00:12 +0200

    Update version bump script

--- bump-version.sh
+++ bump-version.sh
@@ -1,7 +1,7 @@
-echo "[INFO] - Bump runbotics-orchestrator version started"
-cd runbotics-orchestrator && npm version patch && ./gradlew incrementVersion --versionIncrementType=PATCH && cd ..
-echo "[INFO] - Bump runbotics-orchestrator version completed"
-
 echo "[INFO] - Bump rush apps versions started"
 cd runbotics && sh bump-rush.sh && cd ..
 echo "[INFO] - Bump rush apps versions completed"
+
+echo "[INFO] - Bump runbotics-orchestrator version started"
+cd runbotics-orchestrator && npm version patch && ./gradlew incrementVersion --versionIncrementType=PATCH && cd ..
+echo "[INFO] - Bump runbotics-orchestrator version completed"
