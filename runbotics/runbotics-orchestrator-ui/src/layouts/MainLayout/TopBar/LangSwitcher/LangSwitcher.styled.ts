bb5090a696b4214cddda591da3a7bbdc7dc2dc30
Mateusz Jabłoński <mat.w.jablonski@gmail.com>
2022-09-01 09:13:56 +0200

    [RPA-407] lang switcher (#2)
    Co-authored-by: Mateusz Jabłoński <mateusz.jablonski@all-for-one.com>

--- runbotics/runbotics-orchestrator-ui/src/layouts/MainLayout/TopBar/LangSwitcher/LangSwitcher.styled.ts
+++ runbotics/runbotics-orchestrator-ui/src/layouts/MainLayout/TopBar/LangSwitcher/LangSwitcher.styled.ts
@@ -1,9 +1,23 @@
-import { FormControl } from '@mui/material';
+import { FormControl, MenuItem } from '@mui/material';
 import styled from 'styled-components';
 
 export const LangFormControl = styled(FormControl)`
     &[class*="MuiFormControl"] {
-        min-width: 150px;
+        min-width: 120px;
         margin: 0 ${({ theme }) => theme.spacing(2)};
+        height: 18px;
+
+        [class*="MuiInputLabel"] {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            width: 100%;
+        }
+    }
+`;
+
+export const StyledMenuItem = styled(MenuItem)`
+    &[class*="MuiMenuItem"] {
+        font-size: ${({ theme }) => theme.typography.pxToRem(14)};
     }
 `;
