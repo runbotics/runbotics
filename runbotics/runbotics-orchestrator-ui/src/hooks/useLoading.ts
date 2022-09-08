2fb913d30b7a1a38c1ed4488211dde031b6e423d
Karol Żuraniewski <58148761+nick-here@users.noreply.github.com>
2022-09-08 11:40:24 +0200

    [RPA-334] Processes view rererendering  (#9)
    * fix: processes view no longer rerenders on each search phrase change
    
    * fix: display mode cannot be deselected
    
    * feat: process list loading debounce
    
    * ref: If's merged into one
    
    * Revert "ref: If's merged into one"
    
    This reverts commit b18aa435c3391cf35f568ee8b53e57e3035e1750.
    
    * ref: If's merged into one
    
    * ref: console.log removed

---
+++ runbotics/runbotics-orchestrator-ui/src/hooks/useLoading.ts
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { LOADING_DEBOUNCE } from 'src/views/process/ProcessBrowseView/ProcessList/ProcessList.utils';
+
+const useLoading = (isLoading: boolean, delay = LOADING_DEBOUNCE) => {
+    const [isDelayPassed, setIsDelayPassed] = useState(!isLoading);
+    const [showLoading, setShowLoading] = useState(isLoading);
+
+    useEffect(() => {
+        if (!isLoading && isDelayPassed) {
+            setShowLoading(false);
+            return;
+        }
+
+        setIsDelayPassed(false);
+        setShowLoading(true);
+
+        const id = setTimeout(() => {
+            setIsDelayPassed(true);
+            clearTimeout(id);
+        }, delay);
+    }, [isLoading, delay]);
+
+    useEffect(() => {
+        setShowLoading(isLoading);
+    }, [isDelayPassed]);
+
+    return showLoading;
+};
+
+export default useLoading;
