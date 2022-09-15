907aa10acdf8bc6866fa984ca5dac69d73b8221b
nick-here <zuraniewski.karol@gmail.com>
2022-09-15 08:38:25 +0200

    ref: ErrorType with string values

--- runbotics/runbotics-orchestrator-ui/src/views/process/AddProcess.tsx
+++ runbotics/runbotics-orchestrator-ui/src/views/process/AddProcess.tsx
@@ -37,7 +37,7 @@ const formData: IProcess = {
 };
 
 enum ErrorType {
-    NAME_NOT_AVAILABLE,
+    NAME_NOT_AVAILABLE = 'NAME_NOT_AVAILABLE',
 }
 
 const errorMessages: Record<ErrorType, string> = {
