7d84215340a64d0d406230c4a1a25e03eb63ec9c
nick-here <zuraniewski.karol@gmail.com>
2022-09-15 14:37:18 +0200

    fix: name field resets on dialog close

--- runbotics/runbotics-orchestrator-ui/src/views/process/AddProcess.tsx
+++ runbotics/runbotics-orchestrator-ui/src/views/process/AddProcess.tsx
@@ -52,8 +52,13 @@ const AddProcessDialog: FC<AddProcessDialogProps> = ({ open, onClose, onAdd }) =
         onAdd(result.payload);
     };
 
+    const handleClose = () => {
+        setName(null);
+        onClose();
+    }
+
     return (
-        <Dialog open={open} onClose={onClose} fullWidth maxWidth="lg">
+        <Dialog open={open} onClose={handleClose} fullWidth maxWidth="lg">
             <DialogTitle>{translate('Process.Add.Title')}</DialogTitle>
             <DialogContent>
                 <Box sx={{ pt: 1, pb: 3 }}>
@@ -71,7 +76,7 @@ const AddProcessDialog: FC<AddProcessDialogProps> = ({ open, onClose, onAdd }) =
             <DialogActions>
                 <Button
                     color="primary"
-                    onClick={onClose}
+                    onClick={handleClose}
                 >
                     {translate('Common.Cancel')}
                 </Button>
