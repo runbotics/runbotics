66a32289bd3592822cb79e3728f192fd4258e129
nick-here <zuraniewski.karol@gmail.com>
2022-09-19 16:04:34 +0200

    feat: palette translations

--- runbotics/runbotics-orchestrator-ui/src/views/process/ProcessBuildView/Modeler/modeler-palette/index.ts
+++ runbotics/runbotics-orchestrator-ui/src/views/process/ProcessBuildView/Modeler/modeler-palette/index.ts
@@ -1,5 +1,7 @@
 import ModelerPalette from './ModelerPalette';
 
+export * from './ModelerPalette.types.d';
+
 export default {
     __init__: ['customPaletteProvider'],
     customPaletteProvider: ['type', ModelerPalette],
