04af7f5602164dd6d585dca82aa7de5f43bf67da
nick-here <zuraniewski.karol@gmail.com>
2022-09-20 15:36:21 +0200

    fix: drawer smoother resizing

---
+++ runbotics/runbotics-orchestrator-ui/src/components/ResizableDrawer/ResizableDrawer.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Drawer } from '@mui/material';
+import { DrawerProps } from '@mui/material/Drawer/Drawer';
+
+const INITIAL_WIDTH = 300;
+
+const Dragger = styled('div')(({ theme }) => ({
+    width: '3px',
+    cursor: 'ew-resize',
+    pointerEvents: 'all',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+
+    ':hover, [data-dragmode] &': {
+        backgroundColor: `${theme.palette.primary.light}`,
+    },
+}));
+
+const ResizableDrawer = ({ children, ...other }: DrawerProps) => {
+    const [width, setWidth] = useState(INITIAL_WIDTH);
+
+    const handleMousemove = (event: MouseEvent) => {
+        setWidth(window.innerWidth - event.clientX);
+    };
+
+    const handleDragEnd = () => {
+        document.removeEventListener('mousemove', handleMousemove);
+        delete document.body.dataset.dragmode;
+    };
+
+    const handleDragStart = () => {
+        document.addEventListener('mousemove', handleMousemove);
+        document.addEventListener('mouseup', handleDragEnd, { once: true });
+        document.body.dataset.dragmode = '';
+    };
+
+    return (
+        <Drawer
+            {...other}
+            PaperProps={{
+                style: {
+                    maxWidth: '1000px',
+                    minWidth: '10px',
+                    width,
+                    transition: 'none',
+                },
+            }}
+        >
+            <Dragger onMouseDown={handleDragStart} />
+            {children}
+        </Drawer>
+    );
+};
+
+export default ResizableDrawer;
---
+++ runbotics/runbotics-orchestrator-ui/src/components/ResizableDrawer/index.ts
@@ -0,0 +1,4 @@
+import ResizableDrawer from './ResizableDrawer';
+import './ResizableDrawer.styles.scss';
+
+export default ResizableDrawer;
