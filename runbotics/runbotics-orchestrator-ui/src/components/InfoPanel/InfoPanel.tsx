263185be53f917c4a7a89a7eaa855edfc8513cdf
Karol Żuraniewski <58148761+nick-here@users.noreply.github.com>
2022-09-26 13:27:45 +0200

    [RPA-448] Process panel adjustments (#25)
    * fix: drawer smoother resizing
    
    * fix: app hover effects disabled while dragging panel
    
    * fix: right panel adjustments
    
    * fix: panel hide animation adjust
    
    * fix: redux dispatch events
    
    * version bump
    
    * ref: drawer and sidebar adjustments
    
    * ref: react event listeners for drawer resizing
    
    * ref: renamed dirname and filename
    
    * version bump

--- runbotics/runbotics-orchestrator-ui/src/components/InfoPanel/InfoPanel.tsx
+++ runbotics/runbotics-orchestrator-ui/src/components/InfoPanel/InfoPanel.tsx
@@ -1,26 +1,11 @@
-import React, { VFC } from 'react';
-import styled from 'styled-components';
-import {
-    Box, IconButton, SxProps, Theme,
-} from '@mui/material';
-import ResizableDrawer from 'src/components/ResizableDrawer';
+import React from 'react';
+import { Box, IconButton, SxProps, Theme } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import ProcessInstanceDetails from './ProcessInstanceDetails/ProcessInstanceDetails';
 import ProcessInstanceEventsDetails from './ProcessInstanceEventsDetails';
 import If from '../utils/If';
 
 export const drawerWidth = 450;
-
-const StyledResizableDrawer = styled(ResizableDrawer)({
-    minHeight: '100%',
-    '& .drawerPaper': {
-        top: 'auto',
-        width: drawerWidth,
-        position: 'relative',
-        borderLeft: '1px solid rgba(0, 0, 0, 0.12)',
-        height: '100%',
-    },
-});
 
 interface InfoPanelProps {
     processInstanceId?: string;
@@ -29,10 +14,8 @@ interface InfoPanelProps {
     sx?: SxProps<Theme>;
 }
 
-const InfoPanel: VFC<InfoPanelProps> = ({
-    processInstanceId, onClose, showCloseButton, sx,
-}) => (
-    <StyledResizableDrawer open variant="permanent" anchor="right" sx={sx} classes={{ paper: 'drawerPaper' }}>
+const InfoPanel = ({ processInstanceId, onClose, showCloseButton = false }: InfoPanelProps) => (
+    <>
         <If condition={showCloseButton}>
             <Box display="flex" justifyContent="flex-end" paddingRight="0.625rem">
                 <IconButton aria-label="close" size="small" onClick={onClose}>
@@ -40,14 +23,18 @@ const InfoPanel: VFC<InfoPanelProps> = ({
                 </IconButton>
             </Box>
         </If>
-        <Box sx={{
-            ml: '3px', height: '100%', overflowY: 'auto', overflowX: 'hidden',
-        }}
+        <Box
+            sx={{
+                ml: '3px',
+                height: '100%',
+                overflowY: 'auto',
+                overflowX: 'hidden',
+            }}
         >
             <ProcessInstanceDetails processInstanceId={processInstanceId} />
             <ProcessInstanceEventsDetails processInstanceId={processInstanceId} />
         </Box>
-    </StyledResizableDrawer>
+    </>
 );
 
 export default InfoPanel;
---
+++ runbotics/runbotics-orchestrator-ui/src/views/process/ProcessBuildView/SidebarNavigationPanel/Siderbar.styled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const SidebarNavigationWrapper = styled('div')({
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+});
+
+export const SidebarNavigationButton = styled('button')<{ selected: boolean }>(({ theme, selected }) => ({
+    border: `${theme.typography.pxToRem(1)} solid ${theme.palette.grey[300]}`,
+    borderRadius: `${theme.typography.pxToRem(15)} 0 0 ${theme.typography.pxToRem(15)}`,
+    borderRight: `none`,
+    width: `calc(1.5em)`,
+    padding: `${theme.typography.pxToRem(26)} ${theme.typography.pxToRem(2)}`,
+    background: selected ? theme.palette.primary.main : theme.palette.grey[100],
+    color: selected ? theme.palette.common.white : theme.palette.primary.main,
+    fontFamily: theme.typography.fontFamily,
+    fontSize: theme.typography.body1.fontSize,
+    transition: theme.transitions.create('right', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    cursor: 'pointer',
+    writingMode: 'vertical-lr',
+}));
