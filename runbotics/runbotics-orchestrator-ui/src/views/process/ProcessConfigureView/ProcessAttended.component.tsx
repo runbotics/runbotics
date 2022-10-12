e651a3678a192de158f04c74d73a583aa48173f4
GBCC\kaczkowskil <lukasz.kaczkowski@all-for-one.com>
2022-10-12 15:31:38 +0200

    fix: update process store after configure switches change

--- runbotics/runbotics-orchestrator-ui/src/views/process/ProcessConfigureView/ProcessAttended.component.tsx
+++ runbotics/runbotics-orchestrator-ui/src/views/process/ProcessConfigureView/ProcessAttended.component.tsx
@@ -1,11 +1,13 @@
-import React, { VFC, ChangeEvent, useState, useEffect } from 'react';
+import React, {
+    VFC, ChangeEvent, useState, useEffect,
+} from 'react';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import { FormControlLabel, Switch } from '@mui/material';
 import useTranslations from 'src/hooks/useTranslations';
-import { Wrapper } from './BotComponent.styles';
 import If from 'src/components/utils/If';
 import useFeatureKey from 'src/hooks/useFeatureKey';
 import { FeatureKey } from 'runbotics-common';
+import { Wrapper } from './BotComponent.styles';
 
 interface ProcessAttendedProps {
     isProcessAttended: boolean;
@@ -14,22 +16,27 @@ interface ProcessAttendedProps {
 
 const ProcessAttendedComponent: VFC<ProcessAttendedProps> = ({ isProcessAttended, onAttendedChange }) => {
     const hasReadIsProcessAttended = useFeatureKey([FeatureKey.PROCESS_IS_ATTENDED_READ]);
+    const hasEditIsProcessAttended = useFeatureKey([FeatureKey.PROCESS_IS_ATTENDED_EDIT]);
     const { translate } = useTranslations();
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         onAttendedChange(e.target.checked);
     };
-    
+
+    const attendedSwitch = (
+        <Switch onChange={handleChange} checked={isProcessAttended} disabled={!hasEditIsProcessAttended} />
+    );
+
     return (
         <If condition={hasReadIsProcessAttended}>
             <Wrapper>
                 <PersonOutlinedIcon />
                 <FormControlLabel
-                    control={<Switch onChange={handleChange} checked={isProcessAttended} />}
+                    control={attendedSwitch}
                     label={translate('Process.Edit.Form.Fields.IsAttended.Label')}
                     labelPlacement="start"
                     sx={{ height: '1.75rem' }}
-                    />
+                />
             </Wrapper>
         </If>
     );
