4cb24571509f38c8355e27c9dd0028a507da4f2f
Franek Marciniak <ProstyFranek@pm.me>
2022-10-25 11:49:48 +0200

    Processes table fix

--- runbotics/runbotics-orchestrator-ui/src/components/Tile/Tile.avatar.tsx
+++ runbotics/runbotics-orchestrator-ui/src/components/Tile/Tile.avatar.tsx
@@ -1,6 +1,6 @@
 import { Avatar, Link } from '@mui/material';
 import styled from 'styled-components';
-import React, { VFC } from 'react';
+import { VFC } from 'react';
 import useTranslations from 'src/hooks/useTranslations';
 import RouterLink from 'next/link';
 import { getAvatarText } from './Tile.utils';
@@ -21,14 +21,24 @@ interface TileAvatarProps {
 const TileAvatar: VFC<TileAvatarProps> = ({ href, title }) => {
     const { translate } = useTranslations();
 
+    const AvatarWrapper = ({ children }) => {
+        if (href) {
+            return (
+                <RouterLink href={href} passHref>
+                    <Link>{children}</Link>
+                </RouterLink>
+            );
+        }
+
+        return children;
+    };
+
     return (
-        <RouterLink href={href} passHref>
-            <Link variant="h5">
-                <StyledAvatar aria-label={translate('Component.Tile.Avatar.AriaLabel')}>
-                    {getAvatarText(title)}
-                </StyledAvatar>
-            </Link>
-        </RouterLink>
+        <AvatarWrapper>
+            <StyledAvatar aria-label={translate('Component.Tile.Avatar.AriaLabel')}>
+                {getAvatarText(title)}
+            </StyledAvatar>
+        </AvatarWrapper>
     );
 };
 
