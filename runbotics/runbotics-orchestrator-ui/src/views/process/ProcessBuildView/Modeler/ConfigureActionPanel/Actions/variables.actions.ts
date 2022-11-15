d865d73136a5ba36e0a377fe93be522029e7b1c9
Stanisław Jarocki <stanislaw.jarocki@all-for-one.com>
2022-11-15 12:16:03 +0100

    Self CR 1.

--- runbotics/runbotics-orchestrator-ui/src/views/process/ProcessBuildView/Modeler/ConfigureActionPanel/Actions/variables.actions.ts
+++ runbotics/runbotics-orchestrator-ui/src/views/process/ProcessBuildView/Modeler/ConfigureActionPanel/Actions/variables.actions.ts
@@ -19,8 +19,7 @@ const getVariablesActions: () => Record<string, IBpmnAction> = () => ({
                             variable: {
                                 title: translate('Process.Details.Modeler.Actions.Variable.Assign.Variable'),
                                 type: 'string',
-                                pattern: '^[a-zA-Z0-9]*$',
-                                message: 'cannot contain spaces or special characters',
+                                pattern: '^[a-zA-Z0-9_]*$',
                             },
 
                             value: {
