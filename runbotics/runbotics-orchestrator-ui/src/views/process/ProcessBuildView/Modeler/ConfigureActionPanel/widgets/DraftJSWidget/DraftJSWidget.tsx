829f9092aaae05143dff69f69d1a89ea6fd5a1a7
Stanisław Jarocki <stanislaw.jarocki@all-for-one.com>
2022-11-27 18:02:26 +0100

    Make text editor (in Send Mail action) active.

---
+++ runbotics/runbotics-orchestrator-ui/src/views/process/ProcessBuildView/Modeler/ConfigureActionPanel/widgets/DraftJSWidget/DraftJSWidget.tsx
@@ -0,0 +1,132 @@
+import React, { FC, useMemo, useState } from 'react';
+
+import { useTheme } from '@mui/material';
+import { WidgetProps } from '@rjsf/core';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { EditorState, ContentState, convertFromHTML } from 'draft-js';
+import { stateToHTML } from 'draft-js-export-html';
+import i18next from 'i18next';
+import { Editor } from 'react-draft-wysiwyg';
+
+import useTranslations from 'src/hooks/useTranslations';
+
+
+const parseContentToEditorState = (text: string) => {
+    if (text) {
+        const isHTML = /<\/?[a-z][\s\S]*>/i.test(text);
+        let content;
+
+        if (isHTML) {
+            const blocksFromHTML = convertFromHTML(text);
+            content = ContentState.createFromBlockArray(blocksFromHTML.contentBlocks, blocksFromHTML.entityMap);
+        } else {
+            content = ContentState.createFromText(text);
+        }
+        const state: EditorState = EditorState.createWithContent(content);
+        return state;
+    }
+    return EditorState.createEmpty();
+};
+
+const DraftJSEditor: FC<WidgetProps> = (props) => {
+    const { translate } = useTranslations();
+    const [state, setState] = useState(parseContentToEditorState(props.value));
+    const [isActive, setIsActive] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
+    const theme = useTheme();
+    
+    
+    const wrapperStyleObject = useMemo(() => ({
+        margin: 1,
+        color: theme.palette.text.primary,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: theme.palette.action.disabled,
+    }), [theme.palette.action.disabled, theme.palette.text.primary]);
+    
+    const toolbarStyleObject = useMemo(() => ({
+        padding: '0.6rem',
+        borderRadius: 5,
+        borderBottomWidth: 1,
+        borderBottomStyle: 'solid',
+        borderBottomColor: theme.palette.action.disabled,
+    }), [theme.palette.action.disabled]);
+    
+    const hoverWrapperStyleObject = useMemo(() => ({
+        margin: 1,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: theme.palette.action.active,
+    }), [theme.palette.action.active]);
+    
+    const activeWrapperStyleObject = useMemo(() => ({
+        borderWidth: 2,
+        borderRadius: 5,
+        borderStyle: 'solid',
+        borderColor: theme.palette.primary.main,
+    }), [theme.palette.primary.main]);
+    
+    const editorStyleObject = {
+        paddingTop: 0,
+        paddingRight: '1rem',
+        paddingBottom: '1rem',
+        paddingLeft: '1rem',
+        minHeight: 200,
+        borderRadius: 5,
+    };
+
+    const currentWrapperStyle = useMemo(() => {
+        if(isActive) return activeWrapperStyleObject;
+        else if(isHovered) return hoverWrapperStyleObject;
+        return wrapperStyleObject;
+    }, [isActive, isHovered, activeWrapperStyleObject, hoverWrapperStyleObject, wrapperStyleObject]);
+
+    const handleHoverEditorArea = () => {
+        setIsHovered(true);
+    };
+
+    const handleLeaveEditorArea = () => {
+        setIsHovered(false);
+    };
+
+    const handleEnterEditorArea = () => {
+        setIsActive(true);
+    };
+
+
+    const handleBlurEditorArea = () => {
+        setIsActive(false);
+    };
+
+    const onEditorStateChange = (editorState) => {
+        setState(editorState);
+        const json = stateToHTML(editorState.getCurrentContent());
+        if(json !== props.value) props.onChange(json);
+    };
+
+    return (
+        <div 
+            onMouseEnter={handleHoverEditorArea}
+            onFocus={handleEnterEditorArea}
+            onMouseLeave={handleLeaveEditorArea}
+            onBlur={handleBlurEditorArea}
+        >
+            <Editor
+                editorState={state}
+                onEditorStateChange={onEditorStateChange}
+                editorClassName="editorClassName"
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorStyle={editorStyleObject}
+                toolbarStyle={toolbarStyleObject}
+                wrapperStyle={currentWrapperStyle}
+                localization={{ locale: i18next.language }}
+                placeholder={translate('Process.Details.Modeler.Actions.Mail.Send.Content.Placeholder')}
+            />
+        </div>
+    );
+};
+
+export default DraftJSEditor;
