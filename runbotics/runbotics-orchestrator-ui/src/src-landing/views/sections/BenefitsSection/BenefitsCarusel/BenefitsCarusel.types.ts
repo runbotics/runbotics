aba1187ab32cc0af8bf96d07235e795867cff89a
Franek Marciniak <ProstyFranek@pm.me>
2022-12-13 07:50:20 +0100

    translations keys type

--- runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/BenefitsSection/BenefitsCarusel/BenefitsCarusel.types.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/BenefitsSection/BenefitsCarusel/BenefitsCarusel.types.ts
@@ -1,8 +1,15 @@
-import { TranslationsDescriptors } from '#src-app/translations/translations';
+
+type SlideTitleKey = 'Landing.Benefits.Slides.Repetitive.Title'
+    | 'Landing.Benefits.Slides.Efficiency.Title'
+    | 'Landing.Benefits.Slides.Accuracy.Title';
+
+type SlideDescriptionKey = 'Landing.Benefits.Slides.Repetitive.Description'
+    | 'Landing.Benefits.Slides.Efficiency.Description'
+    | 'Landing.Benefits.Slides.Accuracy.Description';
 
 export interface Slide {
-    titleKey: keyof TranslationsDescriptors;
-    descriptionKey: keyof TranslationsDescriptors;
+    titleKey: SlideTitleKey;
+    descriptionKey: SlideDescriptionKey;
     imageSrc: string;
     imageAlt: string;
 };
