6b7e7ba404bf22e326b8c48924fe9086835b982a
Franek Marciniak <ProstyFranek@pm.me>
2022-12-14 14:49:24 +0100

    fixed indent

--- runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/BenefitsSection/BenefitsCarusel/MobileCaruselNav/MobileCaruselNav.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/BenefitsSection/BenefitsCarusel/MobileCaruselNav/MobileCaruselNav.tsx
@@ -6,11 +6,17 @@ import Typography from '#src-landing/components/Typography';
 import styles from './MobileCaruselNav.module.scss';
 import { MobileCaruselNavProps } from './MobileCaruselNav.types';
 
-const MobileCaruselNav: FC<MobileCaruselNavProps> = ({ decrementSlide, incrementSlide, length, currentSlide }) => (
+const MobileCaruselNav: FC<MobileCaruselNavProps> = ({
+    decrementSlide,
+    incrementSlide,
+    length,
+    currentSlide,
+}) => (
     <div className={styles.root}>
         <CaruselButton direction="backward" onClick={decrementSlide} />
         <Typography variant="body1" className={styles.counter}>
-            <span className={styles.counterCurrent}>{currentSlide + 1}</span> / {length}
+            <span className={styles.counterCurrent}>{currentSlide + 1}</span> /{' '}
+            {length}
         </Typography>
         <CaruselButton direction="forward" onClick={incrementSlide} />
     </div>
