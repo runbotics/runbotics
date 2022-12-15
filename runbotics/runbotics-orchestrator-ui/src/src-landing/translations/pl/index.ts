49b31633e798248ce2c13b07c1dfb2da2d364e63
Stanisław Jarocki <stanislaw.jarocki@all-for-one.com>
2022-12-15 10:09:13 +0100

    Self CR 2.

--- runbotics/runbotics-orchestrator-ui/src/src-landing/translations/pl/index.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/translations/pl/index.ts
@@ -1,11 +1,13 @@
 import benefitsTranslations from './benefits.json';
 import heroTranslations from './hero.json';
 import integrationTranslations from './integration.json';
+import prosTranslations from './pros.json';
 
 const landingPageTranslations = {
     ...heroTranslations,
     ...integrationTranslations,
-    ...benefitsTranslations
+    ...benefitsTranslations,
+    ...prosTranslations,
 };
 
 export default landingPageTranslations;
