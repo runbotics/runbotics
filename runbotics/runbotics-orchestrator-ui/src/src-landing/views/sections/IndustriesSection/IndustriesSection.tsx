da1d742f9e01bd9766590e27fccea9dd7d5de5ba
GBCC\samelakk <klaudyna.samelak@snpgroup.com>
2023-01-03 11:04:23 +0100

    Add scrolling to new sections

--- runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/IndustriesSection/IndustriesSection.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/IndustriesSection/IndustriesSection.tsx
@@ -1,6 +1,7 @@
 import useTranslations from '#src-app/hooks/useTranslations';
 import Typography from '#src-landing/components/Typography';
 
+import { INDUSTRY_SECTORS_SECTION_ID } from '#src-landing/utils/utils';
 import { SLIDES } from '#src-landing/views/sections/IndustriesSection/IndustriesSection.utils';
 
 import IndustriesCarousel from './IndustriesCarousel/IndustiresCarousel';
@@ -9,7 +10,7 @@ import styles from './IndustriesSection.module.scss';
 const IndustriesSection = () => {
     const { translate } = useTranslations();
     return (
-        <section className={styles.root}>
+        <section className={styles.root} id={INDUSTRY_SECTORS_SECTION_ID}>
             <div className={styles.title}>
                 <Typography variant="h2">
                     <div>{translate('Landing.Industries.Title.Part.1')}</div>
