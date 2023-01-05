4f65aa345fc0bcbd4697381117abf3130cce329f
Franek Marciniak <ProstyFranek@pm.me>
2023-01-05 16:19:22 +0100

    removed proxy from prod

--- runbotics/runbotics-orchestrator-ui/next.config.js
+++ runbotics/runbotics-orchestrator-ui/next.config.js
@@ -10,16 +10,7 @@ module.exports = {
                 destination: 'http://localhost:4000/scheduler/:path*',
             },
         ]
-        : [
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:8080/api/:path*', // The :path parameter is used here so will not be automatically passed in the query
-            },
-            {
-                source: '/scheduler/:path*',
-                destination: 'http://localhost:4000/scheduler/:path*',
-            },
-        ],
+        : [],
     webpack: (config) => {
         config.resolve.fallback = { '@material-ui/core': false, '@material-ui/icons': false };
 
