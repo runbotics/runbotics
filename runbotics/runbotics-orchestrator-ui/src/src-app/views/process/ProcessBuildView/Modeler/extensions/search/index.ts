dd52d02d1f0637619618b307b4b710f6b4db8065
Franek Marciniak <ProstyFranek@pm.me>
2023-01-20 12:19:37 +0100

    fix: :ambulance: added providers

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/search/index.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/search/index.ts
@@ -1,7 +1,10 @@
+import SearchPadModule from 'diagram-js/lib/features/search-pad';
+
 import BpmnSearchProvider from './BpmnSearch';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+    __depends__: [SearchPadModule],
     __init__: ['bpmnSearch'],
     bpmnSearch: ['type', BpmnSearchProvider]
 };
