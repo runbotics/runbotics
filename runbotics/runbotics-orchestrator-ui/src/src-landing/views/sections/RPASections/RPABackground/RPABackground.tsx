dd1ab49831747123b6f1868ec0d1781f5164df02
Stanislaw Jarocki <stanislaw.jarocki@all-for-one.com>
2023-01-24 09:38:53 +0100

    fix(runbotics-orchestrator-ui): :lipstick: Fix the scaling of the RPA section.

--- runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/RPASections/RPABackground/RPABackground.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/RPASections/RPABackground/RPABackground.tsx
@@ -1,18 +1,25 @@
 import { FC } from 'react';
 
-import { RPA_SECTION_ID } from '#src-landing/utils/utils';
-
 import styles from './RPABackground.module.scss';
 
-const RPABackground: FC = ({children}) => (
-    <section className={styles.root} id={RPA_SECTION_ID}>
-        <div className={styles.backgroundBanner}>
-            <div className={styles.backgroundDimmer} />
-            <div className={styles.threeArrows} />
-            {children}
-            <div className={styles.oneArrow}/>
-        </div>
-    </section>
-);
+import useTranslations from '#src-app/hooks/useTranslations';
+import Typography from '#src-landing/components/Typography';
+import { RPA_SECTION_ID } from '#src-landing/utils/utils';
+
+const RPABackground: FC = ({ children }) => {
+    const { translate } = useTranslations();
+
+    return (
+        <section className={styles.root} id={RPA_SECTION_ID}>
+            <div className={styles.centered}>
+                <div className={styles.rightColumn}>
+                    <div className={styles.contentWrapper}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
 
 export default RPABackground;
--- runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/RPASections/RPAContent/RPAContent.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/RPASections/RPAContent/RPAContent.tsx
@@ -1,36 +1,37 @@
 import { FC } from 'react';
 
+import styles from './RPAContent.module.scss';
+
 import useTranslations from '#src-app/hooks/useTranslations';
 import Typography from '#src-landing/components/Typography';
-
-import styles from './RPAContent.module.scss';
 
 const RPAContent: FC = () => {
     const { translate } = useTranslations();
 
     return (
         <div className={styles.root}>
-            <div className={styles.contentWrapper}>
-                <Typography variant="h2" className={styles.title}>
-                    {translate('Landing.RPA.Title')}
+            <div className={styles.title}>
+                <Typography>
+                    {translate('Landing.RPA.Title.Part1')}
                 </Typography>
-                <div className={styles.content}>
-                    <div className={styles.paragraph1}>
-                        <Typography>
-                            {translate('Landing.RPA.Content.Paragraph1.Part1')}
-                        </Typography>
-                        <Typography>
-                            {translate('Landing.RPA.Content.Paragraph1.Part2')}
-                        </Typography>
-                        <Typography>
-                            {translate('Landing.RPA.Content.Paragraph1.Part3')}
-                        </Typography>
-                    </div>
-                    <Typography>
-                        {translate('Landing.RPA.Content.Paragraph2')}
-                    </Typography>
-                </div>
+                <Typography>
+                    {translate('Landing.RPA.Title.Part2')}
+                </Typography>
             </div>
+            <div className={styles.paragraphs}>
+                <Typography>
+                    {translate('Landing.RPA.Content.Paragraph1.Part1')}
+                </Typography>
+                <Typography>
+                    {translate('Landing.RPA.Content.Paragraph1.Part2')}
+                </Typography>
+                <Typography>
+                    {translate('Landing.RPA.Content.Paragraph1.Part3')}
+                </Typography>
+            </div>
+            <Typography>
+                {translate('Landing.RPA.Content.Paragraph2')}
+            </Typography>
         </div>
     );
 };
