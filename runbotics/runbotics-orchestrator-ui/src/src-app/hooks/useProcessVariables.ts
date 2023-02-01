ee70ec499c95caa6dc9697fc3e0e2e41f08d8df1
Barbara Darowska <comamysza@gmail.com>
2023-02-01 14:18:58 +0100

    adds custom hook for useProcessVariables

---
+++ runbotics/runbotics-orchestrator-ui/src/src-app/hooks/useProcessVariables.ts
@@ -0,0 +1,70 @@
+
+
+import { getProcessVariables } from '@bpmn-io/extract-process-variables';
+
+import { useSelector } from '#src-app/store';
+import { globalVariableSelector } from '#src-app/store/slices/GlobalVariable';
+
+import { currentProcessSelector } from '#src-app/store/slices/Process';
+
+import { useModelerContext } from './useModelerContext';
+
+enum VariableTag {
+  Global = 'GLOBAL',
+  InputOutput = 'INPUT/OUTPUT',
+  ActionAssigned = 'ACTION ASSIGNED'
+}
+
+const useProcessVariables = () => {
+    const { globalVariables } = useSelector(globalVariableSelector);
+    const context = useModelerContext();
+    const { executionInfo, isAttended } = useSelector(
+        currentProcessSelector
+    );
+    const { passedInVariables } = useSelector(
+        state => state.process.modeler);
+    
+    const getActionVariables = () => {
+        const canvas = context?.modeler?.get('canvas');
+        const rootElement = canvas.getRootElement();
+        const processVariables = getProcessVariables(rootElement.businessObject);
+
+        return processVariables;
+    };
+
+    const taggedActionVariables = getActionVariables().map(actionVariable => ({
+        name: `#{${actionVariable.name}}`,
+        tag: VariableTag.ActionAssigned
+    }));
+
+
+    const taggedGlobalVariables = globalVariables.map(item => ({
+        name: `#{${item.name}}`,
+        tag: VariableTag.Global
+    }));
+
+    const taggedAttendedVariables =
+            isAttended && executionInfo
+                ? passedInVariables.map(variable => ({
+                    name: `#{${variable}}`,
+                    tag: VariableTag.InputOutput
+                    
+                }))
+                : [];
+
+    // console.log(passedInVariables);
+    console.log('globalVariables', taggedGlobalVariables);
+    console.log('actionVariables', taggedActionVariables);
+    console.log('attended', taggedAttendedVariables);
+
+
+    // console.log('attended', attendedProcessVariables);
+    return {
+        taggedGlobalVariables,
+        taggedActionVariables,
+        taggedAttendedVariables
+    };
+
+};
+
+export default useProcessVariables;
