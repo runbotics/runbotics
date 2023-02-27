06431fd4f460a5cfdbcc5913d8b184eb11ee48cc
Barbara Darowska <comamysza@gmail.com>
2023-02-27 15:45:37 +0100

    rename and refactor

---
+++ runbotics/runbotics-orchestrator-ui/src/src-app/components/ProcessVariablesPanel/VariableCopyMenu.tsx
@@ -0,0 +1,61 @@
+
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import {
+    Menu, MenuItem, ListItemText, ListItemIcon
+} from '@mui/material';
+import { useSnackbar } from 'notistack';
+
+import { translate } from '#src-app/hooks/useTranslations';
+
+import { VariableTag } from './VariablesPanel/VariablesPanel';
+
+
+
+const VariableCopyMenu = ({ anchorElement, handleMenuClose, menuId, tag }) => {
+    const { enqueueSnackbar } = useSnackbar();
+    const hashName = `#{${menuId}}`;
+
+    const dollarName = tag === VariableTag.ACTION_OUTPUT ? `\${environment.output.${menuId}}` : `\${environment.variables.${menuId}}`;
+
+    const itemsToCopy = [hashName, dollarName];
+    
+    const isOpen = Boolean(menuId);
+
+    const handleCopy = (variableName: string) => {
+        navigator.clipboard.writeText(variableName);
+
+        enqueueSnackbar(
+            translate(
+                'Process.Modeler.VariablesPanel.Copy.Message.Success',
+            ),
+            { variant: 'success' },
+        );
+       
+        handleMenuClose();
+    };
+
+    const menuItems = itemsToCopy.map(item => (
+        <MenuItem key={item} onClick={() => handleCopy(item)}>
+            <ListItemText>{item}</ListItemText>
+            <ListItemIcon sx={{marginLeft: '1rem'}}>
+                <ContentCopyIcon/>
+            </ListItemIcon>
+        </MenuItem>)
+    );
+
+    return (
+        <Menu 
+            key={menuId}
+            onClose={handleMenuClose}
+            id={menuId}
+            anchorEl={anchorElement}
+            open={isOpen}
+        >
+            {menuItems}
+        </Menu>
+    );
+
+};
+
+
+export default VariableCopyMenu;
