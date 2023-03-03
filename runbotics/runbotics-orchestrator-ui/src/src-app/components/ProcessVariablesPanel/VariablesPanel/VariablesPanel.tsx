3006f806024e163bbdf4e3ade189a218b635ecb2
Barbara Darowska <comamysza@gmail.com>
2023-03-03 10:28:09 +0100

    refactors VariableRow to FC

--- runbotics/runbotics-orchestrator-ui/src/src-app/components/ProcessVariablesPanel/VariablesPanel/VariablesPanel.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/components/ProcessVariablesPanel/VariablesPanel/VariablesPanel.tsx
@@ -78,36 +78,6 @@ const VariablesPanel = () => {
             </GridContainer>
             <Divider />
         </Box>);
-
-    // const VariableRow = ({ name, tag }: { name: string, tag: VariableTag } ) => (
-    //     <Box key={name}>
-    //         <GridContainer container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-    //             <GridVariable item xs={5}>
-    //                 {name}
-    //             </GridVariable>
-    //             <GridTag item xs={5}>
-    //                 <Chip
-    //                     label={translate(`Process.Modeler.VariablesPanel.${tag}`).toUpperCase()}
-    //                     sx={{ bgcolor: getTagBgColor(tag), color: 'black', letterSpacing: 1.1}}
-    //                     size="medium"/>
-    //             </GridTag>
-    //             <Grid item xs={2}>
-    //                 <IconButton size="medium" onClick={(e) => handleMenuClick(e, name)}>
-    //                     <MoreVertIcon/>
-    //                 </IconButton>
-    //                 {openMenuId?.menuId === name ?
-    //                     <VariableCopyMenu 
-    //                         key={openMenuId.menuId}
-    //                         anchorElement={openMenuId.anchorElement} 
-    //                         handleMenuClose={handleMenuClose} 
-    //                         menuId={openMenuId.menuId} 
-    //                         tag={tag}
-    //                     />
-    //                     : null}
-    //             </Grid>
-    //         </GridContainer>
-    //         <Divider />
-    //     </Box>);
         
     const variables = [...globalVariables, ...attendedVariables, ...inputActionVariables];
 
@@ -119,14 +89,6 @@ const VariablesPanel = () => {
                 {translate('Process.Modeler.VariablesPanel.Empty.Message')}
             </Typography>);
     }
-
-    // const variableElements =  variables.map(variable => (
-    //     <VariableRow key={variable.name} name={variable.name} tag={VariableTag.VARIABLE}/>
-    // )); 
-
-    // const outputVariableElements =  outputActionVariables.map(variable => (
-    //     <VariableRow key={variable.name} name={variable.name} tag={VariableTag.ACTION_OUTPUT}/>
-    // )); 
 
     const variableElements =  variables.map(variable => 
         VariableRow({name: variable.name, tag: VariableTag.VARIABLE}
