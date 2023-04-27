76fd4aa0d721c1aa9cf96ac4c6c67f674279d177
Stanisław Jarocki <69820069+Stanlee77@users.noreply.github.com>
2023-04-27 14:31:55 +0200

    Add hover effect for blog post card (#202)
    * feat(runbotics-orchestrator-ui): :sparkles: Add hover effect for blog cards.
    
    * Self CR.
    
    * Replace LP lib moment with Intl & CR 1.
    
    * chore: :pushpin: bump app version to 2.6.0-SNAPSHOT.14
    
    * CR 2.
    
    ---------
    
    Co-authored-by: Stanislaw Jarocki <stanislaw.jarocki@all-for-one.com>

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogCard/BlogCard.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogCard/BlogCard.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 
-import moment from 'moment';
 import Image from 'next/image';
 
 import Link from 'next/link';
@@ -30,44 +29,46 @@ const BlogCard: FC<BlogCardProps> = ({ post }) => {
     const { translate } = useTranslations();
 
     return (
-        <article className={styles.root}>
-            <Image
-                src={post.featuredImage?.url}
-                fill
-                alt=""
-                className={styles.img}
-            />
-            {/* temp solution to tags, to be added in contentful schema */}
-            {post.category.slug === 'category-two' ? (
-                <CardBadge
-                    text={post.category.title}
-                    className={styles.badge}
-                />
-            ) : null}
-            <div className={styles.info}>
-                <Typography variant="body4">
-                    {moment(post.date).format('D.MM.YYYY')}
-                </Typography>
-                <Typography variant="body4" className={styles.category}>
-                    {post.category.title}
-                </Typography>
-            </div>
-            <div className={styles.content}>
-                <Typography variant="h4" className={styles.title}>
-                    {post.title}
-                </Typography>
-                <Typography variant="body3">
-                    {cutText(post.summary, 130)}
-                </Typography>
-            </div>
-            <div className={styles.readMore}>
-                <Link href={`/blog/post/${post.slug}`}>
-                    <Typography variant="body3">
-                        {translate('Landing.Blog.Card.ReadMore')}
-                    </Typography>
-                </Link>
-            </div>
-        </article>
+        <div className={styles.root}>
+            <Link className={styles.link} href={`/blog/post/${post.slug}`}>
+                <article className={styles.article}>
+                    <Image
+                        src={post.featuredImage?.url}
+                        fill
+                        alt=""
+                        className={styles.img}
+                    />
+                    {/* temp solution to tags, to be added in contentful schema */}
+                    {post.category.slug === 'category-two' ? (
+                        <CardBadge
+                            text={post.category.title}
+                            className={styles.badge}
+                        />
+                    ) : null}
+                    <div className={styles.info}>
+                        <Typography variant="body4">
+                            {new Intl.DateTimeFormat().format(new Date(post.date))}
+                        </Typography>
+                        <Typography variant="body4" className={styles.category}>
+                            {post.category.title}
+                        </Typography>
+                    </div>
+                    <div className={styles.content}>
+                        <Typography variant="h4" className={styles.title}>
+                            {post.title}
+                        </Typography>
+                        <Typography variant="body3">
+                            {cutText(post.summary, 130)}
+                        </Typography>
+                    </div>
+                    <div className={styles.readMore}>
+                        <Typography variant="body3">
+                            {translate('Landing.Blog.Card.ReadMore')}
+                        </Typography>
+                    </div>
+                </article>
+            </Link>
+        </div>
     );
 };
 
--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/FeaturedBlogCard/FeaturedBlogCard.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/FeaturedBlogCard/FeaturedBlogCard.tsx
@@ -1,8 +1,6 @@
 import React, { FC } from 'react';
 
-import moment from 'moment';
 import Image from 'next/image';
-
 import Link from 'next/link';
 
 import useTranslations from '#src-app/hooks/useTranslations';
@@ -19,39 +17,42 @@ interface FeaturedBlogCardProps {
 const FeaturedBlogCard: FC<FeaturedBlogCardProps> = ({ post }) => {
     const { translate } = useTranslations();
     if(!post) return null;
+
     return (
-        <article className={styles.root}>
-            <div className={styles.content}>
-                <Image
-                    src={post.featuredImage.url}
-                    fill
-                    alt=""
-                    className={styles.img}
-                />
-                <div className={styles.info}>
-                    <CardBadge text={post.category.title} />
-                    <Typography variant="body4">
-                        {moment(post.date).format('D.MM.YYYY')}
-                    </Typography>
-                    <Typography variant="body4" className={styles.category}>
-                        {post.category.title}
-                    </Typography>
-                </div>
-                <Typography variant="h3" className={styles.title}>
-                    {post.title}
-                </Typography>
-                <Typography variant="body3" className={styles.description}>
-                    {cutText(post.summary, 230)}
-                </Typography>
-                <div className={styles.readMore}>
-                    <Link href={`/blog/post/${post.slug}`}>
-                        <Typography variant="body5">
-                            {translate('Landing.Blog.Card.ReadMore')}
+        <div className={styles.root}>
+            <Link className={styles.link} href={`/blog/post/${post.slug}`}>
+                <article className={styles.article}>
+                    <div className={styles.content}>
+                        <Image
+                            src={post.featuredImage.url}
+                            fill
+                            alt=""
+                            className={styles.img}
+                        />
+                        <div className={styles.info}>
+                            <CardBadge text={post.category.title} />
+                            <Typography variant="body4">
+                                {new Intl.DateTimeFormat().format(new Date(post.date))}
+                            </Typography>
+                            <Typography variant="body4" className={styles.category}>
+                                {post.category.title}
+                            </Typography>
+                        </div>
+                        <Typography variant="h3" className={styles.title}>
+                            {post.title}
                         </Typography>
-                    </Link>
-                </div>
-            </div>
-        </article>
+                        <Typography variant="body3" className={styles.description}>
+                            {cutText(post.summary, 230)}
+                        </Typography>
+                        <div className={styles.readMore}>
+                            <Typography variant="body5">
+                                {translate('Landing.Blog.Card.ReadMore')}
+                            </Typography>
+                        </div>
+                    </div>
+                </article>
+            </Link>
+        </div>
     );
 };
 
