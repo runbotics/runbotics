fb4de44083c54d1a404a38350665a1eb7e0061f8
Stanislaw Jarocki <stanislaw.jarocki@all-for-one.com>
2023-04-27 12:59:14 +0200

    feat(runbotics-orchestrator-ui): :sparkles: Add hover effect for blog cards.

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/FeaturedBlogCard/FeaturedBlogCard.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/FeaturedBlogCard/FeaturedBlogCard.tsx
@@ -20,38 +20,40 @@ const FeaturedBlogCard: FC<FeaturedBlogCardProps> = ({ post }) => {
     const { translate } = useTranslations();
     if(!post) return null;
     return (
-        <article className={styles.root}>
-            <div className={styles.content}>
-                <Image
-                    src={post.featuredImage.url}
-                    fill
-                    alt=""
-                    className={styles.img}
-                />
-                <div className={styles.info}>
-                    <CardBadge text={post.category.title} />
-                    <Typography variant="body4">
-                        {moment(post.date).format('D.MM.YYYY')}
-                    </Typography>
-                    <Typography variant="body4" className={styles.category}>
-                        {post.category.title}
-                    </Typography>
-                </div>
-                <Typography variant="h3" className={styles.title}>
-                    {post.title}
-                </Typography>
-                <Typography variant="body3" className={styles.description}>
-                    {cutText(post.summary, 230)}
-                </Typography>
-                <div className={styles.readMore}>
-                    <Link href={`/blog/post/${post.slug}`}>
-                        <Typography variant="body5">
-                            {translate('Landing.Blog.Card.ReadMore')}
+        <div className={styles.root}>
+            <Link className={styles.link} href={`/blog/post/${post.slug}`}>
+                <article className={styles.article}>
+                    <div className={styles.content}>
+                        <Image
+                            src={post.featuredImage.url}
+                            fill
+                            alt=""
+                            className={styles.img}
+                        />
+                        <div className={styles.info}>
+                            <CardBadge text={post.category.title} />
+                            <Typography variant="body4">
+                                {moment(post.date).format('D.MM.YYYY')}
+                            </Typography>
+                            <Typography variant="body4" className={styles.category}>
+                                {post.category.title}
+                            </Typography>
+                        </div>
+                        <Typography variant="h3" className={styles.title}>
+                            {post.title}
                         </Typography>
-                    </Link>
-                </div>
-            </div>
-        </article>
+                        <Typography variant="body3" className={styles.description}>
+                            {cutText(post.summary, 230)}
+                        </Typography>
+                        <div className={styles.readMore}>
+                            <Typography variant="body5">
+                                {translate('Landing.Blog.Card.ReadMore')}
+                            </Typography>
+                        </div>
+                    </div>
+                </article>
+            </Link>
+        </div>
     );
 };
 
