eebe05ae8e65abb6890a33972a6c4fafdd6f2b9d
Stanislaw Jarocki <stanislaw.jarocki@all-for-one.com>
2023-04-28 15:01:21 +0200

    Create input WIP.

--- runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/blog/FiltersSection/FiltersSection.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/views/sections/blog/FiltersSection/FiltersSection.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
+
+
+import useTranslations from '#src-app/hooks/useTranslations';
+
 import styles from './FiltersSection.module.scss';
 
-const FiltersSection = () => <div className={styles.root}>FiltersSection</div>;
+const FiltersSection = () => {
+    const { translate } = useTranslations();
+
+    const handleSearch = () => {
+        console.log('handleSearch');
+    };
+
+    return (
+        <div className={styles.root}>
+            <div className={styles.search}>
+                <img className={styles.icon} src="/images/icons/search-black.svg" alt="search"/>
+                <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Search"
+                    onChange={handleSearch}
+                />
+            </div>
+        </div>
+
+    );
+};
 
 export default FiltersSection;
