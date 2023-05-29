47efca58e3c459f51c870a824674235c1b844d09
MRSIMM0 <s.pasieczny02@gmail.com>
2023-05-29 14:26:31 +0200

    refactor:

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -2,19 +2,19 @@ import React, { VFC, useEffect } from 'react';
 
 import { useRouter } from 'next/router';
 
-
 import useTranslations from '#src-app/hooks/useTranslations';
 import { languages, Language } from '#src-app/translations/translations';
 
+import {capitalizeFirstLetter} from '../../utils/utils';
+
+
 import styles from './LanguageSwitcher.module.scss';
+
 
 const LanguageSwitcher: VFC = () => {
     const { switchLanguage, translate } = useTranslations();
 
     const { push, locale: activeLocale, asPath } = useRouter();
-
-    const capitalizeFirstLetter = (string: string) =>
-        string.charAt(0).toUpperCase() + string.slice(1);
 
     const handleLanguageSwitch = (language: Language) => {
         push(asPath, undefined, {
