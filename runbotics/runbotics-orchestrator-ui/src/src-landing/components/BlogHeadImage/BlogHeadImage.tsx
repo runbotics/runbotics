7ca93fcfafe556c8ee1b30e5da0ffd77074870d7
MRSIMM0 <s.pasieczny02@gmail.com>
2023-05-31 09:24:42 +0200

    feat: :sparkles:

---
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+
+import Image from 'next/image';
+
+import styles from './BlogHeadImage.module.scss';
+
+const BlogHeadImage: FC = () => (
+    <div className={styles.image}>
+    </div>
+);
+
+export default BlogHeadImage;
