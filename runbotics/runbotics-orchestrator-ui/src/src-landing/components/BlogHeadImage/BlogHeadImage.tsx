7b74e35a556f1b0813faf8999501a64c92f67fa4
MRSIMM0 <s.pasieczny02@gmail.com>
2023-06-05 12:37:26 +0200

    perf: :zap: Introduced Next image as background

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
@@ -1,9 +1,14 @@
 import {FC} from 'react';
 
+import Image from 'next/image';
+
 import styles from './BlogHeadImage.module.scss';
 
 const BlogHeadImage: FC = () => (
-    <div className={styles.image} />
+    <div className={styles.image}>
+        <Image src='/images/banners/blog-header.png' alt='' style={{objectFit: 'cover'}} fill={true}/>
+    </div>
+
 );
 
 export default BlogHeadImage;
