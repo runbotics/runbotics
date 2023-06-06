3d6f05ee90248aafdef11ae75977b8b94a596abc
MRSIMM0 <s.pasieczny02@gmail.com>
2023-06-06 14:17:26 +0200

    style: :lipstick: Fixed styles

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
@@ -6,7 +6,7 @@ import styles from './BlogHeadImage.module.scss';
 
 const BlogHeadImage: FC = () => (
     <div className={styles.image}>
-        <Image src='/images/banners/blog-header.png' alt='' style={{objectFit: 'cover'}} fill={true}/>
+        <Image src='/images/banners/blog-header.png' alt='banner' style={{objectFit: 'cover'}} fill={true}/>
     </div>
 
 );
