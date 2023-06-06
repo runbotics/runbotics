5857df785fceb17a52701048a39bc8d19c3b0377
MRSIMM0 <s.pasieczny02@gmail.com>
2023-06-06 15:45:12 +0200

    style(runbotics-orchestrator-ui): :lipstick: Finishing touches

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
@@ -2,11 +2,14 @@ import {FC} from 'react';
 
 import Image from 'next/image';
 
+import Background from '#public/images/banners/blog-header.png';
+
 import styles from './BlogHeadImage.module.scss';
+
 
 const BlogHeadImage: FC = () => (
     <div className={styles.image}>
-        <Image src='/images/banners/blog-header.png' alt='banner' style={{objectFit: 'cover'}} fill={true}/>
+        <Image src={Background} alt='banner' className={styles.background} fill/>
     </div>
 );
 
