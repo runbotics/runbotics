0e2097c08c6f73223262e7ec150b5a483ccdec9b
MRSIMM0 <s.pasieczny02@gmail.com>
2023-06-12 12:39:29 +0200

    style(runbotics-orchestrator-ui): :art: Fixed css

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
@@ -1,20 +1,16 @@
 import { FC } from 'react';
 
 import Image from 'next/image';
-import { CSSProperties } from 'styled-components';
 
 import Background from '#public/images/banners/blog-header.png';
 
 import styles from './BlogHeadImage.module.scss';
 
-const imageStyle: CSSProperties = {
-    objectFit: 'cover',
-    margin: 'auto',
-};
+
 
 const BlogHeadImage: FC = () => (
     <div className={styles.image}>
-        <Image src={Background} alt="banner" style={imageStyle} fill />
+        <Image className={styles.background} src={Background} alt="banner" fill />
     </div>
 );
 
