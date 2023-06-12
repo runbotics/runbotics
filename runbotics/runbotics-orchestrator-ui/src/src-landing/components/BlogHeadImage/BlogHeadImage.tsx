b12976392b92806651fe239639892ee12ad878eb
MRSIMM0 <s.pasieczny02@gmail.com>
2023-06-12 11:31:03 +0200

    style(runbotics-orchestrator-ui): :art: Added separate object for css props

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/BlogHeadImage/BlogHeadImage.tsx
@@ -1,15 +1,20 @@
-import {FC} from 'react';
+import { FC } from 'react';
 
 import Image from 'next/image';
+import { CSSProperties } from 'styled-components';
 
 import Background from '#public/images/banners/blog-header.png';
 
 import styles from './BlogHeadImage.module.scss';
 
+const imageStyle: CSSProperties = {
+    objectFit: 'cover',
+    margin: 'auto',
+};
 
 const BlogHeadImage: FC = () => (
     <div className={styles.image}>
-        <Image src={Background} alt='banner'  style={{objectFit:'cover', margin:'auto'}} fill/>
+        <Image src={Background} alt="banner" style={imageStyle} fill />
     </div>
 );
 
