87a4c79374c08abde580147bb0d7ffd02a2c5291
MRSIMM0 <s.pasieczny02@gmail.com>
2023-06-22 15:24:32 +0200

    feat(runbotics-orchestrator-ui): :art: Deleted replace funtionality from components different form Boundary Event

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/contextPad/CustomContextPad.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/contextPad/CustomContextPad.ts
@@ -1,16 +1,27 @@
+
+import { BpmnElementType } from 'runbotics-common';
+
 // @ts-nocheck
 export default class CustomContextPad {
+
+    contextPad: any;
+    popupMenu: any;
+
     constructor(contextPad) {
         contextPad.registerProvider(this);
+        this.contextPad = contextPad;
     }
 
-    getContextPadEntries() {
+    getContextPadEntries(el) {
         return function (entries) {
             delete entries['append.append-task'];
-
+            if(el.type !== BpmnElementType.BOUNDARY_EVENT){
+                delete entries.replace;
+            }
             return entries;
         };
     }
+
 }
 
-CustomContextPad.$inject = ['contextPad'];
+CustomContextPad.$inject = ['contextPad', 'popupMenu'];
