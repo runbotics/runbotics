f7dd210045448c0337f4119e5562bdd7c745def6
GBCC\samelakk <klaudynasamelak@gmail.com>
2023-07-06 15:23:13 +0200

    DatePicker fixes

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/widgets/DatePickerWidget/DatePickerWidget.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/widgets/DatePickerWidget/DatePickerWidget.tsx
@@ -1,59 +1,93 @@
 import React, { FC, useState } from 'react';
 
 import { TextField } from '@mui/material';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { CalendarOrClockPickerView } from '@mui/x-date-pickers/internals/models';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { WidgetProps } from '@rjsf/core';
+import moment from 'moment';
+
+const inputTypes: {
+    [key: string]: { format: string; views: CalendarOrClockPickerView[] };
+} = {
+    date: {
+        format: 'YYYY-MM-DD',
+        views: ['year', 'month', 'day'],
+    },
+    'date-time': {
+        format: 'YYYY-MM-DD HH:mm',
+        views: ['year', 'month', 'day', 'hours', 'minutes'],
+    },
+    time: {
+        format: 'HH:mm',
+        views: ['hours', 'minutes'],
+    },
+    default: {
+        format: 'YYYY-MM-DD',
+        views: ['year', 'month', 'day'],
+    },
+};
 
 const DatePickerWidget: FC<WidgetProps> = (props) => {
     const [selectedDate, setSelectedDate] = useState<Date | null>(null);
     const format: string = props.uiSchema['ui:options']?.format as string;
-    let inputFormat = '';
+    const typeFound: string = Object.keys(inputTypes).find(
+        (type) => type === format
+    );
+    let inputFormat: string = '';
     let views: CalendarOrClockPickerView[] = [];
+
+    if (typeFound) {
+        inputFormat = inputTypes[typeFound].format;
+        views = inputTypes[typeFound].views;
+    }
+    if (!typeFound && format) {
+        inputFormat = format;
+        views = ['year', 'month', 'day', 'hours', 'minutes'];
+    } else {
+        inputFormat = inputTypes.default.format;
+        views = inputTypes.default.views;
+    }
 
     const handleDataChange = (date: Date | null) => {
         setSelectedDate(date);
-        props.onChange(date.toISOString());
+        if (date !== null) {
+            if (typeFound) {
+                props.onChange(
+                    moment(date).format(inputTypes[typeFound].format)
+                );
+            } else {
+                props.onChange(moment(date).format());
+            }
+        }
     };
 
     const renderInput = (params: any) => (
         <TextField
             {...params}
-            sx={{ button: { marginRight: (theme) => theme.spacing(0.5) } }}
+            label={props.label}
+            sx={{
+                button: { marginRight: (theme) => theme.spacing(0.5) },
+                caretColor: 'transparent',
+            }}
+            required={props.required}
         />
     );
 
-    switch (format) {
-        case 'date':
-            inputFormat = 'yyyy-MM-dd';
-            views = ['year', 'month', 'day'];
-            break;
-        case 'date-time':
-            inputFormat = 'yyyy-MM-dd HH:mm';
-            views = ['year', 'month', 'day', 'hours', 'minutes'];
-            break;
-        case 'time':
-            inputFormat = 'HH:mm';
-            views = ['hours', 'minutes'];
-            break;
-        default:
-            inputFormat = format ?? 'yyyy-MM-dd';
-            views = ['year', 'month', 'day', 'hours', 'minutes'];
-            break;
-    }
-
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <LocalizationProvider dateAdapter={AdapterMoment}>
             <DateTimePicker
-                label={props.required ? `${props.label} *` : props.label}
                 inputFormat={inputFormat}
                 value={selectedDate}
                 onChange={handleDataChange}
                 renderInput={renderInput}
                 views={views}
-                InputProps={{ readOnly: true }}
+                InputProps={{
+                    onKeyDown: (event) => {
+                        event.preventDefault();
+                    },
+                }}
             />
         </LocalizationProvider>
     );
