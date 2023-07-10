3d82abbc78b6e6f9b5f40e19efab4b33f4b5581f
Klau-Sam <63171496+Klau-Sam@users.noreply.github.com>
2023-07-10 09:57:51 +0200

    [feature/RPA-820-2]: DatePicker widget (#260)
    * [feature/RPA-820]: DatePicker widget
    
    * UI fixes
    
    * DatePicker fixes
    
    * UI build final fix
    
    * CR fixes
    
    * chore: 📌 bump app version to 2.7.0-SNAPSHOT.39
    
    * Type check add

---
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/widgets/DatePickerWidget/DatePickerWidget.tsx
@@ -0,0 +1,95 @@
+import React, { FC, useState } from 'react';
+
+import { TextField } from '@mui/material';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { WidgetProps } from '@rjsf/core';
+
+import moment from 'moment';
+
+import { StyledDateTimePicker } from './DatePickerWidget.styles';
+
+const inputTypes = {
+    date: {
+        format: 'YYYY-MM-DD',
+        views: ['year', 'month', 'day'],
+    },
+    'date-time': {
+        format: 'YYYY-MM-DD HH:mm',
+        views: ['year', 'month', 'day', 'hours', 'minutes'],
+    },
+    time: {
+        format: 'HH:mm',
+        views: ['hours', 'minutes'],
+    },
+} as const;
+
+const checkFormat = (format: any) => {
+    if (!format || typeof format !== 'string') {
+        return {
+            inputFormat: inputTypes.date.format,
+            views: inputTypes.date.views,
+        };
+    }
+
+    const typeFound: string = Object.keys(inputTypes).find(
+        (type) => type === format
+    );
+    if (typeFound) {
+        return {
+            inputFormat: inputTypes[typeFound].format,
+            views: inputTypes[typeFound].views,
+        };
+    }
+
+    return { inputFormat: format, views: inputTypes['date-time'].views };
+};
+
+const DatePickerWidget: FC<WidgetProps> = (props) => {
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+    const format = props.uiSchema['ui:options']?.format;
+    const { inputFormat, views } = checkFormat(format);
+
+    const handleDateChange = (date: Date | null) => {
+        setSelectedDate(date);
+        if (date !== null) {
+            props.onChange(moment(date).format(inputFormat));
+        }
+    };
+
+    const renderInput = (params: any) => (
+        <TextField
+            {...params}
+            label={props.label}
+            required={props.required}
+            color={selectedDate === null ? 'error' : 'primary'}
+            sx={{
+                button: { marginRight: (theme) => theme.spacing(0.5) },
+                caretColor: 'transparent',
+            }}
+            {...(selectedDate === null && {
+                InputLabelProps: { style: { color: 'error' } },
+            })}
+        />
+    );
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+            <StyledDateTimePicker
+                inputFormat={inputFormat}
+                value={selectedDate}
+                onChange={handleDateChange}
+                renderInput={renderInput}
+                views={views}
+                InputProps={{
+                    onKeyDown: (event) => {
+                        event.preventDefault();
+                    },
+                }}
+                error={selectedDate === null}
+            />
+        </LocalizationProvider>
+    );
+};
+
+export default DatePickerWidget;
