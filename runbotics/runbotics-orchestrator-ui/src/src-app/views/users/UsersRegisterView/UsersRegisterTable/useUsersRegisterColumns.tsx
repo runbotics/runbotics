29eb42f5636eb77bda80b09e1563bd8114dfad0d
Ania <anna.gasiorowska@all-for-one.com>
2023-07-18 10:12:36 +0200

    feat(runbotics-orchestrator-ui): :sparkles: edited column with calendar format

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/users/UsersRegisterView/UsersRegisterTable/useUsersRegisterColumns.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/users/UsersRegisterView/UsersRegisterTable/useUsersRegisterColumns.tsx
@@ -1,5 +1,6 @@
 import { Select, MenuItem } from '@mui/material';
-import { GridColDef } from '@mui/x-data-grid';
+import { GridColDef, GridValueFormatterParams } from '@mui/x-data-grid';
+import moment from 'moment';
 
 import useTranslations from '#src-app/hooks/useTranslations';
 
@@ -17,43 +18,46 @@ const useUsersRegisterColumns = (): GridColDef[] => {
         {
             field: 'createdDate',
             headerName: 'Create date',
-            flex: 0.3
+            flex: 0.3,
+            valueFormatter: (params: GridValueFormatterParams) =>
+                moment(params.value as string).format('YYYY-MM-DD HH:mm')
         },
         {
             field: 'role',
             headerName: 'Role',
             flex: 0.5,
-            renderCell: () => {
-                return (
-                    <Select fullWidth>
-                        <MenuItem value='User'>User</MenuItem>
-                        <MenuItem value='Admin'>Admin</MenuItem>
-                    </Select>
-                );
-            },
+            renderCell: () => (
+                <Select
+                    fullWidth
+                    required
+                >
+                    <MenuItem value='user'>User</MenuItem>
+                    <MenuItem value='admin'>Admin</MenuItem>
+                </Select>
+            )
         },
         {
             field: 'action',
             headerName: 'Action',
             flex: 0.6,
-            renderCell: () => {
-                return (
-                    <>
-                        <StyledButton
-                            variant='contained'
-                            color='primary'
-                        >
-                            Accept
-                        </StyledButton>
-                        <StyledButton 
-                            variant='outlined'
-                            color='primary'
-                        >
-                            Deny
-                        </StyledButton>
-                    </>
-                );
-            },
+            renderCell: () => (
+                <>
+                    <StyledButton
+                        type='submit'
+                        variant='contained'
+                        color='primary'
+                    >
+                        Accept
+                    </StyledButton>
+                    <StyledButton
+                        type='submit'
+                        variant='outlined'
+                        color='primary'
+                    >
+                        Deny
+                    </StyledButton>
+                </>
+            )
         }
     ];
 };
