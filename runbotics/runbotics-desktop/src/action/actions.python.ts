7c71864ec54e6dac79439d2bc071c0e6ee5f7d7b
Stanislaw Jarocki <stanislaw.jarocki@all-for-one.com>
2023-07-25 08:53:28 +0200

    refactor(runbotics-desktop): :adhesive_bandage: add runPythonScript docs & other (cr fixes)

--- runbotics/runbotics-desktop/src/action/actions.python.ts
+++ runbotics/runbotics-desktop/src/action/actions.python.ts
@@ -1,17 +1,28 @@
 import { PythonShellConfig } from './action.types';
 import { PythonShell } from 'python-shell';
 
+/**
+ *  @name Run Python Script
+ *  @description Runs desired .py file with given arguments.
+ *  @param args - Array of arguments to be passed to python script.
+ *  @param pythonScriptDirectory - Path to directory with python script file.
+ *  @param pythonScriptName - Name of the .py file.
+ *  @example
+ *      args: ['C:/myDir/myExcelFile.xlsx', 'A', 1, 'myValue']
+ *      pythonScriptDirectory: './src/action/excel/python'
+ *      pythonScriptName: 'set_cell_value.py');
+ *  @returns array of python messages (prints) or thrown error.
+ */
 export const runPythonScript = async (args, pythonScriptDirectory, pythonScriptName) => {
     const options: PythonShellConfig = {
         mode: 'text',
         pythonPath: 'python',
-        pythonOptions: ['-u'], // print results in real-time
+        pythonOptions: ['-u'],
         scriptPath: pythonScriptDirectory,
         args: [...args],
     };
 
     return PythonShell
         .run(pythonScriptName, options)
-        .then(messages => messages) // array of python script print() messages
         .catch(error => { throw error; });
 };
