71c327c50b9ee180580b9d49f92024ae234f6abf
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2023-07-28 15:16:26 +0200

    Adds partialAccountUpdate method

--- runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/Users/Users.thunks.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/Users/Users.thunks.ts
@@ -2,14 +2,24 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { IUser } from 'runbotics-common';
 
-export const getAll = createAsyncThunk<IUser[], void>(
-    'users/getAll',
-    () => axios.get<IUser[]>('/api/admin/users')
-        .then((response) => response.data),
+export const getAll = createAsyncThunk<IUser[], void>('users/getAll', () =>
+    axios.get<IUser[]>('/api/admin/users').then((response) => response.data)
 );
 
 export const getAllLimited = createAsyncThunk<IUser[], void>(
     'users/getAll',
-    () => axios.get<IUser[]>('/api/admin/users/limited')
-        .then((response) => response.data),
+    () =>
+        axios
+            .get<IUser[]>('/api/admin/users/limited')
+            .then((response) => response.data)
+);
+
+export const partialAccountUpdate = createAsyncThunk(
+    'account/update',
+    async (fieldsToUpdate: IUser, { rejectWithValue }) => {
+        await axios
+            .patch('/api/account', fieldsToUpdate)
+            .then((response) => response.data)
+            .catch((error) => rejectWithValue(error.response.data));
+    }
 );
