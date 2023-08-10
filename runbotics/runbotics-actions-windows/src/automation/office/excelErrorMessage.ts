bb5e9e45b515dc093c548966adab1288f4044123
Stanisław Jarocki <69820069+Stanlee77@users.noreply.github.com>
2023-08-10 16:35:59 +0200

    [RPA-919] Add Find first empty row action. (#318)
    * feat(runbotics-orchestrator-ui): :sparkles: implement new Set first empty row action
    
    * feat(runbotics-orchestrator-ui): :recycle: finish implementation of Set First Empty Row action
    
    * refactor(runbotics-orchestrator-ui): :recycle: change >set< first empty row to >find<, remove utils & other
    
    * refactor: :adhesive_bandage: remove switchWorksheet method
    
    * refactor(runbotics-orchestrator-ui): :recycle: allow for cell skip in Set cells action
    
    * chore: :pushpin: bump app version to 2.8.0-SNAPSHOT.18
    
    ---------
    
    Co-authored-by: Stanislaw Jarocki <stanislaw.jarocki@all-for-one.com>

---
+++ runbotics/runbotics-actions-windows/src/automation/office/excelErrorMessage.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export default class ExcelErrorMessage {
+    constructor() { }
+
+    static columnCoordinateIncorrectInput(e?: Error): string {
+        return `
+            ${this.getDisclaimer(e)}
+            Row must be a number (e.g. 5).
+            Check the Input tab above.
+        `
+    }
+
+    static cellCoordinatesIncorrectInput(e?: Error): string {
+        return `
+            ${this.getDisclaimer(e)}
+            Start row must be a number (e.g. 5).
+            Start column must be a string (e.g. A).
+            Check startRow and startColumn in the Input tab above.
+        `
+    }
+
+    static setCellsIncorrectInput(e?: Error): string {
+        return `
+            ${this.getDisclaimer(e)}
+            Input must be an Array (e.g. [["C3", "D3", "E3"], ["C4", "D4", "E4"]]).
+            Check the Input tab above.
+            Try to pass it as variable (e.g. #{myArray}).
+        `
+    }
+
+    static noPreviousWorksheet(e?: Error): string {
+        return `
+            ${this.getDisclaimer(e)}
+            There is no previous worksheet.
+        `
+    }
+
+    private static getDisclaimer(e?: Error): string {
+        return `
+                ${e ?? ''}
+                Common (related to action, not error) solutions:
+            `
+    }
+}
