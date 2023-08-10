e1c8074cb0a2b0d55e425c9eff5c7f88b00542fa
Szymon Pasieczny <44839807+MRSIMM0@users.noreply.github.com>
2023-08-10 12:46:59 +0200

    [RPA 894] Revert default flow option (#333)
    * fix(runbotics-orchestrator-ui): :bug: Added replace to SequenceFlow
    
    * fix(runbotics-orchestrator-ui): :bug: Covered all edge cases
    
    * chore: :pushpin: bump app version to 2.8.0-SNAPSHOT.16

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/contextPad/CustomContextPad.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/contextPad/CustomContextPad.ts
@@ -1,9 +1,7 @@
-
 import { BpmnElementType } from 'runbotics-common';
 
 // @ts-nocheck
 export default class CustomContextPad {
-
     constructor(contextPad) {
         contextPad.registerProvider(this);
     }
@@ -11,13 +9,23 @@ export default class CustomContextPad {
     getContextPadEntries(el) {
         return function (entries) {
             delete entries['append.append-task'];
-            if(el.type !== BpmnElementType.BOUNDARY_EVENT){
+
+            if (el.type !== BpmnElementType.BOUNDARY_EVENT) {
                 delete entries.replace;
             }
+
+            if (el.type === BpmnElementType.SEQUENCE_FLOW) {
+                entries.replace = {
+                    group: 'edit',
+                    className: 'bpmn-icon-screw-wrench',
+                    title: 'Change type',
+                    action: {},
+                };
+            }
+
             return entries;
         };
     }
-
 }
 // @ts-ignore
 CustomContextPad.$inject = ['contextPad'];
