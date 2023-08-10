7c65e02c407d605ea19bf3e3ac6ebdc3c43b985f
MRSIMM0 <s.pasieczny02@gmail.com>
2023-08-10 11:44:03 +0200

    fix(runbotics-orchestrator-ui): :bug: Added replace to SequenceFlow

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/contextPad/CustomContextPad.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/contextPad/CustomContextPad.ts
@@ -1,9 +1,7 @@
-
 import { BpmnElementType } from 'runbotics-common';
 
 // @ts-nocheck
 export default class CustomContextPad {
-
     constructor(contextPad) {
         contextPad.registerProvider(this);
     }
@@ -11,13 +9,15 @@ export default class CustomContextPad {
     getContextPadEntries(el) {
         return function (entries) {
             delete entries['append.append-task'];
-            if(el.type !== BpmnElementType.BOUNDARY_EVENT){
+            if (
+                el.type !== BpmnElementType.BOUNDARY_EVENT &&
+                el.type !== BpmnElementType.SEQUENCE_FLOW
+            ) {
                 delete entries.replace;
             }
             return entries;
         };
     }
-
 }
 // @ts-ignore
 CustomContextPad.$inject = ['contextPad'];
