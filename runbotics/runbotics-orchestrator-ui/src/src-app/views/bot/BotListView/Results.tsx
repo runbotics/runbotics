c8eb3695dc80fede04a337f41ae81c154ca7ab8b
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2023-08-30 12:36:40 +0200

    [RPA-955] Fixes display of a menu icon in bot list view (#368)
    * fix(runbotics-orchestrator-ui): :lipstick: Corrects display of a menu icon for bot list view
    
    * chore: :pushpin: bump app version to 2.8.0-SNAPSHOT.49

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/bot/BotListView/Results.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/bot/BotListView/Results.tsx
@@ -149,7 +149,7 @@ const Results: FC<ResultsProps> = ({ className, collectionId, ...rest }) => {
                                 <TableCell>{translate('Bot.ListView.Results.Table.Header.System')}</TableCell>
                                 <TableCell>{translate('Bot.ListView.Results.Table.Header.Version')}</TableCell>
                                 <TableCell>{translate('Bot.ListView.Results.Table.Header.LastConnected')}</TableCell>
-                                <TableCell width={20} />
+                                <TableCell width={80} />
                             </TableRow>
                         </TableHead>
                         <TableBody>
