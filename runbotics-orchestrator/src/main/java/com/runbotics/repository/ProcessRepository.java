ec0cd62bb75a93231c256b87527d1e967bc076b7
preludium <szymczak.mikolaj08@gmail.com>
2023-09-05 16:54:20 +0200

    fix(runbotics-orchestrator): :bug: add missing import

--- runbotics-orchestrator/src/main/java/com/runbotics/repository/ProcessRepository.java
+++ runbotics-orchestrator/src/main/java/com/runbotics/repository/ProcessRepository.java
@@ -1,15 +1,13 @@
 package com.runbotics.repository;
 
 import com.runbotics.domain.Process;
-import com.runbotics.domain.User;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.DoubleStream;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * Spring Data SQL repository for the Process entity.
