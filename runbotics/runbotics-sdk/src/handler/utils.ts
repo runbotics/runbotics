face46c473addff91043c0ab04fc8391a00fa662
Stanisław Jarocki <st.jar57@gmail.com>
2023-09-13 17:32:00 +0200

    feat(runbotics-desktop): :sparkles: implement multithreading

--- runbotics/runbotics-sdk/src/handler/utils.ts
+++ runbotics/runbotics-sdk/src/handler/utils.ts
@@ -4,25 +4,23 @@ import { StatefulActionHandler } from './StatefulActionHandler';
 export enum ActionHandler {
     Stateful = 'StatefulActionHandler',
     Stateless = 'StatelessActionHandler',
+    StatefulMultithreaded = 'StatefulMultithreaded',
+    StatelessMultithreaded = 'StatelessMultithreaded',
 };
 
 export const isStatelessActionHandler = (
     handler: unknown
-): handler is StatelessActionHandler => handler !== undefined
-    && handler !== null
-    && typeof handler === 'object'
-    && 'getType' in handler
-    && handler.getType
-    && typeof handler.getType === 'function'
-    && handler.getType() === ActionHandler.Stateless;
+): handler is StatelessActionHandler => typeof handler === 'object'
+&& 'getType' in handler
+&& handler.getType
+&& typeof handler.getType === 'function'
+    && [ActionHandler.Stateless, ActionHandler.StatelessMultithreaded].includes(handler.getType());
 
 export const isStatefulActionHandler = (
     handler: unknown
-): handler is StatefulActionHandler => handler !== undefined
-    && handler !== null
-    && typeof handler === 'object'
-    && 'getType' in handler
-    && typeof handler.getType === 'function'
-    && handler.getType() === ActionHandler.Stateful
-    && 'tearDown' in handler
+): handler is StatefulActionHandler => typeof handler === 'object'
+&& 'getType' in handler
+&& typeof handler.getType === 'function'
+&& [ActionHandler.Stateful, ActionHandler.StatefulMultithreaded].includes(handler.getType())
+&& 'tearDown' in handler
     && typeof handler.tearDown === 'function';
