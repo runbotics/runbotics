aaa54fdd8f19759511b8524bf4797525045159ce
Stanisław Jarocki <69820069+Stanlee77@users.noreply.github.com>
2023-09-22 11:50:16 +0200

    [RPA-937.2] Revert Multithreading changes (#410)
    * Revert "fix(runbotics-sdk): :ambulance: fix bot not working (#406)"
    
    * Revert "[RPA-937] Implement multithreading (#400)"
    
    * fix(runbotics-scheduler): :ambulance: fix scheduler db connection
    
    * chore: :pushpin: bump app version to 2.8.0-SNAPSHOT.83
    
    * fix(runbotics-desktop): :adhesive_bandage: fix extensions import

--- runbotics/runbotics-sdk/src/handler/utils.ts
+++ runbotics/runbotics-sdk/src/handler/utils.ts
@@ -4,8 +4,6 @@ import { StatefulActionHandler } from './StatefulActionHandler';
 export enum ActionHandler {
     Stateful = 'StatefulActionHandler',
     Stateless = 'StatelessActionHandler',
-    MultithreadStateful = 'MultithreadStatefulActionHandler',
-    MultithreadStateless = 'MultithreadStatelessActionHandler',
 };
 
 export const isStatelessActionHandler = (
@@ -16,7 +14,7 @@ export const isStatelessActionHandler = (
     && 'getType' in handler
     && handler.getType
     && typeof handler.getType === 'function'
-    && ActionHandler.Stateless === handler.getType();
+    && handler.getType() === ActionHandler.Stateless;
 
 export const isStatefulActionHandler = (
     handler: unknown
@@ -25,28 +23,6 @@ export const isStatefulActionHandler = (
     && typeof handler === 'object'
     && 'getType' in handler
     && typeof handler.getType === 'function'
+    && handler.getType() === ActionHandler.Stateful
     && 'tearDown' in handler
-    && typeof handler.tearDown === 'function'
-    &&  ActionHandler.Stateful === handler.getType();
-
-
-export const isMultithreadStatelessActionHandler = (
-    handler: unknown
-): handler is StatelessActionHandler => handler !== undefined
-    && handler !== null
-    && typeof handler === 'object'
-    && 'getType' in handler
-    && handler.getType
-    && typeof handler.getType === 'function'
-    && ActionHandler.MultithreadStateless === handler.getType();
-
-export const isMultithreadStatefulActionHandler = (
-    handler: unknown
-): handler is StatefulActionHandler => handler !== undefined
-    && handler !== null
-    && typeof handler === 'object'
-    && 'getType' in handler
-    && typeof handler.getType === 'function'
-    && 'tearDown' in handler
-    && typeof handler.tearDown === 'function'
-    && ActionHandler.MultithreadStateful === handler.getType();
+    && typeof handler.tearDown === 'function';
