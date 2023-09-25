01c179f4bdfdb7125fc575f69237005f65a09965
Depcik <marcin.depcik@all-for-one.com>
2023-09-25 18:36:57 +0200

    fix(runbotics-orchestrator-ui): :zap: Add custom server and refactor caching

--- runbotics/runbotics-orchestrator-ui/next.config.js
+++ runbotics/runbotics-orchestrator-ui/next.config.js
@@ -1,60 +1,49 @@
-const { PHASE_PRODUCTION_SERVER } = require('next/constants');
-const { initializeContentfulCache } = require('./src/contentful/initializeContentfulCache.js');
-
-module.exports = async (phase) => {
-    const nextConfig = {
-        rewrites: () => process.env.NODE_ENV === 'development'
-            ? [
-                {
-                    source: '/api/:path*',
-                    destination: 'http://localhost:8080/api/:path*', // The :path parameter is used here so will not be automatically passed in the query
-                },
-                {
-                    source: '/scheduler/:path*',
-                    destination: 'http://localhost:4000/scheduler/:path*',
-                },
-            ]
-            : [],
-        webpack: (config) => {
-            config.resolve.fallback = { '@material-ui/core': false, '@material-ui/icons': false };
-
-            return config;
-        },
-        publicRuntimeConfig: {
-            runboticsEntrypointUrl: process.env.RUNBOTICS_ENTRYPOINT_URL || 'http://localhost:4000',
-            mixpanelAnalyticsToken: process.env.MIXPANEL_ANALYTICS_TOKEN
-        },
-
-        modularizeImports: {
-            '@mui/icons-material': {
-                transform: '@mui/icons-material/{{member}}',
+module.exports = {
+    rewrites: () => process.env.NODE_ENV === 'development'
+        ? [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:8080/api/:path*', // The :path parameter is used here so will not be automatically passed in the query
             },
-            '@mui/material': {
-                transform: '@mui/material/{{member}}',
-            }
-        },
+            {
+                source: '/scheduler/:path*',
+                destination: 'http://localhost:4000/scheduler/:path*',
+            },
+        ]
+        : [],
+    webpack: (config) => {
+        config.resolve.fallback = { '@material-ui/core': false, '@material-ui/icons': false };
 
-        serverRuntimeConfig: {
-            mailHost: process.env.MAIL_HOST,
-            mailPort: process.env.MAIL_PORT,
-            mailUsername: process.env.MAIL_USERNAME,
-            mailPassword: process.env.MAIL_PASSWORD,
-        },
+        return config;
+    },
+    publicRuntimeConfig: {
+        runboticsEntrypointUrl: process.env.RUNBOTICS_ENTRYPOINT_URL || 'http://localhost:4000',
+        mixpanelAnalyticsToken: process.env.MIXPANEL_ANALYTICS_TOKEN
+    },
 
-        images: {
-            domains: [ "images.ctfassets.net" ]
+    modularizeImports: {
+        '@mui/icons-material': {
+            transform: '@mui/icons-material/{{member}}',
         },
-        // consts declared at: runbotics\runbotics\runbotics-orchestrator-ui\src\src-app\translations\translations.ts
-        i18n: {
-            locales: [ 'en', 'pl' ],
-            defaultLocale: 'en',
-            localeDetection: false,
-        },
-    }
+        '@mui/material': {
+            transform: '@mui/material/{{member}}',
+        }
+    },
 
-    if (phase === PHASE_PRODUCTION_SERVER) {
-        await initializeContentfulCache();
-    }
+    serverRuntimeConfig: {
+        mailHost: process.env.MAIL_HOST,
+        mailPort: process.env.MAIL_PORT,
+        mailUsername: process.env.MAIL_USERNAME,
+        mailPassword: process.env.MAIL_PASSWORD,
+    },
 
-    return nextConfig;
+    images: {
+        domains: [ "images.ctfassets.net" ]
+    },
+    // consts declared at: runbotics\runbotics\runbotics-orchestrator-ui\src\src-app\translations\translations.ts
+    i18n: {
+        locales: [ 'en', 'pl' ],
+        defaultLocale: 'en',
+        localeDetection: false,
+    },
 };
