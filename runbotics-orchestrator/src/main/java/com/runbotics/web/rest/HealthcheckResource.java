b07b62addf99cb360e18a09ef38339ab5f249a25
Depcik <marcin.depcik@all-for-one.com>
2023-10-24 16:08:41 +0200

    style(runbotics-orchestrator): :art: Add line break

--- runbotics-orchestrator/src/main/java/com/runbotics/web/rest/HealthcheckResource.java
+++ runbotics-orchestrator/src/main/java/com/runbotics/web/rest/HealthcheckResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HealthcheckResource {
+
     @GetMapping("/healthcheck")
     public String healthcheck() {
         return "Orchestrator is running";
