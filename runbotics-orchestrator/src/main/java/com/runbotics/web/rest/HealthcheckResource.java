b8b7366dfbd56f85e4d7f19fdb7d3d4843306e93
Marcin <82586165+marcin-depcik@users.noreply.github.com>
2023-10-25 07:22:04 +0200

    [RPA-1057] Add healthchecks in all services (#446)
    * feat(runbotics-orchestrator): :stethoscope: Add healthcheck endpoint
    
    * style(runbotics-orchestrator): :art: Add line break
    
    * chore: :pushpin: bump app version to 2.8.0-SNAPSHOT.118
    
    ---------
    
    Co-authored-by: Depcik <marcin.depcik@all-for-one.com>

---
+++ runbotics-orchestrator/src/main/java/com/runbotics/web/rest/HealthcheckResource.java
@@ -0,0 +1,18 @@
+package com.runbotics.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller to check if orchestrator is still running.
+ */
+@RestController
+@RequestMapping("/api")
+public class HealthcheckResource {
+
+    @GetMapping("/healthcheck")
+    public String healthcheck() {
+        return "Orchestrator is running";
+    }
+}
