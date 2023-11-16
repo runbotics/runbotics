121a37f0524880faf52b8279def1f79d7528d028
Depcik <marcin.depcik@all-for-one.com>
2023-11-16 22:22:22 +0100

    fix(runbotics-scheduler): :bug: Remove Map of socket ids and improve method to acknowledge socket emit

--- runbotics/runbotics-scheduler/src/websocket/websocket.service.ts
+++ runbotics/runbotics-scheduler/src/websocket/websocket.service.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { BotWebSocketGateway } from './bot/bot.gateway';
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { BotService } from '#/database/bot/bot.service';
 import { BotWsMessage } from 'runbotics-common';
 import { hasErrorMessage } from '#/utils/ws-acknowledgement';
+import { Logger } from '#/utils/logger';
 
 @Injectable()
 export class WebsocketService {
-    private readonly logger = new Logger(WebsocketService.name);
+    private readonly logger: Logger = new Logger(WebsocketService.name);
 
     constructor(
         private readonly botService: BotService,
@@ -23,38 +24,23 @@ export class WebsocketService {
             throw new NotFoundException(`Bot ${botId} does not exist`);
         }
 
-        const connectedBotsCount = this.botWebSocketGateway.connectedBotsCount;
-        if (!connectedBotsCount) {
-            this.logger.error('There are no connected bots');
-            throw new NotFoundException('There are no connected bots');
-        }
-
-        const socketId = this.botWebSocketGateway.getConnectedBotSocketId(botId);
-        if (!socketId) {
-            this.logger.error(`There is no connection with bot ${botId}`);
-            throw new NotFoundException(`There is no connection with bot ${botId}`);
-        }
-
         try {
-            await this.send(socketId, message, body);
+            await this.send(bot.installationId, message, body);
         } catch (error: any) {
-            throw new Error(error);
+            this.logger.error(error);
+            throw error;
         }
     }
 
-    private send(socketId: string, message: BotWsMessage, body?: any) {
-        const socket = this.botWebSocketGateway.server.sockets.sockets.get(socketId);
+    private async send(installationId: string, message: BotWsMessage, body?: any) {
+        const server = this.botWebSocketGateway.server;
+        const response = await server
+            .in(installationId)
+            .timeout(5000)
+            .emitWithAck(message, body);
 
-        return new Promise((resolve, reject) => {
-            const handlePromise = (response) => {
-                hasErrorMessage(response)
-                    ? reject(response.errorMessage)
-                    : resolve(message);
-            };
-
-            socket.emit(message, body, (response) => {
-                handlePromise(response);
-            });
-        });
+        if (hasErrorMessage(response[0])) {
+            throw new Error(response[0].errorMessage);
+        }
     }
 }
