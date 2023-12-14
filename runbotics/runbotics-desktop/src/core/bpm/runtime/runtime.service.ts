2ba29e94cc77d72193b7a573b543f19129983aed
Damian Litwin <damian.litwin@all-for-one.com>
2023-12-14 10:42:53 +0100

    Bugfix(runbotics-desktop): [RPA-1168] removed method to debug

--- runbotics/runbotics-desktop/src/core/bpm/runtime/runtime.service.ts
+++ runbotics/runbotics-desktop/src/core/bpm/runtime/runtime.service.ts
@@ -280,24 +280,6 @@ export class RuntimeService implements OnApplicationBootstrap, OnModuleDestroy {
                 });
             }
         });
-        
-        //todo remove it
-        function stringify(obj) {
-            let cache = [];
-            let str = JSON.stringify(obj, function(key, value) {
-                if (typeof value === "object" && value !== null) {
-                    if (cache.indexOf(value) !== -1) {
-                        // Circular reference found, discard key
-                        return;
-                    }
-                    // Store value in our collection
-                    cache.push(value);
-                }
-                return value;
-            });
-            cache = null; // reset the cache
-            return str;
-        }
 
         // @ts-ignore
         engine.once('error', (error) => {
