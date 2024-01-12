90b4a197fc996b05984d17ba6ef5b29b15d81d95
Depcik <marcin.depcik@all-for-one.com>
2024-01-12 16:33:43 +0100

    fix(runbotics-orchestrator-ui): :bug: Fix selecting bot collections

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/bot/BotBrowseView/BotBrowseView.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/bot/BotBrowseView/BotBrowseView.tsx
@@ -8,8 +8,6 @@ import If from '#src-app/components/utils/If';
 
 import useFeatureKey from '#src-app/hooks/useFeatureKey';
 
-import useQuery from '#src-app/hooks/useQuery';
-
 import useTranslations from '#src-app/hooks/useTranslations';
 
 import { StyledContainer, StyledPage } from './BotBrowseView.styles';
@@ -26,8 +24,8 @@ const BotBrowseView: VFC = () => {
     const router = useRouter();
     const tab = router.asPath.split('/').slice(-1)[0].split('?')[0];
     const { translate } = useTranslations();
-    const query = useQuery();
-    const collectionId = query.get('collection');
+    const collectionIdQueryParam = router.query['collection'];
+    const collectionId = typeof collectionIdQueryParam === 'string' ? collectionIdQueryParam : null;
     const hasBotsCollectionTabAccess = useFeatureKey([FeatureKey.BOT_COLLECTION_READ]);
     const hasBotsTabAccess = useFeatureKey([FeatureKey.BOT_READ]);
 
