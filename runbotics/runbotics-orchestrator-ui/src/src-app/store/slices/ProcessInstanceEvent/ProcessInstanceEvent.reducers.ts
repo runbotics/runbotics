c1b02b0c826fb04777688f879b57c9710ead138d
Patryk Jakubowski <48670422+bocian1337@users.noreply.github.com>
2024-02-02 16:07:47 +0100

    [RPA-1297] Sent info about loop events to frontend, handle them in redux

--- runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/ProcessInstanceEvent/ProcessInstanceEvent.reducers.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/ProcessInstanceEvent/ProcessInstanceEvent.reducers.ts
@@ -2,6 +2,7 @@ import { PayloadAction } from '@reduxjs/toolkit';
 
 import { initialState } from './ProcessInstanceEvent.slice';
 import { ProcessInstanceEventState } from './ProcessInstanceEvent.state';
+import { IProcessInstanceLoopEvent } from 'runbotics-common';
 
 export const resetAll = (state: ProcessInstanceEventState) => {
     state.all = initialState.all;
@@ -17,4 +18,24 @@ export const reduceCrumbs = (
         0,
         index + 1
     );
+};
+
+export const updateSingleActiveLoopEvent = (state: ProcessInstanceEventState, action: PayloadAction<IProcessInstanceLoopEvent>) => {
+    if (state.all.nestedEvents[action.payload.loopId]) {
+        if (!state.all.nestedEvents[action.payload.loopId][action.payload.iterationNumber]) {
+            state.all.nestedEvents[action.payload.loopId][action.payload.iterationNumber] = [action.payload];
+            return;
+        }
+
+        if (state.all.nestedEvents[action.payload.loopId][action.payload.iterationNumber].some((loopEvent) => loopEvent.id === action.payload.id)) {
+            state.all.nestedEvents[action.payload.loopId][action.payload.iterationNumber] = state.all.nestedEvents[action.payload.loopId][action.payload.iterationNumber]
+                .map((loopEvent) => {
+                    if (loopEvent.id === action.payload.id) return action.payload;
+                    return loopEvent;
+                });
+            return;
+        }
+
+        state.all.nestedEvents[action.payload.loopId][action.payload.iterationNumber].push(action.payload);
+    }
 };
