c1cb05e99619db39e2b23e470a6b60ac595f5e35
Anna Gąsiorowska <80831299+Aeri4a@users.noreply.github.com>
2024-02-09 10:47:32 +0100

    [RPA-1263] Fetch process collections from base collection (#555)
    * feat(runbotics-orchestrator): :sparkles: added endpoint to get process collections
    
    * feat(runbotics-orchestrator): :sparkles: added 404 endpoint return if empty list
    
    * refactor(runbotics-orchestrator): :recycle: added index on parent_id & function rename
    
    * chore: :pushpin: bump app version to 2.10.0-SNAPSHOT.9
    
    ---------
    
    Co-authored-by: Anna <anna.gasiorowska@all-for-one.com>

--- runbotics-orchestrator/src/main/java/com/runbotics/service/ProcessCollectionService.java
+++ runbotics-orchestrator/src/main/java/com/runbotics/service/ProcessCollectionService.java
@@ -1,7 +1,12 @@
 package com.runbotics.service;
 
+import com.runbotics.service.criteria.ProcessCollectionCriteria;
 import com.runbotics.service.dto.ProcessCollectionDTO;
 
+import java.util.List;
+
 public interface ProcessCollectionService {
+
+    List<ProcessCollectionDTO> getCollectionsByCriteria(ProcessCollectionCriteria criteria);
     ProcessCollectionDTO save(ProcessCollectionDTO ProcessCollectionDTO);
 }
