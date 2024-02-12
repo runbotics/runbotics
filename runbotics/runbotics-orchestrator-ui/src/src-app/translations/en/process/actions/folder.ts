cf239d192e7862eebfbd78cd4bb1e0f1df3b9fbc
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2024-02-12 12:24:50 +0100

    Adds translations

--- runbotics/runbotics-orchestrator-ui/src/src-app/translations/en/process/actions/folder.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/translations/en/process/actions/folder.ts
@@ -10,6 +10,11 @@ const folderActionsTranslations = {
     'Process.Details.Modeler.Actions.Folder.Create.Name': 'Name',
     'Process.Details.Modeler.Actions.Folder.Create.Name.Info': 'New folder name',
     'Process.Details.Modeler.Actions.Folder.Create.Output.Info': 'Save path of the newly created folder to a variable',
+    'Process.Details.Modeler.Actions.Folder.Rename.Label': 'Rename',
+    'Process.Details.Modeler.Actions.Folder.Rename.Path.Info': 'Path, e.g. C:\\Documents\\OldFolderName',
+    'Process.Details.Modeler.Actions.Folder.Rename.NewName': 'New name',
+    'Process.Details.Modeler.Actions.Folder.Rename.NewName.Info': 'New name, e.g. NewFolderName',
+    'Process.Details.Modeler.Actions.Folder.Rename.Output.Info': 'Save path of the renamed folder to a variable',
     'Process.Details.Modeler.Actions.Folder.Path': 'Path',
     'Process.Details.Modeler.Actions.Folder.Path.Info': 'Path to directory where you want to create a new folder',
 };
--- runbotics/runbotics-orchestrator-ui/src/src-app/translations/pl/process/actions/folder.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/translations/pl/process/actions/folder.ts
@@ -12,6 +12,11 @@ const folderActionsTranslations: typeof englishFolderActionsTranslations = {
     'Process.Details.Modeler.Actions.Folder.Create.Name': 'Nazwa',
     'Process.Details.Modeler.Actions.Folder.Create.Name.Info': 'Nazwa folderu do utworzenia',
     'Process.Details.Modeler.Actions.Folder.Create.Output.Info': 'Zapisz ścieżkę do utworzonego folderu jako zmienną',
+    'Process.Details.Modeler.Actions.Folder.Rename.Label': 'Zmień nazwę',
+    'Process.Details.Modeler.Actions.Folder.Rename.NewName': 'Nowa nazwa',
+    'Process.Details.Modeler.Actions.Folder.Rename.NewName.Info': 'Nowa nazwa, np. NowaNazwaFolderu',
+    'Process.Details.Modeler.Actions.Folder.Rename.Path.Info': 'Ścieżka do folderu wraz z nazwą foldery, np. C:\\MojeDokumenty\\StaraNazwaFolderu',
+    'Process.Details.Modeler.Actions.Folder.Rename.Output.Info': 'Zapisz ścieżkę do folderu jako zmienną',
     'Process.Details.Modeler.Actions.Folder.Path': 'Ścieżka',
     'Process.Details.Modeler.Actions.Folder.Path.Info': 'Ścieżka do nadrzędnego folderu w hierarchii plików',
 };
