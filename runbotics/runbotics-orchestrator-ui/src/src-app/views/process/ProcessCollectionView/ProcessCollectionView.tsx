9a91864246009d32f0e63326f276cad8356f244e
Erwin <erwin.majewski@all-for-one.com>
2024-02-13 10:10:56 +0100

    Fix locales, add isLoading

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessCollectionView/ProcessCollectionView.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessCollectionView/ProcessCollectionView.tsx
@@ -1,20 +1,28 @@
 import { Box } from '@mui/material';
 
+import { useSelector } from 'react-redux';
+
 import InternalPage from '../../../components/pages/InternalPage';
 import ProcessCollectionList from '../../../components/Tile/ProcessCollectionTile/ProcessCollectionList';
+import LoadingScreen from '../../../components/utils/LoadingScreen';
 import useQuery from '../../../hooks/useQuery';
+import { processCollectionSelector } from '../../../store/slices/ProcessCollection';
 import Header, { ProcessesTabs } from '../ProcessBrowseView/Header';
 
 export const ProcessCollectionView = () => {
     const { firstValueFrom } = useQuery();
+    const { isLoading } = useSelector(processCollectionSelector).childrenCollections;
     const collectionId = firstValueFrom(ProcessesTabs.COLLECTIONS);
 
     return (
         <InternalPage title="Processes Collections">
             <Header />
-            <Box pt={6}>
-                <ProcessCollectionList />
-            </Box>
+            {isLoading ?
+                <LoadingScreen /> : (
+                    <Box pt={6}>
+                        <ProcessCollectionList />
+                    </Box>
+                )}
         </InternalPage>
     );
 };
