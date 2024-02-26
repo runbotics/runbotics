245dab98cf6eb6992537718bba5abede4850a8b0
preludium <szymczak.mikolaj08@gmail.com>
2024-02-26 16:41:01 +0100

    fix(runbotics-orchestrator-ui): :bug: Adjust zip action translation

--- runbotics/runbotics-orchestrator-ui/src/src-app/translations/en/process/actions/zip.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/translations/en/process/actions/zip.ts
@@ -1,9 +1,9 @@
 const zipTranslations = {
-    'Process.Details.Modeler.Actions.Zip.Unzip.Label': 'Unzip archive',
-    'Process.Details.Modeler.Actions.Zip.Unzip.Path': 'Path',
-    'Process.Details.Modeler.Actions.Zip.Unzip.Name': 'Name',
-    'Process.Details.Modeler.Actions.Zip.Unzip.Path.Info': 'Path of folder containing zip file, e.g. "C:\\Users\\User\\Documents".',
-    'Process.Details.Modeler.Actions.Zip.Unzip.Name.Info': 'File name (without extension), e.g. "zippedArchive".'
+  'Process.Details.Modeler.Actions.Zip.UnzipFile.Label': 'Unzip archive',
+  'Process.Details.Modeler.Actions.Zip.Unzip.Path': 'Path',
+  'Process.Details.Modeler.Actions.Zip.Unzip.Name': 'Name',
+  'Process.Details.Modeler.Actions.Zip.Unzip.Path.Info': 'Path of folder containing zip file, e.g. "C:\\Users\\User\\Documents".',
+  'Process.Details.Modeler.Actions.Zip.Unzip.Name.Info': 'File name (without extension), e.g. "zippedArchive".'
 };
 
 export default zipTranslations;
