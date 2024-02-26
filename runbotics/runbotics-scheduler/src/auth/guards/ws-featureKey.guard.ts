4f0e0b72c2af78a951dd3ea9fd2811161e7e12c8
Patryk Jakubowski <48670422+bocian1337@users.noreply.github.com>
2024-02-26 15:39:07 +0100

    [RPA-1319] Added websockets feature key guard

---
+++ runbotics/runbotics-scheduler/src/auth/guards/ws-featureKey.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { AuthService } from '../auth.service';
+import { FeatureKey } from 'runbotics-common';
+import { FEATURE_KEY } from '../featureKey.decorator';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class WsFeatureKeyGuard implements CanActivate {
+    @Inject() readonly reflector: Reflector;
+    constructor(private authService: AuthService) { }
+
+    async canActivate(context: ExecutionContext) {
+        const client = context.switchToWs().getClient<Socket>();
+
+        const user = await this.authService.validateWebsocketConnection(client);
+
+        const requiredKeys = this.reflector.getAllAndOverride<FeatureKey[] | undefined>(FEATURE_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]) ?? [];
+
+        const featureKeys = [...new Set([...requiredKeys])];
+
+        const userKeys = user.authorities.flatMap((auth) => auth.featureKeys).map((featureKey) => featureKey.name);
+
+        return featureKeys.every((key) => userKeys.includes(key));
+    }
+}
