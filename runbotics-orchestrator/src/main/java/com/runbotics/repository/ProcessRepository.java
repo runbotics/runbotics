8ba6c79be28b017617808952a5db03a20e0e2657
Anna <anna.gasiorowska@all-for-one.com>
2024-03-12 10:13:26 +0100

    refactor(runbotics-orchestrator): :recycle: removed old queries from process repository

--- runbotics-orchestrator/src/main/java/com/runbotics/repository/ProcessRepository.java
+++ runbotics-orchestrator/src/main/java/com/runbotics/repository/ProcessRepository.java
@@ -3,7 +3,6 @@ package com.runbotics.repository;
 import com.runbotics.domain.Process;
 
 import java.util.List;
-import java.util.UUID;
 
 import com.runbotics.domain.User;
 import org.springframework.data.domain.Page;
@@ -39,82 +38,6 @@ public interface ProcessRepository extends JpaRepository<Process, Long>, JpaSpec
         "select process from Process process where process.botCollection.name like %?1% and (process.createdBy.login = ?2 or process.isPublic = true)"
     )
     Page<Process> findByBotCollection(String collectionName, String userLogin, Pageable pageable);
-
-    @Query(value =
-        "SELECT p.* FROM process p " +
-            "LEFT JOIN tag_process tp ON p.id = tp.process_id " +
-            "LEFT JOIN tag t ON tp.tag_id = t.id " +
-            "LEFT JOIN jhi_user u ON u.id = p.created_by_id " +
-            "WHERE (p.name iLIKE %:name% OR t.name iLIKE %:tagName% OR u.email iLIKE %:createdByName%) " +
-            "GROUP BY p.id"
-        , nativeQuery = true
-    )
-    Page<Process> findBySearchForAdmin2(String name, String tagName, String createdByName, Pageable pageable);
-
-    @Query(value =
-        "SELECT p.* FROM process p " +
-            "LEFT JOIN tag_process tp ON p.id = tp.process_id " +
-            "LEFT JOIN tag t ON tp.tag_id = t.id " +
-            "LEFT JOIN jhi_user u ON u.id = p.created_by_id " +
-            "WHERE u.id NOT IN (SELECT g.user_id FROM guest g) " +
-            "AND (p.created_by_id = :id OR p.is_public = true) " +
-            "AND (p.name iLIKE %:name% OR t.name iLIKE %:tagName% OR u.email iLIKE %:createdByName%) " +
-            "GROUP BY p.id"
-        , nativeQuery = true
-    )
-    Page<Process> findBySearchForUser2(Long id, String name, String tagName, String createdByName, Pageable pageable);
-
-    @Query(value =
-        "SELECT p.* FROM process p " +
-            "LEFT JOIN tag_process tp ON p.id = tp.process_id " +
-            "LEFT JOIN tag t ON tp.tag_id = t.id " +
-            "LEFT JOIN jhi_user u ON u.id = p.created_by_id " +
-            "WHERE p.collection_id = :collectionId " +
-            "AND (p.name iLIKE %:name% OR t.name iLIKE %:tagName% OR u.email iLIKE %:createdByName%) " +
-            "GROUP BY p.id"
-        , nativeQuery = true
-    )
-    Page<Process> findBySearchAndCollectionForAdmin2(String name, String tagName, String createdByName, UUID collectionId, Pageable pageable);
-
-    @Query(value =
-        "SELECT p.* FROM process p " +
-            "LEFT JOIN tag_process tp ON p.id = tp.process_id " +
-            "LEFT JOIN tag t ON tp.tag_id = t.id " +
-            "LEFT JOIN jhi_user u ON u.id = p.created_by_id " +
-            "WHERE u.id NOT IN (SELECT g.user_id FROM guest g) " +
-            "AND (p.created_by_id = :id OR p.is_public = true) " +
-            "AND p.collection_id = :collectionId " +
-            "AND (p.name iLIKE %:name% OR t.name iLIKE %:tagName% OR u.email iLIKE %:createdByName%) " +
-            "GROUP BY p.id"
-        , nativeQuery = true
-    )
-    Page<Process> findBySearchAndCollectionForUser2(Long id, String name, String tagName, String createdByName, UUID collectionId, Pageable pageable);
-
-    @Query(value =
-        "SELECT p.* FROM process p " +
-            "LEFT JOIN tag_process tp ON p.id = tp.process_id " +
-            "LEFT JOIN tag t ON tp.tag_id = t.id " +
-            "LEFT JOIN jhi_user u ON u.id = p.created_by_id " +
-            "WHERE p.collection_id IS NULL " +
-            "AND (p.name iLIKE %:name% OR t.name iLIKE %:tagName% OR u.email iLIKE %:createdByName%) " +
-            "GROUP BY p.id"
-        , nativeQuery = true
-    )
-    Page<Process> findBySearchWithoutCollectionForAdmin2(String name, String tagName, String createdByName, Pageable pageable);
-
-    @Query(value =
-        "SELECT p.* FROM process p " +
-            "LEFT JOIN tag_process tp ON p.id = tp.process_id " +
-            "LEFT JOIN tag t ON tp.tag_id = t.id " +
-            "LEFT JOIN jhi_user u ON u.id = p.created_by_id " +
-            "WHERE u.id NOT IN (SELECT g.user_id FROM guest g) " +
-            "AND (p.created_by_id = :id OR p.is_public = true) " +
-            "AND p.collection_id IS NULL " +
-            "AND (p.name iLIKE %:name% OR t.name iLIKE %:tagName% OR u.email iLIKE %:createdByName%) " +
-            "GROUP BY p.id"
-        , nativeQuery = true
-    )
-    Page<Process> findBySearchWithoutCollectionForUser2(Long id, String name, String tagName, String createdByName, Pageable pageable);
 
     @Query(
         value = "SELECT COUNT(*) FROM Process process WHERE process.createdBy.id = ?1"
