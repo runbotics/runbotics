d1518a3aecdde839ac62b079a5e37940bf79442b
Depcik <marcin.depcik@all-for-one.com>
2024-03-13 08:50:41 +0100

    feat(runbotics-scheduler): :sparkles: Add queue-message service

---
+++ runbotics/runbotics-scheduler/src/queue/queue-message.service.ts
@@ -0,0 +1,50 @@
+import { Logger } from '#/utils/logger';
+import { Job } from '#/utils/process';
+import { Injectable } from '@nestjs/common';
+import Axios from 'axios';
+import {
+    BaseQueueMessageBody,
+    GeneralQueueMessageBody,
+    QueueMessageBody,
+    UpdateQueueMessageBody,
+} from 'runbotics-common';
+
+@Injectable()
+export class QueueMessageService {
+    private readonly logger = new Logger(QueueMessageService.name);
+
+    public async sendQueueMessage(eventType: GeneralQueueMessageBody['type'], job: Job): Promise<void>;
+    public async sendQueueMessage(eventType: UpdateQueueMessageBody['type'], job: Job, queuePosition: UpdateQueueMessageBody['queuePosition']): Promise<void>;
+    public async sendQueueMessage(eventType: QueueMessageBody['type'], job: Job, queuePosition?: number) {
+        const queueCallbackUrl = job?.data?.input?.queueCallbackUrl;
+        if (queueCallbackUrl) {
+            const baseQueueMessageBody = this.getBaseQueueMessageBody(job);
+
+            switch (eventType) {
+                case 'UPDATE':
+                    await Axios.post<unknown, unknown, QueueMessageBody>(queueCallbackUrl, {
+                        ...baseQueueMessageBody,
+                        type: eventType,
+                        queuePosition,
+                    });
+                    break;
+                default:
+                    await Axios.post<unknown, unknown, QueueMessageBody>(queueCallbackUrl, {
+                        ...baseQueueMessageBody,
+                        type: eventType,
+                    });
+                    break;
+            }
+        }
+    }
+
+    private getBaseQueueMessageBody(job: Job): BaseQueueMessageBody {
+        return {
+            initialDate: new Date(job?.timestamp).toISOString(),
+            updateDate: new Date().toISOString(),
+            input: job?.data?.input,
+            processId: job?.data?.process?.id,
+            processQueueId: job?.id,
+        };
+    }
+}
