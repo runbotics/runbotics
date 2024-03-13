8bffc87e966bfa277a8e0d9873cea00349ebe370
Depcik <marcin.depcik@all-for-one.com>
2024-03-13 08:54:34 +0100

    feat(runbotics-scheduler): :sparkles: Enable to update process instance status in UI via WS

--- runbotics/runbotics-scheduler/src/websocket/ui/ui.gateway.ts
+++ runbotics/runbotics-scheduler/src/websocket/ui/ui.gateway.ts
@@ -8,10 +8,12 @@ import { Server } from 'socket.io';
 import { AuthService } from '#/auth/auth.service';
 import { AuthSocket } from '#/types';
 import { Logger } from '#/utils/logger';
+import { IUser, ProcessQueueMessage } from 'runbotics-common';
 
 @WebSocketGateway({ path: '/ws-ui', cors: { origin: '*' } })
 export class UiGateway implements OnGatewayDisconnect, OnGatewayConnection {
     private logger: Logger = new Logger(UiGateway.name);
+    private uiSocketMap: Map<string, IUser> = new Map();
     @WebSocketServer() server: Server;
 
     constructor(
@@ -23,6 +25,7 @@ export class UiGateway implements OnGatewayDisconnect, OnGatewayConnection {
             this.logger.log(`Client ${client.id} is trying to establish connection`);
             const user = await this.authService.validateWebsocketConnection(client);
             this.logger.log(`Client connected: ${user.login} | ${client.id}`);
+            this.uiSocketMap.set(client.id, user);
         } catch (error) {
             this.logger.error(`Client ${client.id} failed to connect`, error);
         }
@@ -30,5 +33,29 @@ export class UiGateway implements OnGatewayDisconnect, OnGatewayConnection {
 
     handleDisconnect(client: AuthSocket) {
         this.logger.log(`Client disconnected: ${client.id}`);
+        this.uiSocketMap.delete(client.id);
+    }
+
+    emitByUserId<T extends keyof ProcessQueueMessage>(userId: IUser['id'], event: T, data: ProcessQueueMessage[T]) {
+        const clientId = this.getClientIdByUserId(userId);
+        if (!clientId) {
+            this.logger.error(`User with userId (${userId}) is not connected.`);
+            return;
+        }
+
+        const clientSocket = this.server.sockets.sockets.get(clientId);
+        if (!clientSocket) {
+            this.logger.error(`Client with clientId (${clientId}) is not connected.`);
+            return;
+        }
+
+        clientSocket.emit(event, data);
+    }
+
+    private getClientIdByUserId(userId: IUser['id']) {
+        const sockets = this.uiSocketMap.entries();
+        for (const [clientId, user] of sockets) {
+            if (user.id === userId) return clientId;
+        }
     }
 }
