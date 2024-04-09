7504ccafba1991268c91f9447ac2d2e5b8b33586
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2024-04-09 14:32:09 +0200

    Adds functionality where on ctrl+f searching phrase can be a variable

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/search/BpmnSearch.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/extensions/search/BpmnSearch.ts
@@ -1,6 +1,7 @@
+
 import getElementLabel from '#src-app/utils/getElementLabel';
 
-import { BPMNElement } from '../../helpers/elementParameters';
+import { BPMNElement, CamundaParameter, IBpmnSubProcessBusinessObject, ISequenceFlowBusinessObject, getParameterValue } from '../../helpers/elementParameters';
 
 interface Token {
     normal?: string;
@@ -31,25 +32,30 @@ export default class BpmnSearchProvider implements SearchProvider {
 
     public find(pattern: string): Result[] {
         const rootElement = this._canvas.getRootElement();
-        return this._elementRegistry
+
+        const foundVariablesInActivities = this._elementRegistry
+            .filter((element: BPMNElement) => element !== rootElement && hasVariables(element, pattern))
+            .map((element: BPMNElement) => ({
+                primaryTokens: [{ normal: `variable in: ${element.id.startsWith('Gateway') ? element.id : getElementLabel(element)}` }],
+                secondaryTokens: [],
+                element: element
+            }))
+            .sort((item: Result) => item + item.element.id);
+
+        const actions = this._elementRegistry
             .filter(
                 (element: BPMNElement) =>
-                    element !== rootElement && element.id.startsWith('Activity')
+                    (element !== rootElement && hasVariables(element, pattern)) || (rootElement && element.id.startsWith('Activity'))
             )
             .map((element: BPMNElement) => ({
                 primaryTokens: matchAndSplit(getElementLabel(element), pattern),
                 secondaryTokens: [],
                 element: element
             }))
-            .filter(
-                (item: Result) =>
-                    hasMatched(item.primaryTokens) ||
-                    hasMatched(item.secondaryTokens)
-            )
-            .sort(
-                (item: Result) =>
-                    getElementLabel(item.element) + item.element.id
-            );
+            .filter((item: Result) => hasMatched(item.primaryTokens) || hasMatched(item.secondaryTokens))
+            .sort((item: Result) => getElementLabel(item.element) + item.element.id);
+
+        return [...foundVariablesInActivities, ...actions];
     }
 }
 
@@ -57,6 +63,78 @@ const hasMatched = (tokens: Token[]): boolean => {
     const matched = tokens.filter((token: Token) => Boolean(token.matched));
 
     return matched.length > 0;
+};
+
+const hasVariables = (element: BPMNElement, pattern: string) => {
+    const lowerCasePattern = pattern.toLowerCase();
+    const businessObject = element.businessObject;
+    let variableFound = false;
+
+    if (element.id.startsWith('Gateway')) {
+        const gatewayOutgoingFlows: ISequenceFlowBusinessObject[] = (businessObject as unknown as BPMNElement)
+            .outgoing as unknown as ISequenceFlowBusinessObject[];
+
+        const gatewayExpressions = gatewayOutgoingFlows
+            .map(flow => flow.conditionExpression.body.toLowerCase())
+            .filter(expression => expression.includes(lowerCasePattern));
+
+        variableFound = gatewayExpressions.length > 0;
+    }
+
+    if (variableFound) {
+        return variableFound;
+    }
+    
+    if (businessObject.actionId === 'loop.loop') {
+        const loopVariable = (element.businessObject as IBpmnSubProcessBusinessObject).loopCharacteristics.loopCardinality.body;
+        variableFound = loopVariable.toLowerCase().includes(lowerCasePattern);
+    }
+
+    if (variableFound) {
+        return variableFound;
+    }
+
+    const extensionElements = businessObject.extensionElements;
+
+    if (!extensionElements) {
+        return variableFound;
+    }
+
+    const inputValues = extensionElements.values[0].inputParameters;
+    const outputValues = extensionElements.values[0].outputParameters;
+
+    if (inputValues && inputValues.length > 0) {
+        const inputVariable = inputValues.filter(
+            value =>
+                (value.name === 'variable' && value.value.toLowerCase().includes(lowerCasePattern)) ||
+                (value.name === 'value' && (value.value.startsWith('#{') || value.value.startsWith('${')) && value.value.toLowerCase().includes(lowerCasePattern))
+        );
+
+        const hashVariables = inputValues
+            .filter(value => value.name === 'variables' || value.name === 'functionParams')
+            .map((camundaParam: CamundaParameter) => Object.values(getParameterValue(camundaParam)))
+            .flatMap(arr => arr)
+            .filter((foundValue: string) => foundValue.toLowerCase().includes(lowerCasePattern));
+
+        variableFound = [...inputVariable, ...hashVariables].length > 0;
+    }
+
+    if (variableFound) {
+        return variableFound;
+    }
+
+    if (outputValues && outputValues.length > 0) {
+        const outputVariable = outputValues.filter(
+            value => value.name === 'variableName' && value.value.toLowerCase().includes(lowerCasePattern)
+        );
+        variableFound = outputVariable.length > 0;
+    }
+
+    if (variableFound) {
+        return variableFound;
+    }
+
+    return false;
 };
 
 const matchAndSplit = (text: string, pattern: string): Token[] => {
