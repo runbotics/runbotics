408a8f40d84680c4ecd65639065cb058a0edea31
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2024-05-07 10:17:35 +0200

    Mocked credentials tab

--- runbotics/runbotics-orchestrator-ui/src/pages/app/credentials/collections.tsx
+++ runbotics/runbotics-orchestrator-ui/src/pages/app/credentials/collections.tsx
@@ -1,6 +1,6 @@
 import { FeatureKey } from 'runbotics-common';
 
 import { withAuthGuard } from '#src-app/components/guards/AuthGuard';
-import CredentialsCollectionsView from '#src-app/views/credentials/CredenitalsCollection/CredentialsCollectionsView';
+import CredentialsCollectionsView from '#src-app/views/credentials/CredentialsCollection/CredentialsCollectionsView';
 
 export default withAuthGuard({ Component: CredentialsCollectionsView, featureKeys: [FeatureKey.CREDENTIALS_COLLECTIONS_READ] });
