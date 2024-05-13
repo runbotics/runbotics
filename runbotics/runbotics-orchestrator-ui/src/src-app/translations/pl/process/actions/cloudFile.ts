b4c16edb6b9cf5c6f6a6e4089f3b5d5935aabc3c
pilakw <wiktor.pilak@all-for-one.com>
2024-05-13 18:00:59 +0200

    [RPA-1507] Get list items

--- runbotics/runbotics-orchestrator-ui/src/src-app/translations/pl/process/actions/cloudFile.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/translations/pl/process/actions/cloudFile.ts
@@ -24,7 +24,7 @@ const cloudFileActionsTranslations: typeof englishCloudFileActionsTranslations =
     'Process.Details.Modeler.Actions.CloudFile.CreateShareLink.ShareType': 'Typ udostępnienia',
     'Process.Details.Modeler.Actions.CloudFile.CreateShareLink.ShareScope': 'Zakres udostępnienia',
 
-    'Process.Details.Modeler.Actions.CloudFile.GetSharePointList.Label': 'Pobierz listę Sharepoint',
+    'Process.Details.Modeler.Actions.CloudFile.GetSharePointList.Label': 'Pobierz elementy listy Sharepoint',
     'Process.Details.Modeler.Actions.CloudFile.GetSharePointList.Name': 'Nazwa listy',
 };
 
