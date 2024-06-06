63e19ff697f9ed7d394d338ff0d3f194d4813f84
Depcik <marcin.depcik@all-for-one.com>
2024-06-06 15:42:47 +0200

    fix(runbotics-orchestrator-ui): :bug: Fix ms cloud actions to work using siteRelativePath and add output to createShareLink

--- runbotics/runbotics-orchestrator-ui/src/src-app/translations/en/process/actions/microsoft.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/translations/en/process/actions/microsoft.ts
@@ -1,6 +1,8 @@
 const microsoftTranslations = {
     'Process.Details.Modeler.Actions.Microsoft.Platform': 'Platform',
     'Process.Details.Modeler.Actions.Microsoft.SiteName': 'Site',
+    'Process.Details.Modeler.Actions.Microsoft.SiteRelativePath': 'Site relative path',
+    'Process.Details.Modeler.Actions.Microsoft.SiteRelativePath.Info': 'Relative path of the site eg. "/sites/pl/SDD/DRO/RPA"',
     'Process.Details.Modeler.Actions.Microsoft.ListName': 'List',
 };
 
--- runbotics/runbotics-orchestrator-ui/src/src-app/translations/pl/process/actions/microsoft.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/translations/pl/process/actions/microsoft.ts
@@ -3,6 +3,8 @@ import englishMicrosoftActionsTranslations from '#src-app/translations/en/proces
 const microsoftTranslations: typeof englishMicrosoftActionsTranslations = {
     'Process.Details.Modeler.Actions.Microsoft.Platform': 'Platforma',
     'Process.Details.Modeler.Actions.Microsoft.SiteName': 'Strona',
+    'Process.Details.Modeler.Actions.Microsoft.SiteRelativePath': 'Ścieżka strony',
+    'Process.Details.Modeler.Actions.Microsoft.SiteRelativePath.Info': 'Ścieżka do strony np. "/sites/pl/SDD/DRO/RPA"',
     'Process.Details.Modeler.Actions.Microsoft.ListName': 'Lista',
 };
 
