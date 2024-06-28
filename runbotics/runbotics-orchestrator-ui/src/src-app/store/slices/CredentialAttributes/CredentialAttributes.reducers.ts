abea410e88f74d0a3bbc508759b757900cbb189e
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2024-06-28 16:40:59 +0200

    Updates store for CredentialAttribute and CredentialCollection

--- runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/CredentialAttributes/CredentialAttributes.reducers.ts
+++
@@ -1,9 +0,0 @@
-import { PayloadAction } from '@reduxjs/toolkit';
-
-import { EditAtributeDto } from '../../../views/credentials/Credential/EditCredential/CredentialAttribute/CredentialAttribute.types';
-
-export const updateAttribute = (state: EditAtributeDto, action: PayloadAction<EditAtributeDto>) => {
-    state.description = action.payload.description,
-    state.name = action.payload.description,
-    state.value = action.payload.value;
-};
