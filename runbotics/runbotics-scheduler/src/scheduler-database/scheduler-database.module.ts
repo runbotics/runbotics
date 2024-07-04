1cf328adcda203bfe66aedb8c4fdfaa01d4234b4
Stanisław Jarocki <s.jarocki354@gmail.com>
2024-07-04 12:28:18 +0200

    refactor(runbotics-scheduler): :recycle: combine two migration sources into one

--- runbotics/runbotics-scheduler/src/scheduler-database/scheduler-database.module.ts
+++ runbotics/runbotics-scheduler/src/scheduler-database/scheduler-database.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ServerConfigService } from '#/config/server-config';
 import { ConfigModule } from '#/config/config.module';
-import { SecretModule } from "#/scheduler-database/secret/secret.module";
+import { SecretModule } from '#/scheduler-database/secret/secret.module';
 import { ProcessContextModule } from '#/scheduler-database/process-context/process-context.module';
 import { ProcessContextSecretModule } from '#/scheduler-database/process-context-secret/process-context-secret.module';
+import { DatabaseModule } from '#/database/database.module';
 
 @Module({
     imports: [
@@ -28,6 +29,7 @@ import { ProcessContextSecretModule } from '#/scheduler-database/process-context
             },
             inject: [ServerConfigService],
         }),
+        DatabaseModule,
         SecretModule,
         ProcessContextModule,
         ProcessContextSecretModule,
