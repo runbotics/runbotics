5608d74696198a0a43d05c60e3078635f7a81087
Adrianna Suchomska <adrianna.suchomska@gmail.com>
2024-07-24 11:28:30 +0200

    refactor(runbotics-orchestrator-ui): :art: Changes after CR

--- runbotics/runbotics-orchestrator-ui/src/src-app/translations/pl/tenants/actions/modals.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/translations/pl/tenants/actions/modals.ts
@@ -4,9 +4,8 @@ const modalsActionsTranslations: typeof englishModalsActionsTranslations = {
     'Tenants.Actions.Modals.DeleteModal.TitleMessage': 'Czy na pewno chcesz usunąć następujące organizacje?',
     'Tenants.Actions.Modals.DeleteModal.Message.Success': 'Pomyślnie usunięto organizacje',
     'Tenants.Actions.Modals.DeleteModal.Message.Fail': 'Błąd podczas usuwania organizacji',
-    'Tenants.Actions.Modals.DeleteModal.Message.Fail.BadTenantID': 'Nie można znaleźć klienta o podanym identyfikatorze',
-    'Tenants.Actions.Modals.DeleteModal.Message.Fail.RelatedTenant': 'Nie można usunąć klienta powiązanego z innymi zasobami',
-
+    'Tenants.Actions.Modals.DeleteModal.Message.Fail.BadTenantID': 'Nie można znaleźć organizacji o podanym identyfikatorze',
+    'Tenants.Actions.Modals.DeleteModal.Message.Fail.RelatedTenant': 'Nie można usunąć organizacji powiązanej z innymi zasobami',
     'Tenants.Actions.Modals.CreateModal.TitleMessage': 'Dodaj organizację',
     'Tenants.Actions.Modals.CreateModal.Field.Name': 'Nazwa',
     'Tenants.Actions.Modals.CreateModal.Button.Create': 'Stwórz',
