229cf939f9a8288d37d0669c6b3c3fcd9af797d2
Anna <anna.gasiorowska@all-for-one.com>
2024-08-05 16:12:05 +0200

    refactor(runbotics-scheduler): :recycle: added min length of name for global variable

--- runbotics/runbotics-scheduler/src/scheduler-database/global-variable/dto/create-global-variable.dto.ts
+++ runbotics/runbotics-scheduler/src/scheduler-database/global-variable/dto/create-global-variable.dto.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { GlobalVariableType } from 'runbotics-common';
 
 export const createGlobalVariableSchema = z.object({
-    name: z.string().max(255),
+    name: z.string().trim().min(1).max(255),
     value: z.string(),
     type: z.nativeEnum(GlobalVariableType),
     description: z.string().max(255).optional()
