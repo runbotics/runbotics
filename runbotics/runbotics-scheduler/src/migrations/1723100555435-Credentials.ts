3659e57056a3b51b3e6cf7e3b51628f0023e1a01
Stanisław Jarocki <s.jarocki354@gmail.com>
2024-08-08 09:07:34 +0200

    fix(runbotics-scheduler): :card_file_box: adjust credential migrations after rebase

--- runbotics/runbotics-scheduler/src/migrations/1723100555435-Credentials.ts
+++ runbotics/runbotics-scheduler/src/migrations/1723100555435-Credentials.ts
@@ -1,22 +1,18 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Credentials1722932873862 implements MigrationInterface {
-    name = 'Credentials1722932873862'
+export class Credentials1723100555435 implements MigrationInterface {
+    name = 'Credentials1723100555435'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "process_global_variable" DROP CONSTRAINT "fk_process_global_variable_process_id"`);
-        await queryRunner.query(`ALTER TABLE "process_global_variable" DROP CONSTRAINT "fk_process_global_variable_global_variable_id"`);
         await queryRunner.query(`CREATE TABLE "scheduler"."credential_template_attribute" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "description" character varying, "template_id" uuid NOT NULL, CONSTRAINT "UQ_1b70895f87cfcec88f2e9b2a0c4" UNIQUE ("name", "template_id"), CONSTRAINT "PK_fb9643ff7794611a6abe693c3be" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "scheduler"."credential_template" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "description" character varying, CONSTRAINT "UQ_8363f48a7697a4ee6e41f584b87" UNIQUE ("name"), CONSTRAINT "PK_94262abd8352fd19e4c500fff82" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "scheduler"."credential_collection_user_privilege_type_enum" AS ENUM('READ', 'WRITE')`);
         await queryRunner.query(`CREATE TABLE "scheduler"."credential_collection_user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "collection_id" uuid NOT NULL, "user_id" bigint NOT NULL, "privilege_type" "scheduler"."credential_collection_user_privilege_type_enum" NOT NULL DEFAULT 'WRITE', CONSTRAINT "PK_6031e489fe209c6475d5d375f57" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "scheduler"."credential_collection_access_type_enum" AS ENUM('PRIVATE', 'GROUP')`);
-        await queryRunner.query(`CREATE TYPE "scheduler"."credential_collection_color_enum" AS ENUM('ORANGE', 'YELLOW', 'BLUE', 'GREEN', 'RED')`);
-        await queryRunner.query(`CREATE TABLE "scheduler"."credential_collection" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "tenant_id" uuid NOT NULL, "description" character varying, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "created_by_id" bigint NOT NULL, "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_by_id" bigint NOT NULL, "access_type" "scheduler"."credential_collection_access_type_enum" NOT NULL DEFAULT 'PRIVATE', "color" "scheduler"."credential_collection_color_enum" NOT NULL DEFAULT 'ORANGE', CONSTRAINT "UQ_c712ddc22fb9bcd2c85d1579af5" UNIQUE ("name", "tenant_id", "created_by_id"), CONSTRAINT "PK_8928d022104b6c32bc3923e12ea" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE "scheduler"."credential_collection_color_enum" AS ENUM('LIGHT_ORANGE', 'DARK_ORANGE', 'LIGHT_GREEN', 'DARK_GREEN', 'LIGHT_BLUE', 'DARK_BLUE', 'LIGHT_GREY', 'DARK_GREY')`);
+        await queryRunner.query(`CREATE TABLE "scheduler"."credential_collection" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "tenant_id" uuid NOT NULL, "description" character varying, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "created_by_id" bigint NOT NULL, "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_by_id" bigint NOT NULL, "access_type" "scheduler"."credential_collection_access_type_enum" NOT NULL DEFAULT 'PRIVATE', "color" "scheduler"."credential_collection_color_enum" NOT NULL DEFAULT 'LIGHT_ORANGE', CONSTRAINT "UQ_c712ddc22fb9bcd2c85d1579af5" UNIQUE ("name", "tenant_id", "created_by_id"), CONSTRAINT "PK_8928d022104b6c32bc3923e12ea" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "scheduler"."credential" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "tenant_id" uuid NOT NULL, "description" character varying, "collection_id" uuid NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "created_by_id" bigint NOT NULL, "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_by_id" bigint NOT NULL, "template_id" uuid NOT NULL, CONSTRAINT "UQ_afc42c6ae4fa2eaf78ef7ad0145" UNIQUE ("collection_id", "name", "tenant_id"), CONSTRAINT "PK_3a5169bcd3d5463cefeec78be82" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "scheduler"."credential_attribute" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "tenant_id" uuid NOT NULL, "description" character varying, "masked" boolean NOT NULL, "secret_id" uuid NOT NULL, "credential_id" uuid NOT NULL, CONSTRAINT "UQ_0040a5c98f673bbdb4ef8f35a87" UNIQUE ("name", "credential_id"), CONSTRAINT "REL_10dfcac0476d35e610b4ef31fa" UNIQUE ("secret_id"), CONSTRAINT "PK_1751f2a8037133943ca06ac21e4" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE INDEX "IDX_a5c44b5b12113dbd973043fb6b" ON "process_global_variable" ("process_id") `);
-        await queryRunner.query(`CREATE INDEX "IDX_b6660b0a9bcd79f1fe11e34ac4" ON "process_global_variable" ("global_variable_id") `);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_template_attribute" ADD CONSTRAINT "FK_c19a33d4662595010e94f69a85d" FOREIGN KEY ("template_id") REFERENCES "scheduler"."credential_template"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_collection_user" ADD CONSTRAINT "FK_4d667ee9c8f087bbeaa5744fc00" FOREIGN KEY ("collection_id") REFERENCES "scheduler"."credential_collection"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_collection_user" ADD CONSTRAINT "FK_14c0e239df3748b2a116b22ba9b" FOREIGN KEY ("user_id") REFERENCES "jhi_user"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
@@ -31,13 +27,9 @@ export class Credentials1722932873862 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_attribute" ADD CONSTRAINT "FK_1a441188b5c83ce83ec267761e0" FOREIGN KEY ("tenant_id") REFERENCES "tenant"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_attribute" ADD CONSTRAINT "FK_10dfcac0476d35e610b4ef31faa" FOREIGN KEY ("secret_id") REFERENCES "scheduler"."secret"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_attribute" ADD CONSTRAINT "FK_c70d5eab619f137e2053e371456" FOREIGN KEY ("credential_id") REFERENCES "scheduler"."credential"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "process_global_variable" ADD CONSTRAINT "FK_a5c44b5b12113dbd973043fb6ba" FOREIGN KEY ("process_id") REFERENCES "process"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE "process_global_variable" ADD CONSTRAINT "FK_b6660b0a9bcd79f1fe11e34ac4a" FOREIGN KEY ("global_variable_id") REFERENCES "global_variable"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "process_global_variable" DROP CONSTRAINT "FK_b6660b0a9bcd79f1fe11e34ac4a"`);
-        await queryRunner.query(`ALTER TABLE "process_global_variable" DROP CONSTRAINT "FK_a5c44b5b12113dbd973043fb6ba"`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_attribute" DROP CONSTRAINT "FK_c70d5eab619f137e2053e371456"`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_attribute" DROP CONSTRAINT "FK_10dfcac0476d35e610b4ef31faa"`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_attribute" DROP CONSTRAINT "FK_1a441188b5c83ce83ec267761e0"`);
@@ -52,8 +44,6 @@ export class Credentials1722932873862 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_collection_user" DROP CONSTRAINT "FK_14c0e239df3748b2a116b22ba9b"`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_collection_user" DROP CONSTRAINT "FK_4d667ee9c8f087bbeaa5744fc00"`);
         await queryRunner.query(`ALTER TABLE "scheduler"."credential_template_attribute" DROP CONSTRAINT "FK_c19a33d4662595010e94f69a85d"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_b6660b0a9bcd79f1fe11e34ac4"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_a5c44b5b12113dbd973043fb6b"`);
         await queryRunner.query(`DROP TABLE "scheduler"."credential_attribute"`);
         await queryRunner.query(`DROP TABLE "scheduler"."credential"`);
         await queryRunner.query(`DROP TABLE "scheduler"."credential_collection"`);
@@ -63,8 +53,6 @@ export class Credentials1722932873862 implements MigrationInterface {
         await queryRunner.query(`DROP TYPE "scheduler"."credential_collection_user_privilege_type_enum"`);
         await queryRunner.query(`DROP TABLE "scheduler"."credential_template"`);
         await queryRunner.query(`DROP TABLE "scheduler"."credential_template_attribute"`);
-        await queryRunner.query(`ALTER TABLE "process_global_variable" ADD CONSTRAINT "fk_process_global_variable_global_variable_id" FOREIGN KEY ("global_variable_id") REFERENCES "global_variable"("id") ON DELETE RESTRICT ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE "process_global_variable" ADD CONSTRAINT "fk_process_global_variable_process_id" FOREIGN KEY ("process_id") REFERENCES "process"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
 }
