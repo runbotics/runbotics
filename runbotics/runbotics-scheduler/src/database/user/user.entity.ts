ce8064671d42e5b2eac6df206cda94fcb465cbcb
Depcik <marcin.depcik@all-for-one.com>
2024-08-22 15:42:44 +0200

    fix(runbotics-desktop): :bug: Fix many to many relations with user entity

--- runbotics/runbotics-scheduler/src/database/user/user.entity.ts
+++ runbotics/runbotics-scheduler/src/database/user/user.entity.ts
@@ -1,5 +1,5 @@
 import { IAuthority, IUser } from 'runbotics-common';
-import { Entity, Column, PrimaryColumn, ManyToMany, JoinTable, OneToMany, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, Column, PrimaryColumn, ManyToMany, JoinTable, OneToMany } from 'typeorm';
 import { dateTransformer, numberTransformer } from '../database.utils';
 import { Tenant } from '#/scheduler-database/tenant/tenant.entity';
 import { Credential } from '#/scheduler-database/credential/credential.entity';
@@ -78,7 +78,7 @@ export class UserEntity implements IUser {
     @OneToMany(
         () => CredentialCollectionUser,
         (credentialCollectionUser) =>
-            credentialCollectionUser.userId,
+            credentialCollectionUser.user,
     )
     credentialCollectionUser: CredentialCollectionUser[];
 }
