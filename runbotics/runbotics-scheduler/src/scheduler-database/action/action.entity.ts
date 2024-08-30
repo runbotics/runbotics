6359016a694517813e95d6aa84fa159597fa2a1c
Anna <anna.gasiorowska@all-for-one.com>
2024-08-30 11:14:38 +0200

    feat(runbotics-scheduler): :sparkles: added tenant id to action

--- runbotics/runbotics-scheduler/src/scheduler-database/action/action.entity.ts
+++ runbotics/runbotics-scheduler/src/scheduler-database/action/action.entity.ts
@@ -1,7 +1,9 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, Unique } from 'typeorm';
+import { Tenant } from '../tenant/tenant.entity';
 
 
 @Entity({ name: 'action' })
+@Unique(['tenantId', 'id'])
 export class Action {
     @PrimaryColumn({ type: 'varchar', length: 255 })
     id: string;
@@ -14,4 +16,11 @@ export class Action {
 
     @Column({ type: 'varchar', length: 255 })
     script: string;
+
+    @Column({ name: 'tenant_id', type: 'uuid', default: 'b7f9092f-5973-c781-08db-4d6e48f78e98' })
+    tenantId: string;
+
+    @ManyToOne(() => Tenant, tenant => tenant.id, { nullable: false })
+    @JoinColumn({ name: 'tenant_id' })
+    tenant: Tenant;
 }
