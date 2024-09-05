16631203d76c528ad855bd44a3c672b18f8f7b27
Depcik <marcin.depcik@all-for-one.com>
2024-09-05 10:59:59 +0200

    feat: :sparkles: Update types

--- runbotics/runbotics-sdk/src/handler/DesktopRunRequest.ts
+++ runbotics/runbotics-sdk/src/handler/DesktopRunRequest.ts
@@ -8,6 +8,15 @@ interface CommonDesktopRunRequest {
     trigger: { name: string };
     parentProcessInstanceId?: string;
     triggerData?: unknown;
+    credentials?: {
+        name: string;
+        template: string;
+        attributes: {
+            id: string;
+            name: string;
+            value: string;
+        }[];
+    }[];
 }
 
 export interface DesktopRunRequest<S extends string = string, I = any> extends CommonDesktopRunRequest {
