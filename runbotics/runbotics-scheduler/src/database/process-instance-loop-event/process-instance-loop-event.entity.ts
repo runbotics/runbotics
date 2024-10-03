28a10e92e467a63733638e2f03e67d487209188a
Depcik <marcin.depcik@all-for-one.com>
2024-10-03 15:36:36 +0200

    feat(runbotics-scheduler): :sparkles: Add process instance loop event migration

--- runbotics/runbotics-scheduler/src/database/process-instance-loop-event/process-instance-loop-event.entity.ts
+++ runbotics/runbotics-scheduler/src/database/process-instance-loop-event/process-instance-loop-event.entity.ts
@@ -1,52 +1,52 @@
-import { Entity, Column, ManyToOne, JoinColumn, Generated, PrimaryColumn } from 'typeorm';
-import { ProcessInstanceEntity } from '../process-instance/process-instance.entity';
-import { IProcessInstance, IProcessInstanceLoopEvent, ProcessInstanceEventStatus } from 'runbotics-common';
-import { dateTransformer, numberTransformer } from '../database.utils';
+// import { Entity, Column, ManyToOne, JoinColumn, Generated, PrimaryColumn } from 'typeorm';
+// import { IProcessInstance, IProcessInstanceLoopEvent, ProcessInstanceEventStatus } from 'runbotics-common';
+// import { dateTransformer, numberTransformer } from '../database.utils';
+// import { ProcessInstance } from '#/scheduler-database/process-instance/process-instance.entity';
 
-@Entity({ name: 'process_instance_loop_event', synchronize: false })
-export class ProcessInstanceLoopEventEntity implements IProcessInstanceLoopEvent {
-    @Generated()
-    @PrimaryColumn({ type: 'bigint', transformer: numberTransformer })
-        id: number;
+// @Entity({ name: 'process_instance_loop_event', synchronize: false })
+// export class ProcessInstanceLoopEventEntity implements IProcessInstanceLoopEvent {
+//     @Generated()
+//     @PrimaryColumn({ type: 'bigint', transformer: numberTransformer })
+//         id: number;
 
-    @Column({ transformer: dateTransformer })
-        created: string;
+//     @Column({ transformer: dateTransformer })
+//         created: string;
 
-    @Column()
-        log: string;
+//     @Column()
+//         log: string;
 
-    @Column()
-        step: string;
-    @Column()
-        script: string;
+//     @Column()
+//         step: string;
+//     @Column()
+//         script: string;
 
-    @ManyToOne(() => ProcessInstanceEntity, { onDelete: 'CASCADE' })
-    @JoinColumn({ name: 'process_instance_id', referencedColumnName: 'id' })
-        processInstance: IProcessInstance;
+//     @ManyToOne(() => ProcessInstance, { onDelete: 'CASCADE' })
+//     @JoinColumn({ name: 'process_instance_id', referencedColumnName: 'id' })
+//         processInstance: IProcessInstance;
 
-    @Column({ name: 'execution_id' })
-        executionId: string;
+//     @Column({ name: 'execution_id' })
+//         executionId: string;
 
-    @Column()
-        error: string;
+//     @Column()
+//         error: string;
 
-    @Column()
-        input: string;
+//     @Column()
+//         input: string;
 
-    @Column()
-        output: string;
+//     @Column()
+//         output: string;
 
-    @Column({ transformer: dateTransformer })
-        finished: string;
+//     @Column({ transformer: dateTransformer })
+//         finished: string;
 
-    @Column()
-        status: ProcessInstanceEventStatus;
-    @Column({name: 'iteration_number'})
-        iterationNumber: number;
+//     @Column()
+//         status: ProcessInstanceEventStatus;
+//     @Column({name: 'iteration_number'})
+//         iterationNumber: number;
 
-    @Column({name: 'iterator_element', type: 'jsonb'})
-        iteratorElement: JSON;
+//     @Column({name: 'iterator_element', type: 'jsonb'})
+//         iteratorElement: JSON;
 
-    @Column({name: 'loop_id'})
-        loopId: string;
-}
+//     @Column({name: 'loop_id'})
+//         loopId: string;
+// }
