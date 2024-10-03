2b0607701b26e0586ac2ce023885bc4402c122cc
Depcik <marcin.depcik@all-for-one.com>
2024-10-03 15:35:59 +0200

    feat(runbotics-scheduler): :sparkles: Add process instance event migration

---
+++ runbotics/runbotics-scheduler/src/scheduler-database/process-instance-event/dto/create-process-instance-event.dto.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { ProcessInstanceEvent } from '../process-instance-event.entity';
+
+export const createProcessInstanceEventSchema = z.object({});
+
+export type CreateProcessInstanceEventDto = z.infer<
+    typeof createProcessInstanceEventSchema
+> &
+    Omit<ProcessInstanceEvent, 'id' | 'processInstance'>;
---
+++ runbotics/runbotics-scheduler/src/scheduler-database/process-instance-event/process-instance-event.entity.ts
@@ -0,0 +1,55 @@
+import {
+    Entity,
+    Column,
+    ManyToOne,
+    JoinColumn,
+    Generated,
+    PrimaryColumn,
+} from 'typeorm';
+import { ProcessInstanceEventStatus } from 'runbotics-common';
+import { ProcessInstance } from '#/scheduler-database/process-instance/process-instance.entity';
+import { dateTransformer, numberTransformer } from '#/database/database.utils';
+
+@Entity({ name: 'process_instance_event' })
+export class ProcessInstanceEvent {
+    @Generated()
+    @PrimaryColumn({ type: 'bigint', transformer: numberTransformer })
+    id: number;
+
+    @Column({ type: 'time without time zone', transformer: dateTransformer })
+    created: string;
+
+    @Column({ type: 'text' })
+    log: string;
+
+    @Column({ name: 'process_instance_id', type: 'uuid' })
+    processInstanceId: ProcessInstance['id'];
+
+    @ManyToOne(() => ProcessInstance, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'process_instance_id', referencedColumnName: 'id' })
+    processInstance: ProcessInstance;
+
+    @Column({ type: 'character varying', length: 255 })
+    step: string;
+
+    @Column({ name: 'execution_id', type: 'character varying', length: 255 })
+    executionId: string;
+
+    @Column({ type: 'text' })
+    input?: string;
+
+    @Column({ type: 'text' })
+    output?: string;
+
+    @Column({ type: 'time without time zone', transformer: dateTransformer })
+    finished: string;
+
+    @Column({ type: 'enum', enum: ProcessInstanceEventStatus })
+    status: ProcessInstanceEventStatus;
+
+    @Column({ type: 'text', nullable: true })
+    error?: string;
+
+    @Column({ type: 'character varying', length: 255, nullable: true })
+    script?: string;
+}
