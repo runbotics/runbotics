2461bdf16d1d901c74e67a6613f5d67da0e8192f
Anna <anna.gasiorowska@all-for-one.com>
2024-10-15 11:40:31 +0200

    fix(runbotics-scheduler): :bug: additional: fixed schema validation for actions CRUD

--- runbotics/runbotics-scheduler/src/scheduler-database/action/dto/create-action.dto.ts
+++ runbotics/runbotics-scheduler/src/scheduler-database/action/dto/create-action.dto.ts
@@ -6,15 +6,16 @@ export const createActionSchema = z
         id: z
             .string()
             .startsWith('external.', { message: 'Must start with "external."' })
+            .trim()
             .min(10)
             .max(255),
         script: z
             .string()
             .startsWith('external.', { message: 'Must start with "external."' })
+            .trim()
             .min(10)
-            .max(255)
-            .trim(),
-        label: z.string().min(1).max(255).trim(),
+            .max(255),
+        label: z.string().trim().min(1).max(255),
         form: z.string(),
     })
     .required();
