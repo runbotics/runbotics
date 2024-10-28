d41f60f06633838184bb5b76f8846c1a21937c06
Depcik <marcin.depcik@all-for-one.com>
2024-10-28 12:02:34 +0100

    refactor(runbotics-scheduler): :recycle: Refactor scheduler

--- runbotics/runbotics-scheduler/src/database/trigger-event/trigger-event.entity.ts
+++
@@ -1,8 +0,0 @@
-// import { ITriggerEvent, TriggerEvent } from 'runbotics-common';
-// import { PrimaryColumn, Entity } from 'typeorm';
-
-// @Entity({ name: 'trigger_event', synchronize: false })
-// export class TriggerEventEntity implements ITriggerEvent {
-//     @PrimaryColumn({ type: 'character varying', length: 50 })
-//     name: TriggerEvent;
-// }
