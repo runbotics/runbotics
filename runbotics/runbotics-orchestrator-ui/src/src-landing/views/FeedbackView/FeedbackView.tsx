a45c8ea13e0eca9af6cdc99a91dcdb7c9812b013
Depcik <marcin.depcik@all-for-one.com>
2024-11-13 11:37:33 +0100

    feat(runbotics-orchestrator-ui): :sparkles: Add feedback page

---
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/views/FeedbackView/FeedbackView.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from 'react';
+
+import { Chip, CircularProgress, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { useRouter } from 'next/router';
+
+import LogoIcon from '#public/images/logos/all-for-one-logo.png';
+import useTranslations from '#src-app/hooks/useTranslations';
+import { Language } from '#src-app/translations/translations';
+
+import styles from './FeedbackView.module.scss';
+
+const FeedbackView = () => {
+    const [loading, setLoading] = useState(true);
+    const { translate, switchLanguage } = useTranslations();
+    const { push, locale: activeLocale, asPath } = useRouter();
+    const currentYear = new Date().getFullYear();
+
+    useEffect(() => {
+        switchLanguage(activeLocale as Language);
+        push(asPath, undefined, {
+            locale: activeLocale,
+        });
+    }, [activeLocale]);
+
+    return (
+        <div className={styles.feedbackWrapper}>
+            <nav>
+                <div className={styles.container}>
+                    <Image
+                        src={LogoIcon}
+                        className={styles.logo}
+                        alt="logo icon"
+                    />
+                </div>
+            </nav>
+            <main>
+                <div className={styles.container}>
+                    <div>
+                        <div className={styles.headSection}>
+                            <Typography>
+                                <span>
+                                    {translate(
+                                        'Feedback.Main.HeadSection.WeValueYourOpinion'
+                                    )}
+                                </span>
+                                {translate(
+                                    'Feedback.Main.HeadSection.WeWouldBeGrateful'
+                                )}
+                            </Typography>
+                        </div>
+                        <div className={styles.subsection}>
+                            <Typography>
+                                {translate(
+                                    'Feedback.Main.Subsection.TakePartInSurvey'
+                                )}
+                            </Typography>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={styles.hashtags}>
+                            <Chip
+                                className={`${styles.hashtag} ${styles.planning}`}
+                                label={translate(
+                                    'Feedback.Main.Hashtags.Planning'
+                                )}
+                            />
+                            <Chip
+                                className={`${styles.hashtag} ${styles.digitalization}`}
+                                label={translate(
+                                    'Feedback.Main.Hashtags.Digitalization'
+                                )}
+                            />
+                            <Chip
+                                className={`${styles.hashtag} ${styles.automation}`}
+                                label={translate(
+                                    'Feedback.Main.Hashtags.Automation'
+                                )}
+                            />
+                        </div>
+                        <div className={styles.chatWrapper}>
+                            {loading && (
+                                <CircularProgress className={styles.progress} />
+                            )}
+                            <iframe
+                                loading="lazy"
+                                src={process.env.NEXT_PUBLIC_COPILOT_CHAT_URL}
+                                onLoad={() => setLoading(false)}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                <div className={styles.container}>
+                    <Typography className={styles.copyright}>
+                        &copy; {currentYear} All For One Poland
+                    </Typography>
+                    <div className={styles.findUs}>
+                        <Typography>
+                            {translate('Feedback.Footer.FindUs')}
+                        </Typography>
+                        <div className={styles.links}>
+                            <Typography className={styles.link}>
+                                <Link
+                                    href="https://www.all-for-one.pl"
+                                    target="_blank"
+                                >
+                                    All For One Poland
+                                </Link>
+                            </Typography>
+                            <Typography className={styles.link}>
+                                <Link
+                                    href="https://softwarehouse.all-for-one.pl"
+                                    target="_blank"
+                                >
+                                    Enterprise Software House
+                                </Link>
+                            </Typography>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default FeedbackView;
