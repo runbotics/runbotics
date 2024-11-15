11fed3c33a0dbdecf711179d846872cd500894e5
Stanislaw Jarocki <s.jarocki354@gmail.com>
2024-11-15 09:06:04 +0100

    fix(runbotics-orchestrator-ui): :adhesive_bandage: display 'custom' label for action credential correctly

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/widgets/CredentialWidget/CredentialWidget.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/widgets/CredentialWidget/CredentialWidget.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 
 import { Edit } from '@mui/icons-material';
 import { Box, Button, Divider, Typography } from '@mui/material';
@@ -15,23 +15,50 @@ import { CustomCredentialSelectDialog } from './CustomCredentialSelectDialog';
 
 const CredentialWidget: FC<WidgetProps> = (props) => {
     const [open, setOpen] = useState(false);
-    const processId = useSelector(state => state.process.draft.process.id);
-    const { selectedElement } = useSelector(state => state.process.modeler);
-    const { allProcessAssigned } = useSelector(state => state.credentials);
+    const processId = useSelector((state) => state.process.draft.process.id);
+    const { selectedElement } = useSelector((state) => state.process.modeler);
+    const { allProcessAssigned } = useSelector((state) => state.credentials);
     const dispatch = useDispatch();
     const credentialType = getCredentialType(selectedElement);
 
+    const alreadySelectedCredential = useMemo(() =>
+        allProcessAssigned.find((cred) => 
+            cred?.credential?.id === props?.value
+        )?.credential, [allProcessAssigned]   
+    );
+
     useEffect(() => {
-        dispatch(credentialsActions.fetchAllCredentialsByTemplateAndProcess({ pageParams: { templateName: credentialType, processId: processId } }));
-        dispatch(credentialsActions.fetchAllCredentialsAssignedToProcess({ resourceId: processId }));
+        dispatch(
+            credentialsActions.fetchAllCredentialsByTemplateAndProcess({
+                pageParams: {
+                    templateName: credentialType,
+                    processId: processId,
+                },
+            })
+        );
+        dispatch(
+            credentialsActions.fetchAllCredentialsAssignedToProcess({
+                resourceId: processId,
+            })
+        );
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [credentialType, processId]);
 
-    const alreadySelectedCredential = allProcessAssigned.find(cred => cred?.credential?.id === props?.value)?.credential;
+    useEffect(() => {
+        setCredential(alreadySelectedCredential)
+    }, [alreadySelectedCredential]);
+
     const [credential, setCredential] = useState<CredentialDto | string>(alreadySelectedCredential);
     const isCustomCredential = Boolean(credential);
-    const primaryCredential = allProcessAssigned.find(cred => cred?.order === 1 && cred?.credential?.template?.name === credentialType);
-    const breadcrumb = getCredentialBreadcrumb(credential ?? alreadySelectedCredential, primaryCredential);
+    const primaryCredential = allProcessAssigned.find(
+        (cred) =>
+            cred?.order === 1 &&
+            cred?.credential?.template?.name === credentialType
+    );
+    const breadcrumb = getCredentialBreadcrumb(
+        credential ?? alreadySelectedCredential,
+        primaryCredential
+    );
 
     const handleClose = () => {
         setOpen(false);
@@ -54,9 +81,16 @@ const CredentialWidget: FC<WidgetProps> = (props) => {
 
     return (
         <>
-            <Typography variant="h5">{translate('Credentials.ActionFormSelect.Form.Section.Title')}</Typography>
+            <Typography variant="h5">
+                {translate('Credentials.ActionFormSelect.Form.Section.Title')}
+            </Typography>
             <Divider />
-            <Box display="flex" justifyContent="space-between" alignItems="center" pt={2}>
+            <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                pt={2}
+            >
                 <Typography variant="h6">{breadcrumb}</Typography>
                 <Typography variant="h6">
                     {
@@ -65,7 +99,9 @@ const CredentialWidget: FC<WidgetProps> = (props) => {
                             : translate('Credentials.ActionFormSelect.Form.CustomCredential.False')
                     }
                 </Typography>
-                <Button onClick={handleOpen} variant='text'><Edit /></Button>
+                <Button onClick={handleOpen} variant="text">
+                    <Edit />
+                </Button>
             </Box>
             <CustomCredentialSelectDialog
                 isOpen={open}
