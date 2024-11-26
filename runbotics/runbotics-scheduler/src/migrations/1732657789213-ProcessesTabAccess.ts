d6fa606a1d10cc060bf51debe4a5be96ac2b26dd
Stanisław Jarocki <s.jarocki354@gmail.com>
2024-11-26 15:13:42 +0100

    chore(runbotics-scheduler): :card_file_box: synchronize migrations files with develop

--- runbotics/runbotics-scheduler/src/migrations/1732657789213-ProcessesTabAccess.ts
+++ runbotics/runbotics-scheduler/src/migrations/1732657789213-ProcessesTabAccess.ts
@@ -3,8 +3,8 @@ import { FeatureKey } from '#/scheduler-database/feature-key/feature-key.entity'
 import { FeatureKey as FeatureKeyEnum, Role } from 'runbotics-common';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class ProcessesTabAccess1732557789213 implements MigrationInterface {
-    name = 'ProcessesTabAccess1732557789213';
+export class ProcessesTabAccess1732657789213 implements MigrationInterface {
+    name = 'ProcessesTabAccess1732657789213';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.manager.getRepository(FeatureKey).insert({ name: FeatureKeyEnum.ALL_PROCESSES_READ });
