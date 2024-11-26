b7289120e3995d9f657df16f9ce71b76c6ea8a64
Anna <anna.gasiorowska@all-for-one.com>
2024-11-26 11:48:49 +0100

    refactor(runbotics-scheduler): :recycle: removed comments

--- runbotics/runbotics-scheduler/src/migrations/1732542379024-SequencesBump.ts
+++ runbotics/runbotics-scheduler/src/migrations/1732542379024-SequencesBump.ts
@@ -19,7 +19,6 @@ export class SequencesBump1732542379024 implements MigrationInterface {
                 throw new Error('Cannot find sequence for process id');
         });
 
-        // Find max
         const maxProcessId = await queryRunner.manager
             .createQueryBuilder()
             .select('MAX(p.id)', 'max')
@@ -31,7 +30,7 @@ export class SequencesBump1732542379024 implements MigrationInterface {
 
                 return +res.max;
             });
-        // Reset sequence
+
         await queryRunner.query(`
             ALTER SEQUENCE process_id_seq RESTART WITH ${maxProcessId + 1};
         `);
@@ -47,7 +46,6 @@ export class SequencesBump1732542379024 implements MigrationInterface {
                 throw new Error('Cannot find sequence for tag id');
         });
 
-        // Find max
         const maxTagId = await queryRunner.manager
             .createQueryBuilder()
             .select('MAX(t.id)', 'max')
@@ -59,7 +57,7 @@ export class SequencesBump1732542379024 implements MigrationInterface {
 
                 return +res.max;
             });
-        // Reset sequence
+
         await queryRunner.query(`
             ALTER SEQUENCE tag_id_seq RESTART WITH ${maxTagId + 1};
         `);
@@ -75,7 +73,6 @@ export class SequencesBump1732542379024 implements MigrationInterface {
                 throw new Error('Cannot find sequence for global variable id');
         });
 
-        // Find max
         const maxGlobalVariableId = await queryRunner.manager
             .createQueryBuilder()
             .select('MAX(g.id)', 'max')
@@ -87,8 +84,8 @@ export class SequencesBump1732542379024 implements MigrationInterface {
 
                 return +res.max;
             });
-        // Reset sequence
-        await queryRunner.query(`
+
+            await queryRunner.query(`
             ALTER SEQUENCE global_variable_id_seq RESTART WITH ${maxGlobalVariableId + 1};
         `);
     }
