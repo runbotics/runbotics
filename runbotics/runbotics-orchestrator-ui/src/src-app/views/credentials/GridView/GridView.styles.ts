da88b47c2794002d6ffbba1aa5531c5a8a059e9c
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2024-11-26 13:08:53 +0100

    feat(runbotics-orchestrator-ui): :lipstick: Minor UI fixes

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/credentials/GridView/GridView.styles.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/credentials/GridView/GridView.styles.ts
@@ -4,8 +4,7 @@ import styled from 'styled-components';
 export const TileGrid = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-    grid-auto-rows: 1fr;
-    min-height: 8rem;
+    align-items: stretch;
     gap: 1rem;
 `;
 
