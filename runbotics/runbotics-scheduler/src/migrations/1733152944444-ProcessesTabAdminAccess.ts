d62bf78f81629458e3595406be8411d52b5f9bd0
Stanislaw Jarocki <s.jarocki354@gmail.com>
2024-12-09 09:02:36 +0100

    fix(runbotics-orchestrator-ui): :bug: give tenant admin access to editing private collections and processes

--- runbotics/runbotics-scheduler/src/migrations/1733152944444-ProcessesTabAdminAccess.ts
+++ runbotics/runbotics-scheduler/src/migrations/1733152944444-ProcessesTabAdminAccess.ts
@@ -1,22 +1,31 @@
 import { Authority } from '#/scheduler-database/authority/authority.entity';
-import { FeatureKey } from '#/scheduler-database/feature-key/feature-key.entity';
 import { FeatureKey as FeatureKeyEnum, Role } from 'runbotics-common';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class ProcessesTabAdminAccess1733144215000 implements MigrationInterface {
-    name = 'ProcessesTabAdminAccess1733144215000';
+export class ProcessesTabAdminAccess1733152944444 implements MigrationInterface {
+    name = 'ProcessesTabAdminAccess1733152944444';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         const authorityRepository = await queryRunner.manager.getRepository(Authority);
         const roleAdmin = await authorityRepository.findOneOrFail({ where: { name: Role.ROLE_ADMIN } });
-        roleAdmin.featureKeys.push({ name: FeatureKeyEnum.ALL_PROCESSES_READ });
+        roleAdmin.featureKeys.push(
+            { name: FeatureKeyEnum.ALL_PROCESSES_READ },
+            { name: FeatureKeyEnum.PROCESS_COLLECTION_ALL_ACCESS },
+            { name: FeatureKeyEnum.PROCESS_ALL_ACCESS, }
+        );
         await authorityRepository.save(roleAdmin);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         const authorityRepository = await queryRunner.manager.getRepository(Authority);
         const roleAdmin = await authorityRepository.findOneOrFail({ where: { name: Role.ROLE_ADMIN } });
-        roleAdmin.featureKeys = roleAdmin.featureKeys.filter(featureKey => featureKey.name !== FeatureKeyEnum.ALL_PROCESSES_READ);
+        roleAdmin.featureKeys = roleAdmin.featureKeys.filter(
+            featureKey => ![
+                FeatureKeyEnum.ALL_PROCESSES_READ,
+                FeatureKeyEnum.PROCESS_COLLECTION_ALL_ACCESS,
+                FeatureKeyEnum.PROCESS_ALL_ACCESS,
+            ].includes(featureKey.name)
+        );
         await authorityRepository.save(roleAdmin);
     }
 }
