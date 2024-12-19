db62c05992f60d224c21bb9d3801b914cdbef157
Marcin <82586165+marcin-depcik@users.noreply.github.com>
2024-12-19 12:16:17 +0100

    [RPA-1896] Fix Back to home button (#859)
    * fix(runbotics-orchestrator-ui): :bug: Fix Back to home button
    
    * fix(runbotics-orchestrator-ui): :bug: Fixes
    
    ---------
    
    Co-authored-by: Depcik <marcin.depcik@all-for-one.com>

--- runbotics/runbotics-orchestrator-ui/src/pages/404.tsx
+++ runbotics/runbotics-orchestrator-ui/src/pages/404.tsx
@@ -1,18 +1,16 @@
 import { useEffect } from 'react';
 
-import { useRouter } from 'next/router';
 import { useDispatch } from 'react-redux';
 
 import { setErrorCode } from '../src-app/store/slices/Views/httpErrorSlice';
 import ErrorView from '../src-app/views/errors/ErrorView';
 
 const NotFoundPage = () => {
-    const router = useRouter();
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(setErrorCode(404));
-    }, [router, dispatch]);
+    }, []);
 
     return <ErrorView />;
 };
