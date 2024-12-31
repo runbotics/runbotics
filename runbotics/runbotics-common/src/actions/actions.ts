bd428dd4ea15aa499a5914d2ffc0106db254e67a
Anna <anna.gasiorowska@all-for-one.com>
2024-12-31 13:12:26 +0100

    fix: :bug: filtered actions for guest

--- runbotics/runbotics-common/src/actions/actions.ts
+++ runbotics/runbotics-common/src/actions/actions.ts
@@ -47,6 +47,11 @@ export enum ACTION_GROUP {
     BEEOFFICE = 'beeOffice',
     SAP = 'sap',
     APPLICATION = 'application',
+    DESKTOP = 'desktop',
+    EXCEL = 'excel',
+    POWER_POINT = 'powerPoint',
+    WINDOWS = 'windows',
+    VISUAL_BASIC = 'visualBasic'
 }
 
 export enum VariableAction {
