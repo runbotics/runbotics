136deebb3ef18ef4a05de9f273ad6a608e596e6b
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2025-01-08 11:19:08 +0100

    feat(runbotics-scheduler): :sparkles: Adds BrowserLoginCredentialTemplate migration

---
+++ runbotics/runbotics-scheduler/src/migrations/1736250605617-BrowserLoginCredentialTemplate.ts
@@ -0,0 +1,27 @@
+import { CredentialTemplate } from '#/scheduler-database/credential-template/credential-template.entity';
+import { ActionCredentialType } from 'runbotics-common';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class BrowserLoginCredentialTemplate1736250605617
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.manager.save(CredentialTemplate, [{
+            name: ActionCredentialType.BROWSER_LOGIN,
+            attributes: [
+                {
+                    name: 'login',
+                    description: 'Username/login'
+                },
+                {
+                    name: 'password',
+                    description: 'Account password'
+                }
+            ]
+        }]);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.manager.delete(CredentialTemplate, { name: ActionCredentialType.BROWSER_LOGIN });
+    }
+}
