1dc02e3fb7c41519569be506bdea4563c5a0fec8
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2025-02-03 14:19:34 +0100

    feat(runbotics-orchestrator-ui): :sparkles: Changes gateway validation

---
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/renderers/GatewayFormRenderer/DefaultGatewaySelect.tsx
@@ -0,0 +1,84 @@
+import { FC, ChangeEvent, FocusEvent } from 'react';
+
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
+import BpmnModelerType from 'bpmn-js/lib/Modeler';
+
+import useTranslations from '#src-app/hooks/useTranslations';
+
+
+import { BpmnConnectionFactory, IBpmnConnection, IBpmnGateway } from '../../../helpers/elementParameters';
+
+
+interface DefaultGatewaySelectProps {
+    gateway:IBpmnGateway;
+    defaultFlow: string;
+    setDefaultFlow: React.Dispatch<React.SetStateAction<string>>;
+    handleOnFocus: (event: FocusEvent<HTMLInputElement>) => void;
+    handleOnBlur: (event: FocusEvent<HTMLInputElement>) => void;
+    filteredGatewayConnections: IBpmnConnection[];
+    modeler: BpmnModelerType;
+    expressions: {[key: string]: any};
+    validateFlows: () => void;
+};
+
+const DefaultGatewaySelect: FC<DefaultGatewaySelectProps> = ({ gateway, defaultFlow, setDefaultFlow, handleOnFocus, handleOnBlur, filteredGatewayConnections, modeler, expressions, validateFlows }) => {
+    const { translate } = useTranslations();
+
+
+    const setDefaultConnection = (outgoing: IBpmnConnection | null) => {
+        if (!outgoing) {
+            gateway.businessObject.default = null;
+            setDefaultFlow('None');
+        } else {
+            setDefaultFlow(outgoing.id);
+        }
+        BpmnConnectionFactory.from(modeler).setDefaultConnection(outgoing, gateway);
+        validateFlows();
+    };
+
+    const handleDefaultFlowChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value === 'None') {
+            setDefaultConnection(null);
+            return;
+        }
+        gateway.outgoing.forEach((outgoing) => {
+            if (event.target.value === outgoing.id && expressions) {
+                setDefaultConnection(outgoing);
+                BpmnConnectionFactory.from(modeler).setConditionExpression(
+                    outgoing,
+                    expressions[outgoing.id]
+                );
+            }
+        });
+    };
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel id="default-flow-select-input-label">
+                {translate('Process.Details.Modeler.ActionPanel.Form.Gateway.DefaultFlow')}
+            </InputLabel>
+            <Select
+                labelId="default-flow-select-input-label"
+                label={translate('Process.Details.Modeler.ActionPanel.Form.Gateway.DefaultFlow')}
+                id="default-flow-select"
+                value={defaultFlow}
+                onChange={handleDefaultFlowChange}
+                onFocus={handleOnFocus}
+                onBlur={handleOnBlur}
+            >
+                <MenuItem key='flow-menu-item-none' value="None">None</MenuItem>
+                {
+                    filteredGatewayConnections
+                        .map((outgoing) => (
+                            <MenuItem key={'flow-menu-item-' + outgoing.id} value={outgoing.id}>
+                                {outgoing.businessObject.name ? outgoing.businessObject.name : outgoing.id}
+                            </MenuItem>
+                        ))
+                }
+            </Select>
+        </FormControl>
+    );
+};
+
+export default DefaultGatewaySelect;
---
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/renderers/GatewayFormRenderer/GatewayFlowExpression.tsx
@@ -0,0 +1,61 @@
+import { FC, FocusEvent } from 'react';
+
+import useOptions from '#src-app/hooks/useOptions';
+
+import useTranslations from '#src-app/hooks/useTranslations';
+
+import { FlowExpression } from './GatewayFormRenderer.styles';
+import { isSequenceWithExpression } from './GatewayFormRenderer.utils';
+import { IBpmnConnection, IBpmnGateway } from '../../../helpers/elementParameters';
+import FlowLabelForm from '../../FlowLabelForm';
+import AutocompleteWidget from '../../widgets/AutocompleteWidget';
+import { NewValueWithName } from '../../widgets/AutocompleteWidget/AutocompleteWidget.types';
+
+interface GatewayFlowExpressionProps {
+    gateway: IBpmnGateway;
+    outgoing: IBpmnConnection;
+    handleOnFocus: (event: FocusEvent<HTMLInputElement>) => void;
+    handleOnBlur: (event: FocusEvent<HTMLInputElement>) => void;
+    handleCancel: (flow: IBpmnConnection) => void;
+    handleExpressionChange: (newValueWithName: NewValueWithName) => void
+    handleConnectionNameChange: (inputValue: string, flow: IBpmnConnection) => void
+};
+
+const GatewayFlowExpression: FC<GatewayFlowExpressionProps> = ({ gateway, outgoing, handleOnFocus, handleOnBlur, handleCancel, handleExpressionChange, handleConnectionNameChange }) => {
+    const { options } = useOptions();
+    const { translate } = useTranslations();
+
+    const emptyExpressionError = translate('Process.Edit.Form.Fields.Error.Required');
+
+    return (
+        <FlowExpression key={'flow-expression-' + outgoing.id}>
+            <FlowLabelForm
+                formLabel={translate('Process.Details.Modeler.ActionPanel.Form.FlowName.Title')}
+                onSubmit={(name) => handleConnectionNameChange(name, outgoing)}
+                selectedElement={outgoing}
+                onCancel={() => handleCancel(outgoing)}
+                onFocus={handleOnFocus}
+            />
+            <AutocompleteWidget
+                id={'autocomplete-text-field-' + outgoing.id}
+                withName={true}
+                onChange={handleExpressionChange}
+                autocompleteOptions={options}
+                handleOnBlur={handleOnBlur}
+                handleOnFocus={handleOnFocus}
+                label={`${translate(
+                    'Process.Details.Modeler.ActionPanel.Form.Connection.Expression.Expression'
+                )}`}
+                value={outgoing.businessObject.conditionExpression?.body ?? ''}
+                name={outgoing.id}
+                customErrors={isSequenceWithExpression(gateway, outgoing) ? undefined : [emptyExpressionError]}
+                disabled={false}
+                required={false}
+                autofocus={false}
+            />
+        </FlowExpression>
+    );
+};
+
+
+export default GatewayFlowExpression;
