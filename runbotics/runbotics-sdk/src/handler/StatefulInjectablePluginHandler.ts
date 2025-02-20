ddf0eb07653e97572706bdd5def9c9d7eb8630f8
Depcik <marcin.depcik@all-for-one.com>
2025-02-20 09:05:09 +0100

    feat: :sparkles: Add support for runbotics-plugins on rb-sdk

---
+++ runbotics/runbotics-sdk/src/handler/StatefulInjectablePluginHandler.ts
@@ -0,0 +1,16 @@
+import { DesktopRunRequest } from './DesktopRunRequest';
+import { DesktopRunResponse } from './DesktopRunResponse';
+import { PluginHandler } from './utils';
+
+export abstract class StatefulInjectablePluginHandler {
+    abstract run<This>(
+        ctx: This,
+        request: DesktopRunRequest,
+    ): Promise<DesktopRunResponse | void>;
+
+    getType(): string {
+        return PluginHandler.StatefulInjectable;
+    }
+
+    abstract tearDown(): Promise<void>;
+}
--- runbotics/runbotics-sdk/src/handler/utils.ts
+++ runbotics/runbotics-sdk/src/handler/utils.ts
@@ -1,28 +1,86 @@
 import { StatelessActionHandler } from './StatelessActionHandler';
 import { StatefulActionHandler } from './StatefulActionHandler';
+import { StatelessStandalonePluginHandler } from './StatelessStandalonePluginHandler';
+import { StatelessInjectablePluginHandler } from './StatelessInjectablePluginHandler';
+import { StatefulStandalonePluginHandler } from './StatefulStandalonePluginHandler';
+import { StatefulInjectablePluginHandler } from './StatefulInjectablePluginHandler';
+
+type WithGetType = { getType: () => string };
+type WithTearDown = { tearDown: () => Promise<void> };
+
+export type PluginService =
+    | StatelessInjectablePluginHandler
+    | StatefulInjectablePluginHandler;
+
+export const PLUGIN_SERVICE = 'pluginService';
 
 export enum ActionHandler {
     Stateful = 'StatefulActionHandler',
     Stateless = 'StatelessActionHandler',
-};
+}
+
+export enum PluginHandler {
+    StatefulStandalone = 'StatefulStandalonePluginHandler',
+    StatelessStandalone = 'StatelessStandalonePluginHandler',
+    StatefulInjectable = 'StatefulInjectablePluginHandler',
+    StatelessInjectable = 'StatelessInjectablePluginHandler',
+}
+
+const hasGetType = (handler: unknown): handler is WithGetType =>
+    handler !== undefined &&
+    handler !== null &&
+    typeof handler === 'object' &&
+    'getType' in handler &&
+    handler.getType &&
+    typeof handler.getType === 'function';
+
+const hasTearDown = (handler: unknown): handler is WithTearDown =>
+    handler !== undefined &&
+    handler !== null &&
+    typeof handler === 'object' &&
+    'tearDown' in handler &&
+    typeof handler.tearDown === 'function';
 
 export const isStatelessActionHandler = (
     handler: unknown
-): handler is StatelessActionHandler => handler !== undefined
-    && handler !== null
-    && typeof handler === 'object'
-    && 'getType' in handler
-    && handler.getType
-    && typeof handler.getType === 'function'
-    && handler.getType() === ActionHandler.Stateless;
+): handler is StatelessActionHandler =>
+    hasGetType(handler) && handler.getType() === ActionHandler.Stateless;
+
+export const isStatelessStandalonePluginHandler = (
+    handler: unknown
+): handler is StatelessStandalonePluginHandler =>
+    hasGetType(handler) &&
+    handler.getType() === PluginHandler.StatelessStandalone;
+
+export const isStatelessInjectablePluginHandler = (
+    handler: unknown
+): handler is StatelessInjectablePluginHandler =>
+    hasGetType(handler) &&
+    handler.getType() === PluginHandler.StatelessInjectable;
 
 export const isStatefulActionHandler = (
     handler: unknown
-): handler is StatefulActionHandler => handler !== undefined
-    && handler !== null
-    && typeof handler === 'object'
-    && 'getType' in handler
-    && typeof handler.getType === 'function'
-    && handler.getType() === ActionHandler.Stateful
-    && 'tearDown' in handler
-    && typeof handler.tearDown === 'function';
+): handler is StatefulActionHandler =>
+    hasGetType(handler) &&
+    handler.getType() === ActionHandler.Stateful &&
+    hasTearDown(handler);
+
+export const isStatefulStandalonePluginHandler = (
+    handler: unknown
+): handler is StatefulStandalonePluginHandler =>
+    hasGetType(handler) &&
+    handler.getType() === PluginHandler.StatefulStandalone &&
+    hasTearDown(handler);
+
+export const isStatefulInjectablePluginHandler = (
+    handler: unknown
+): handler is StatefulInjectablePluginHandler =>
+    hasGetType(handler) &&
+    handler.getType() === PluginHandler.StatefulInjectable &&
+    hasTearDown(handler);
+
+export const isPluginHandler = (handler: unknown) =>
+    isStatelessStandalonePluginHandler(handler) ||
+    isStatelessInjectablePluginHandler(handler) ||
+    isStatefulStandalonePluginHandler(handler) ||
+    isStatefulInjectablePluginHandler(handler);
