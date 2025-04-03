747025090e8a636dd41f3f281f1274d898e7ed0d
jankowiakb <blazej.jankowiak@all-for-one.com>
2025-04-03 09:42:28 +0200

    [RPA-2082] accordion with selects in parameters

--- runbotics/runbotics-orchestrator-ui/src/src-landing/components/MarketplaceCartContainer/MarketplaceCartContainer.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-landing/components/MarketplaceCartContainer/MarketplaceCartContainer.tsx
@@ -1,23 +1,13 @@
-import { Dispatch, FC, SetStateAction, useMemo } from 'react';
+import { FC } from 'react';
 
-import { Trash } from 'react-feather';
-
-import { Column } from 'react-table';
-
-import Table from '#src-app/components/tables/Table';
-import { CartItem, useCart } from '#src-app/contexts/CartContext';
-import useTranslations from '#src-app/hooks/useTranslations';
-import Checkbox from '#src-landing/components/Checkbox';
-
-import styles from './MarketplaceCartContainer.module.scss';
 import MarketplaceCartAccordion from '#src-landing/components/MarketplaceCartAccordion';
 
-const MarketplaceCartContainer: FC = () => {
-    return (
-        <div className={styles.root}>
-            <MarketplaceCartAccordion />
-        </div>
-    );
-};
+import styles from './MarketplaceCartContainer.module.scss';
+
+const MarketplaceCartContainer: FC = () => (
+    <div className={styles.root}>
+        <MarketplaceCartAccordion />
+    </div>
+);
 
 export default MarketplaceCartContainer;
