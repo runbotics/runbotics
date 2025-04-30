7b0247af8ed73b151f4295ccb481a24aa335824e
teawithsand <teawithsand@gmail.com>
2025-04-30 10:24:29 +0200

    feat(runbotics-common): :sparkles: Adjusted definition of NotificationProcess for new arbitrary email feature

--- runbotics/runbotics-common/src/model/api/notification-process.model.ts
+++ runbotics/runbotics-common/src/model/api/notification-process.model.ts
@@ -4,6 +4,7 @@ import { User } from "./user.model";
 export interface NotificationProcess {
     id: string;
     user: User;
+    email: string | '';
     type: NotificationProcessType;
     createdAt: string;
 }
