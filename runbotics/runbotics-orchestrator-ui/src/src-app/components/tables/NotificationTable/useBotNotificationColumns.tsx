14f5b02acb70054b9d1cd806df92859210a1a005
teawithsand <teawithsand@gmail.com>
2025-04-30 10:31:30 +0200

    feat(runbotics-orchestrator-ui): :sparkles: Implemented support for arbitrary email process subscriptions in the UI

--- runbotics/runbotics-orchestrator-ui/src/src-app/components/tables/NotificationTable/useBotNotificationColumns.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/components/tables/NotificationTable/useBotNotificationColumns.tsx
@@ -11,7 +11,7 @@ import moment from 'moment';
 import useTranslations from '#src-app/hooks/useTranslations';
 
 import { BotNotificationRow } from './NotificationTableComponent.types';
-import { SubscribersTableFields } from './NotificationTableComponent.utils';
+import { SubscriberBotTableFields } from './NotificationTableComponent.utils';
 
 interface ColumnsActions {
     onDelete: (botSubscriber: BotNotificationRow) => Promise<void>;
@@ -19,40 +19,43 @@ interface ColumnsActions {
 
 const useBotNotificationColumns = ({
     onDelete,
-}: ColumnsActions): GridEnrichedColDef[] => {
+}: ColumnsActions): (GridEnrichedColDef & {
+    field: SubscriberBotTableFields;
+})[] => {
     const { translate } = useTranslations();
 
     return [
         {
-            field: SubscribersTableFields.USER,
+            field: 'userEmail',
             headerName: translate('Bot.Edit.Table.Columns.User'),
-            flex: 0.4
+            flex: 0.4,
         },
         {
-            field: SubscribersTableFields.SUBSCRIBED_AT,
+            field: 'subscribedAt',
             headerName: translate('Bot.Edit.Table.Columns.SubscribedAt'),
             flex: 0.4,
             valueFormatter: (params: GridValueFormatterParams) =>
                 moment(params.value as string).format('YYYY-MM-DD HH:mm'),
         },
         {
-            field: SubscribersTableFields.ACTIONS,
+            field: 'actions',
             headerName: translate('Bot.Edit.Table.Columns.Actions'),
             type: 'actions',
             flex: 0.2,
             getActions: (params: GridRowParams<any>) => {
-
                 const handleDeleteClick = () => {
                     if (onDelete) onDelete(params.row);
                 };
 
                 return [
                     <GridActionsCellItem
-                        label={translate('Bot.Edit.Table.Columns.Actions.Delete')}
+                        label={translate(
+                            'Bot.Edit.Table.Columns.Actions.Delete'
+                        )}
                         icon={<DeleteIcon />}
                         onClick={handleDeleteClick}
                         key="delete"
-                    />
+                    />,
                 ];
             },
         },
--- runbotics/runbotics-orchestrator-ui/src/src-app/components/tables/NotificationTable/useProcessNotificationColumns.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/components/tables/NotificationTable/useProcessNotificationColumns.tsx
@@ -11,7 +11,7 @@ import moment from 'moment';
 import useTranslations from '#src-app/hooks/useTranslations';
 
 import { ProcessNotificationRow } from './NotificationTableComponent.types';
-import { SubscribersTableFields } from './NotificationTableComponent.utils';
+import { SubscriberProcessTableFields } from './NotificationTableComponent.utils';
 
 interface ColumnsActions {
     onDelete: (processSubscriber: ProcessNotificationRow) => Promise<void>;
@@ -19,40 +19,50 @@ interface ColumnsActions {
 
 const useProcessNotificationColumns = ({
     onDelete,
-}: ColumnsActions): GridEnrichedColDef[] => {
+}: ColumnsActions): (GridEnrichedColDef & {
+    field: SubscriberProcessTableFields;
+})[] => {
     const { translate } = useTranslations();
 
     return [
         {
-            field: SubscribersTableFields.USER,
+            field: 'userEmail',
             headerName: translate('Process.Edit.Table.Columns.User'),
-            flex: 0.4
+            flex: 0.4,
         },
         {
-            field: SubscribersTableFields.SUBSCRIBED_AT,
+            field: 'email',
+            headerName: translate('Process.Edit.Table.Columns.User'),
+            flex: 0.4,
+            valueFormatter: (params: GridValueFormatterParams) =>
+                params.value ? params.value : '—',
+        },
+        {
+            field: 'subscribedAt',
             headerName: translate('Process.Edit.Table.Columns.SubscribedAt'),
             flex: 0.4,
             valueFormatter: (params: GridValueFormatterParams) =>
                 moment(params.value as string).format('YYYY-MM-DD HH:mm'),
         },
         {
-            field: SubscribersTableFields.ACTIONS,
+            field: 'actions',
             headerName: translate('Process.Edit.Table.Columns.Actions'),
             type: 'actions',
             flex: 0.2,
             getActions: (params: GridRowParams<any>) => {
-
                 const handleDeleteClick = () => {
                     if (onDelete) onDelete(params.row);
                 };
 
                 return [
                     <GridActionsCellItem
-                        label={translate('Process.Edit.Table.Columns.Actions.Delete')}
+                        label={translate(
+                            'Process.Edit.Table.Columns.Actions.Delete'
+                        )}
                         icon={<DeleteIcon />}
                         onClick={handleDeleteClick}
                         key="delete"
-                    />
+                    />,
                 ];
             },
         },
