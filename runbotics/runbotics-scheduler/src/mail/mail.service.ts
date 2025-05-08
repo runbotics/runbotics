a25debbbb931481d8d381e46b3a040363969bb0f
teawithsand <teawithsand@gmail.com>
2025-05-08 11:54:37 +0200

    Removed empty mail filtering

--- runbotics/runbotics-scheduler/src/mail/mail.service.ts
+++ runbotics/runbotics-scheduler/src/mail/mail.service.ts
@@ -13,6 +13,7 @@ import { NotificationProcessService } from '#/scheduler-database/notification-pr
 import { BotEntity } from '#/scheduler-database/bot/bot.entity';
 import { NotificationBotService } from '#/scheduler-database/notification-bot/notification-bot.service';
 import { DeleteUserDto } from '#/scheduler-database/user/dto/delete-user.dto';
+import { hasFeatureKey } from '#/utils/authority.utils';
 
 export type SendMailInput = {
     to?: string;
@@ -123,8 +124,8 @@ export class MailService {
         if (!failedProcess.isPublic) {
             const filteredSubscriptions = subscriptions
                 .filter(
-                    sub => sub.email !== ''
-                        ||
+                    sub =>
+                        sub.email !== '' ||
                         sub
                             .user
                             .authorities
@@ -132,12 +133,11 @@ export class MailService {
                                 authority => [Role.ROLE_ADMIN, Role.ROLE_TENANT_ADMIN].includes(authority.name)
                             )
                 )
-                .map(x => x.getNotificationEmail())
-                .filter(e => !!e);
+                .map(x => x.getNotificationEmail());
 
             await this.handleNotificationEmail(sendMailInput, [processCreatorEmail, ...filteredSubscriptions]);
         } else {
-            const subscribersAddresses = subscriptions.map(x => x.getNotificationEmail()).filter(e => !!e);
+            const subscribersAddresses = subscriptions.map(x => x.getNotificationEmail());
             await this.handleNotificationEmail(sendMailInput, [processCreatorEmail, ...subscribersAddresses]);
         }
     }
