4055f84c60081c3efec86773849c658e589886ce
teawithsand <teawithsand@gmail.com>
2025-05-29 09:24:57 +0200

    refactor(runbotics-desktop): :recycle: Removed external axios deprecation

--- runbotics/runbotics-desktop/src/config/axios-configuration.ts
+++ runbotics/runbotics-desktop/src/config/axios-configuration.ts
@@ -4,10 +4,4 @@ const TIMEOUT = 1 * 60 * 1000;
 Axios.defaults.timeout = TIMEOUT;
 Axios.defaults.maxRedirects = 0;
 
-/**
- * @deprecated Instead use RequestService.getExternalAxios
- * 
- * @see RequestService
- */
 export const externalAxios = Axios.create({ maxRedirects: 0 });
-// orchestratorAxios.defaults.timeout = TIMEOUT
