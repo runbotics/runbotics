0ce27d4343d4bf4bc438ad652878e0eb30dda4ae
teawithsand <teawithsand@gmail.com>
2025-05-29 08:54:47 +0200

    refactor(runbotics-desktop): :recycle: Removed schedulerAxios in favor or orchestratorAxios

--- runbotics/runbotics-desktop/src/config/axios-configuration.ts
+++ runbotics/runbotics-desktop/src/config/axios-configuration.ts
@@ -4,6 +4,5 @@ const TIMEOUT = 1 * 60 * 1000;
 Axios.defaults.timeout = TIMEOUT;
 Axios.defaults.maxRedirects = 0;
 
-export const schedulerAxios = Axios.create({ maxRedirects: 0 });
 export const externalAxios = Axios.create({ maxRedirects: 0 });
 // orchestratorAxios.defaults.timeout = TIMEOUT
