283dfdcf7b93e0d6bd52bcdee7ab4918015b8881
teawithsand <teawithsand@gmail.com>
2025-05-29 08:46:12 +0200

    refactor(runbotics-desktop): :recycle: Removed orchestratorAxios in favor of AuthService internal axios

--- runbotics/runbotics-desktop/src/config/axios-configuration.ts
+++ runbotics/runbotics-desktop/src/config/axios-configuration.ts
@@ -3,7 +3,7 @@ import Axios from 'axios';
 const TIMEOUT = 1 * 60 * 1000;
 Axios.defaults.timeout = TIMEOUT;
 Axios.defaults.maxRedirects = 0;
-export const orchestratorAxios = Axios.create({ maxRedirects: 0 });
+
 export const schedulerAxios = Axios.create({ maxRedirects: 0 });
 export const externalAxios = Axios.create({ maxRedirects: 0 });
 // orchestratorAxios.defaults.timeout = TIMEOUT
