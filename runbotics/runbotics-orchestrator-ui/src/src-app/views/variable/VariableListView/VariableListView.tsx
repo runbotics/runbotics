ae19b0443c5889edf33394e32c88f7a44697f444
igorspychalaafo <210660167+igorspychalaafo@users.noreply.github.com>
2025-06-05 11:06:10 +0200

    feat(runbotics-orchestrator-ui): :sparkles: Add paging and sorting to gloabal variables

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/variable/VariableListView/VariableListView.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/variable/VariableListView/VariableListView.tsx
@@ -1,15 +1,14 @@
 import React, { FunctionComponent, useState } from 'react';
 
-import { Box } from '@mui/material';
-
+import { Box, TextField } from '@mui/material';
 
 import InternalPage from '#src-app/components/pages/InternalPage';
-
 import useTranslations from '#src-app/hooks/useTranslations';
 
+
 import Header from './Header';
-import Table from './Table/Table';
 import { VariableDetailState } from '../Variable.types';
+import Table from './Table/Table';
 import VariableDetails from '../VariableDetails/VariableDetails';
 
 
@@ -20,12 +19,24 @@ const VariableListView: FunctionComponent = () => {
     const [variableDetailState, setVariableDetailState] = useState<VariableDetailState>(initialVariableDetailState);
     const { translate } = useTranslations();
 
+    const [search, setSearch] = useState('');
+    
+    const handleSearch = (value: string) => { 
+        setSearch(value);
+    };
+
     return (
         <>
             <InternalPage title={translate('Variables.Meta.Title')}>
                 <Header onVariableCreate={() => setVariableDetailState({ show: true })} />
+                <Box display='flex' justifyContent='flex-end' mt={2}>
+                    <TextField 
+                        label={'Search'}
+                        onChange={(e) => handleSearch(e.target.value)}
+                    />
+                </Box>
                 <Box mt={3}>
-                    <Table setVariableDetailState={setVariableDetailState} />
+                    <Table setVariableDetailState={setVariableDetailState} searchValue={search} />
                 </Box>
             </InternalPage>
             <VariableDetails
