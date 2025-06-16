4fc0a777c18b1aae6439b4a2f0a185400df5e6ae
Fiflon <domanskibiznes@gmail.com>
2025-06-16 14:51:00 +0200

    fix(runbotics-orchestrator-ui): :sparkles: Add workaround for Add Item button translation

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/renderers/JSONSchemaFormRenderer.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/process/ProcessBuildView/Modeler/ActionFormPanel/renderers/JSONSchemaFormRenderer.tsx
@@ -5,6 +5,7 @@ import React, {
     useEffect,
     useState,
     useRef,
+    useLayoutEffect
 } from 'react';
 
 import { Box, Button, Grid, Alert } from '@mui/material';
@@ -18,9 +19,12 @@ import { useDispatch, useSelector } from '#src-app/store';
 
 import { ModelerError, processActions } from '#src-app/store/slices/Process';
 
+import i18n from '#src-app/translations/i18n';
+
 import { FormState } from '../../../../../../Actions/types';
 import AutocompleteWidget from '../widgets/AutocompleteWidget';
 import FieldTemplate from '../widgets/FieldTemplate';
+
 
 const Form = withTheme<any>(Mui5Theme) as FC<FormProps<any>>;
 
@@ -55,6 +59,7 @@ const initialFormState = {
 };
 
 const JSONSchemaFormRenderer: FC<FormPropsExtended> = (props) => {
+
     const dispatch = useDispatch();
     const [editMode, setEditMode] = useState(false);
     const [formState, setFormState] = useState<FormState>(initialFormState);
@@ -65,6 +70,9 @@ const JSONSchemaFormRenderer: FC<FormPropsExtended> = (props) => {
     const editModeRef = useRef(editMode);
     const debouncedForm = useDebounce<FormState>(formState, DEBOUNCE_TIME);
     const { selectedElement } = useSelector(state => state.process.modeler);
+
+    const [formRef, setFormRef] = useState<HTMLElement | undefined>(undefined);
+    const [addButton, setAddButton] = useState<HTMLElement | undefined>(undefined);
 
     const handleSubmit = (e: any, nativeEvent?: FormEvent<HTMLFormElement>) => {
         setEditMode(false);
@@ -103,6 +111,7 @@ const JSONSchemaFormRenderer: FC<FormPropsExtended> = (props) => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [debouncedForm]);
+    
     useEffect(
         () => () => {
             if (isFormDirtyRef.current && editModeRef.current) {
@@ -112,8 +121,39 @@ const JSONSchemaFormRenderer: FC<FormPropsExtended> = (props) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
         []
     );
+
+    useLayoutEffect(() => {
+        if (!formRef) return;
+
+        const observer = new MutationObserver(() => {
+            const button = formRef.querySelector(
+                '*#mainActionGrid button:has(svg[data-testid="AddIcon"])'
+            ) as HTMLElement; // TEST ZEBY SPRAWDZIC CZY JAK COS KTOS ZMIENI TO DZIAŁA 4 SURE
+
+            if (button) {
+                setAddButton(button);
+            }
+        });
+
+        observer.observe(formRef, { childList: true, subtree: true });
+
+        // eslint-disable-next-line consistent-return
+        return () => {
+            observer.disconnect();
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [formRef, i18n.language]);
+
+    useLayoutEffect(() => {
+        if (!addButton) return;
+        addButton.childNodes[1].textContent = translate(
+            'Process.Details.Modeler.Widgets.FieldTemplate.Action.AddItem'
+        );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [addButton, i18n.language]);
+
     return (
-        <Grid item xs={12}>
+        <Grid item xs={12} ref={setFormRef} id='mainActionGrid'>
             <Box px={1}>
                 <Box display="flex">{props.panel}</Box>
                 <Box p={1}>
