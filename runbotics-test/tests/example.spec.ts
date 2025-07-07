c0b8c35ebe6d83e1bdb9cdfa7fd11a89b6a4ed83
igorspychalaafo <210660167+igorspychalaafo@users.noreply.github.com>
2025-07-07 11:18:42 +0200

    fix: :bug: add semicolons to test

--- runbotics-test/tests/example.spec.ts
+++ runbotics-test/tests/example.spec.ts
@@ -9,16 +9,16 @@ const password = authData.tenant_admin?.password;
 
 if (!login || !password) throw new Error(`Either login or password are not provided for tenant_admin field`)
 
-test('can login', async ({ page }) => {
-	await page.goto('/login')
-	await page.waitForLoadState("load")
-	await page.getByLabel('Email Address').fill(login)
-	await page.getByLabel('Password').fill(password)
+test('tenant-admin can login', async ({ page }) => {
+	await page.goto('/login');
+	await page.waitForLoadState("load");
+	await page.getByLabel('Email Address').fill(login);
+	await page.getByLabel('Password').fill(password);
 
-	await page.getByText('Login', { exact: true }).click()
+	await page.getByText('Login', { exact: true }).click();
 
-	await page.waitForURL("/app/processes/collections**")
-	await page.waitForLoadState("load")
+	await page.waitForURL("/app/processes/collections**");
+	await page.waitForLoadState("load");
 
 	expect(await page.title()).toMatch(/RunBotics/i)
 });
