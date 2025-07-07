7f0bf6c0e4e46ca00ae738fd3e07c353ad0e4388
igorspychalaafo <210660167+igorspychalaafo@users.noreply.github.com>
2025-07-07 11:21:43 +0200

    [RPA-2241] Add selfhosted E2E test automation (#1060)
    * feat: :sparkles: create dockefile and gh workflow for tests
    
    * fix: :bug: improve workflow
    
    * fix: :bug: change from dockerfile to setup-node
    
    * fix: :bug: change from dockerfile to setup-node
    
    * feat(runbotics-test): :sparkles: Adjusted tests to accept auth.json with multiple users
    
    * fix: :bug: add selfhosted runner, add all users
    
    * fix: :bug: fix naming in e2e.yml
    
    * fix: :bug: fix naming in e2e.yml
    
    * fix: :bug: add semicolons to test
    
    ---------
    
    Co-authored-by: teawithsand <teawithsand@gmail.com>

--- runbotics-test/tests/example.spec.ts
+++ runbotics-test/tests/example.spec.ts
@@ -3,19 +3,19 @@ import { readFileSync } from 'node:fs';
 import path from "node:path"
 
 const authFile = path.join(__dirname, '../.auth/user.json');
-const authData = JSON.parse(readFileSync(authFile, 'utf-8'))
-const login = authData.login;
-const password = authData.password;
+const authData = JSON.parse(readFileSync(authFile, 'utf-8'));
+const login = authData.tenant_admin?.login;
+const password = authData.tenant_admin?.password;
 
-if (!login || !password) throw new Error(`Either login or password are not provided`)
+if (!login || !password) throw new Error(`Either login or password are not provided for tenant_admin field`)
 
-test('can login', async ({ page }) => {
+test('tenant-admin can login', async ({ page }) => {
 	await page.goto('/login');
 	await page.waitForLoadState("load");
-	await page.getByLabel('Email Address').fill(login)
-	await page.getByLabel('Password').fill(password)
+	await page.getByLabel('Email Address').fill(login);
+	await page.getByLabel('Password').fill(password);
 
-	await page.getByText('Login', { exact: true }).click()
+	await page.getByText('Login', { exact: true }).click();
 
 	await page.waitForURL("/app/processes/collections**");
 	await page.waitForLoadState("load");
