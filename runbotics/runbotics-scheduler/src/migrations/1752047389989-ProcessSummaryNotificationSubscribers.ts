a64a9814e5b9d4733af17367c9c6ab1d8228c57d
igorspychalaafo <210660167+igorspychalaafo@users.noreply.github.com>
2025-07-10 09:28:57 +0200

    feat(runbotics-scheduler): :sparkles: Create module for process summary notification

---
+++ runbotics/runbotics-scheduler/src/migrations/1752047389989-ProcessSummaryNotificationSubscribers.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class ProcessSummaryNotificationSubscribers1752047389989 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS "process_summary_notification_subscribers" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "user_id" bigInt NOT NULL,
+                "custom_email" character varying(256) NOT NULL DEFAULT '',
+                "process_id" bigInt NOT NULL,
+                CONSTRAINT "UQ_custom_email_user_process" UNIQUE ("custom_email", "user_id", "process_id"),
+                CONSTRAINT "PK_notification_process" PRIMARY KEY ("id")
+            )
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "process_summary_notification_subscribers"
+            ADD CONSTRAINT "FK_notification_user" FOREIGN KEY ("user_id") REFERENCES "jhi_user"("id") ON DELETE CASCADE
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "process_summary_notification_subscribers"
+            ADD CONSTRAINT "FK_notification_process" FOREIGN KEY ("process_id") REFERENCES "process"("id") ON DELETE CASCADE
+        `);
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('DROP TABLE "process_summary_notification_subscribers"');
+    }
+
+}
---
+++ runbotics/runbotics-scheduler/src/scheduler-database/process_summary_notification_subscribers/process-summary-notification-subscribers.entity.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+import { ProcessEntity } from '#/scheduler-database/process/process.entity';
+import { User } from '#/scheduler-database/user/user.entity';
+
+
+@Entity({ name: 'process_summary_notification_subscribers' })
+@Unique(['customEmail', 'user', 'process'])
+export class ProcessSummaryNotificationSubscribersEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @Column({ type: 'varchar', nullable: false, default: '', length: 256, name: 'custom_email' })
+    customEmail: string;
+
+    @ManyToOne(() => ProcessEntity, { nullable: false, onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'process_id' })
+    process: ProcessEntity;
+}
