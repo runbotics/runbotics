e9367d76bf11f0cbafc8c58739138d0ee0c1d3ea
jankowiakb <blazej.jankowiak@all-for-one.com>
2025-07-16 12:49:50 +0200

    [RPA-2317] CR fix

--- runbotics/runbotics-scheduler/src/scheduler-database/process_summary_notification_subscribers/dto/subscribe.dto.ts
+++ runbotics/runbotics-scheduler/src/scheduler-database/process_summary_notification_subscribers/dto/subscribe.dto.ts
@@ -1,11 +1,14 @@
 import { z } from 'zod';
+import { createZodDto } from 'nestjs-zod';
 
 export const subscribeProcessNotificationsDto = z.object({
     user_id: z.number(),
-    customEmail: z.string().email().optional(),
+    customEmail: z.string().trim().email().optional(),
     process_id: z.number(),
 });
 
+
+export class SubscribeSwaggerDto extends createZodDto(subscribeProcessNotificationsDto){}
 export type SubscribeDto = z.infer<
     typeof subscribeProcessNotificationsDto
 >;
--- runbotics/runbotics-scheduler/src/scheduler-database/process_summary_notification_subscribers/dto/update-subscription.dto.ts
+++ runbotics/runbotics-scheduler/src/scheduler-database/process_summary_notification_subscribers/dto/update-subscription.dto.ts
@@ -1,8 +1,12 @@
 import { z } from 'zod';
+import { createZodDto } from 'nestjs-zod';
 
 export const updatesubscriptionProcessNotificationsDto = z.object({
-    customEmail: z.string().email().nullable(),
+    customEmail: z.string().trim().email().nullable(),
 });
+
+export class UpdateSubscriptionSwaggerDto extends createZodDto(updatesubscriptionProcessNotificationsDto) {
+}
 
 export type UpdateSubscriptionDto = z.infer<
     typeof updatesubscriptionProcessNotificationsDto
