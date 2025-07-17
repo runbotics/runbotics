499ea94cd05ae23eaccc76e28b7e6aa9bfe08e50
jankowiakb <blazej.jankowiak@all-for-one.com>
2025-07-17 14:53:51 +0200

    [RPA-2308] change test to use spyOn

--- runbotics/runbotics-orchestrator-ui/src/src-app/components/Label/Label.test.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/components/Label/Label.test.tsx
@@ -1,103 +1,103 @@
 import { screen } from '@testing-library/react';
 import * as RunboticsCommon from 'runbotics-common';
 import '@testing-library/jest-dom';
-import { describe, expect, it, vi } from 'vitest';
+import { ProcessInstanceStatus } from 'runbotics-common';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { renderWithTestTheme } from 'src/test-utils/renderWithTestTheme';
 
 import Label from './Label';
 
-vi.mock('runbotics-common', async () => {
-    const actual = await vi.importActual<typeof import('runbotics-common')>(
-        'runbotics-common'
-    );
-    return {
-        ...actual,
-        isProcessInstanceFinished: vi.fn(),
-        ProcessInstanceStatus: {
-            FINISHED: 'FINISHED',
-            ERROR: 'ERROR',
-            RUNNING: 'RUNNING',
-        },
-    };
-});
+describe('Label component – null check functions', () => {
+    //@ts-expect-error vi namespace is not recognized
+    let isProcessInstanceFinishedSpy: vi.Mock;
 
-describe('<Label />', () => {
-    const { isProcessInstanceFinished } = RunboticsCommon;
+    beforeEach(() => {
+        vi.restoreAllMocks(); // Resetuje wszystkie mocki przed każdym testem
+        isProcessInstanceFinishedSpy = vi.spyOn(
+            RunboticsCommon,
+            'isProcessInstanceFinished'
+        );
+    });
 
     it('renders children correctly', () => {
-        renderWithTestTheme(<Label>FINISHED</Label>);
-        expect(screen.queryByText('FINISHED')).not;
+        renderWithTestTheme(<Label>{ProcessInstanceStatus.COMPLETED}</Label>);
+        expect(screen.queryByText(ProcessInstanceStatus.COMPLETED)).not.toBeNull();
     });
 
     it('does not render warning icon if warning is false', () => {
-        renderWithTestTheme(<Label warning={false}>FINISHED</Label>);
+        renderWithTestTheme(<Label warning={false}>{ProcessInstanceStatus.COMPLETED}</Label>);
         expect(screen.queryByTestId('WarningAmberRoundedIcon')).toBeNull();
     });
 
     it('renders warning icon if warning is true and process is finished', () => {
-        // @ts-expect-error: isProcessInstanceFinished is statically typed as a regular function, but we're mocking it in tests
-        isProcessInstanceFinished.mockReturnValue(true);
+        isProcessInstanceFinishedSpy.mockReturnValue(true);
 
-        renderWithTestTheme(<Label warning={true}>FINISHED</Label>);
+        renderWithTestTheme(
+            <Label warning={true}>{ProcessInstanceStatus.COMPLETED}</Label>
+        );
         expect(screen.queryByTestId('WarningAmberRoundedIcon')).not.toBeNull();
     });
 
     it('does not render warning icon if warning is true but status is not finished', () => {
-        // @ts-expect-error: isProcessInstanceFinished is statically typed as a regular function, but we're mocking it in tests
-        isProcessInstanceFinished.mockReturnValue(false);
+        isProcessInstanceFinishedSpy.mockReturnValue(false);
 
-        renderWithTestTheme(<Label warning={true}>RUNNING</Label>);
+        renderWithTestTheme(<Label warning={true}>{ProcessInstanceStatus.IN_PROGRESS}</Label>);
         expect(screen.queryByTestId('WarningAmberRoundedIcon')).toBeNull();
     });
 
     it('does not render warning icon if children is not a valid status', () => {
-        // @ts-expect-error: isProcessInstanceFinished is statically typed as a regular function, but we're mocking it in tests
-        isProcessInstanceFinished.mockReturnValue(true);
+        isProcessInstanceFinishedSpy.mockReturnValue(true);
 
         renderWithTestTheme(<Label warning={true}>INVALID</Label>);
         expect(screen.queryByTestId('WarningAmberRoundedIcon')).toBeNull();
     });
 });
 
-describe('<Label />', () => {
-    const { isProcessInstanceFinished } = RunboticsCommon;
+describe('Label component – jest-dom assertions', () => {
+    //@ts-expect-error vi namespace is not recognized
+    let isProcessInstanceFinishedSpy: vi.Mock;
+
+    beforeEach(() => {
+        vi.restoreAllMocks();
+        isProcessInstanceFinishedSpy = vi.spyOn(
+            RunboticsCommon,
+            'isProcessInstanceFinished'
+        );
+    });
 
     it('renders children correctly', () => {
-        renderWithTestTheme(<Label>FINISHED</Label>);
-        expect(screen.getByText('FINISHED')).toBeInTheDocument();
+        renderWithTestTheme(<Label>{ProcessInstanceStatus.COMPLETED}</Label>);
+        expect(screen.getByText(ProcessInstanceStatus.COMPLETED)).toBeInTheDocument();
     });
 
     it('does not render warning icon if warning is false', () => {
-        renderWithTestTheme(<Label warning={false}>FINISHED</Label>);
+        renderWithTestTheme(<Label warning={false}>{ProcessInstanceStatus.COMPLETED}</Label>);
         expect(
             screen.queryByTestId('WarningAmberRoundedIcon')
         ).not.toBeInTheDocument();
     });
 
     it('renders warning icon if warning is true and process is finished', () => {
-        // @ts-expect-error: isProcessInstanceFinished is statically typed as a regular function, but we're mocking it in tests
-        isProcessInstanceFinished.mockReturnValue(true);
+        isProcessInstanceFinishedSpy.mockReturnValue(true);
 
-        renderWithTestTheme(<Label warning={true}>FINISHED</Label>);
+        renderWithTestTheme(<Label warning={true}>{ProcessInstanceStatus.COMPLETED}</Label>);
         expect(
             screen.getByTestId('WarningAmberRoundedIcon')
         ).toBeInTheDocument();
     });
 
     it('does not render warning icon if warning is true but status is not finished', () => {
-        // @ts-expect-error: isProcessInstanceFinished is statically typed as a regular function, but we're mocking it in tests
-        isProcessInstanceFinished.mockReturnValue(false);
+        isProcessInstanceFinishedSpy.mockReturnValue(false);
 
-        renderWithTestTheme(<Label warning={true}>RUNNING</Label>);
+        renderWithTestTheme(<Label warning={true}>{ProcessInstanceStatus.IN_PROGRESS}</Label>);
         expect(
             screen.queryByTestId('WarningAmberRoundedIcon')
         ).not.toBeInTheDocument();
     });
 
     it('does not render warning icon if children is not a valid status', () => {
-        // @ts-expect-error: isProcessInstanceFinished is statically typed as a regular function, but we're mocking it in tests
-        isProcessInstanceFinished.mockReturnValue(true);
+        isProcessInstanceFinishedSpy.mockReturnValue(true);
 
         renderWithTestTheme(<Label warning={true}>INVALID</Label>);
         expect(
