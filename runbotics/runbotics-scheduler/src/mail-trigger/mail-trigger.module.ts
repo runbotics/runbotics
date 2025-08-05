72b260288a3e1515f498a5fd48a56906389e46d8
igorspychalaafo <210660167+igorspychalaafo@users.noreply.github.com>
2025-08-05 10:49:23 +0200

    refactor(runbotics-scheduler): :art: move schedule module to main module

--- runbotics/runbotics-scheduler/src/mail-trigger/mail-trigger.module.ts
+++ runbotics/runbotics-scheduler/src/mail-trigger/mail-trigger.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from '#/database/database.module';
 import { QueueModule } from '#/queue/queue.module';
 import { MailTriggerService } from './mail-trigger.service';
@@ -9,7 +8,6 @@ import { TenantModule } from '#/scheduler-database/tenant/tenant.module';
     imports: [
         DatabaseModule,
         QueueModule,
-        ScheduleModule.forRoot(),
         TenantModule,
     ],
     providers: [MailTriggerService],
