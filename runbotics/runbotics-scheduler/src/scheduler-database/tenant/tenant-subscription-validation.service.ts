b453d0c770ca1daa2bbb21b32919def0c247b1d6
igorspychalaafo <210660167+igorspychalaafo@users.noreply.github.com>
2025-08-20 13:10:29 +0200

    feat(runbotics-scheduler): :sparkles: mail notification about sub expiration

--- runbotics/runbotics-scheduler/src/scheduler-database/tenant/tenant-subscription-validation.service.ts
+++ runbotics/runbotics-scheduler/src/scheduler-database/tenant/tenant-subscription-validation.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { Tenant } from '#/scheduler-database/tenant/tenant.entity';
-import { DataSource } from 'typeorm';
+import { DataSource, EntityManager } from 'typeorm';
 import { Logger } from '#/utils/logger';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import dayjs from 'dayjs';
+import { hasRole } from '#/utils/authority.utils';
+import { Role } from 'runbotics-common';
+import { UserService } from '../user/user.service';
+import { MailService } from '#/mail/mail.service';
 
 @Injectable()
 export class TenantSubscriptionValidationService {
@@ -12,8 +17,9 @@ export class TenantSubscriptionValidationService {
     constructor(
         @InjectDataSource()
         private readonly dataSource: DataSource,
-    ) {
-    }
+        private readonly userService: UserService,
+        private readonly mailService: MailService,
+    ) {}
 
     @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
     async validateTenants() {
@@ -21,27 +27,53 @@ export class TenantSubscriptionValidationService {
             const tenants = await manager.find(Tenant);
 
             for (const tenant of tenants) {
-                if (tenant.subscriptionEnd) {
-                    const now = new Date();
-                    const endDate = new Date(tenant.subscriptionEnd);
+                if (!tenant.subscriptionEnd) continue;
 
-                    if (endDate < now) {
-                        this.logger.warn(`Tenant with subscription end date ${tenant.subscriptionEnd} is expired.`);
-                        const updatedTenant: Tenant = {
-                            ...tenant,
-                            active: false,
-                        };
-                        await this.sendNotification(tenant, 'Your subscription has expired. Please renew to continue using the service.');
-                        await manager.save(Tenant, updatedTenant);
-                    }
+                const now = dayjs();
+                const endDate = dayjs(tenant.subscriptionEnd).endOf('day');
+                const diffDays = endDate.diff(now, 'day');
+
+                if (tenant.active && ([30, 14, 7].includes(diffDays) || diffDays <= 0)) {
+                    await this.notifyUsersAboutExpiration(tenant, diffDays);
+                }
+
+                if (endDate.isBefore(now) && tenant.active) {
+                    await this.deactivateExpiredTenant(manager, tenant);
                 }
             }
-
         });
     }
-    
-    private async sendNotification(tenant: Tenant, message: string) {
-        // TODO: Implement your notification logic here, e.g., send an email or log a message
-        this.logger.log(`Notification for tenant ${tenant.id}: ${message}`);
+
+    private async notifyUsersAboutExpiration(tenant: Tenant, diffDays: number) {
+        const users = await this.userService.findAllByTenantId(tenant.id);
+
+        for (const user of users) {
+            let recipients: string[] = [];
+            let mailToLink = '';
+
+            const isTenantAdmin = hasRole(user, Role.ROLE_TENANT_ADMIN);
+            const isUser = hasRole(user, Role.ROLE_USER);
+
+            if (isUser && tenant) {
+                const tenantAdmins = await this.userService.findAllByTenantIdAndRole(tenant.id, Role.ROLE_TENANT_ADMIN);
+                recipients = tenantAdmins.map(admin => admin.email);
+                mailToLink = `mailto:${recipients.join(',')}`;
+            } else if (isTenantAdmin) {
+                const globalAdmins = await this.userService.findAllByRole(Role.ROLE_ADMIN);
+                recipients = globalAdmins.map(admin => admin.email);
+                mailToLink = `mailto:${recipients.join(',')}`;
+            }
+
+            await this.mailService.sendSubscriptionExpirationNotification(user, diffDays, mailToLink);
+        }
+    }
+
+    private async deactivateExpiredTenant(manager: EntityManager, tenant: Tenant) {
+        this.logger.warn(`Tenant with subscription end date ${tenant.subscriptionEnd} is expired.`);
+        const updatedTenant: Tenant = {
+            ...tenant,
+            active: false,
+        };
+        await manager.save(Tenant, updatedTenant);
     }
 }
