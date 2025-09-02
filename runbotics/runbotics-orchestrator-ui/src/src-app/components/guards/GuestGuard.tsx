f392d30f081d78d17eeb4bdca5a64c393d330152
fiflon <domanskibiznes@gmail.com>
2025-09-02 10:34:07 +0200

    feat(runbotics-orchestrator-ui): :sparkles: Add new ROLE_USER to runbotics-common and ui changes

--- runbotics/runbotics-orchestrator-ui/src/src-app/components/guards/GuestGuard.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/components/guards/GuestGuard.tsx
@@ -25,24 +25,38 @@ export const withGuestGuard = (Component: FC | VFC) => (props: any) => {
         switchLanguage(router.locale as Language);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [router.locale]);
-
+    
     if (isBrowser && isInitialized && isAuthenticated) {
+        const isOnlyUser = user.roles.every((role) => role === Role.ROLE_USER);
         if (user.roles.includes(Role.ROLE_ADMIN)) {
             router.replace('/app/tenants', null, { locale: user.langKey });
         }
 
-        if (![Role.ROLE_GUEST, Role.ROLE_ADMIN].some(role => user.roles.includes(role))) {
-            router.replace('/app/processes/collections', null, { locale: user.langKey });
+        if (isOnlyUser) {
+            router.replace('/app/assistant', null, { locale: user.langKey });
         }
 
-        if (user.roles.includes(Role.ROLE_GUEST) && router.query.guest !== 'true') {
-            dispatch(authActions.logout())
-                .then(() => {
-                    redirectToWebsiteRoot(user.langKey);
-                });
+        if (
+            !isOnlyUser &&
+            ![Role.ROLE_GUEST, Role.ROLE_ADMIN].some((role) =>
+                user.roles.includes(role)
+            )
+        ) {
+            router.replace('/app/processes/collections', null, {
+                locale: user.langKey,
+            });
+        }
+
+        if (
+            user.roles.includes(Role.ROLE_GUEST) &&
+            router.query.guest !== 'true'
+        ) {
+            dispatch(authActions.logout()).then(() => {
+                router.replace('/', null, { locale: user.langKey });
+            });
         }
     }
-
+    
     if (isBrowser && isInitialized && !isAuthenticated) return <Component {...props} />;
 
     return <BlankPage />;
