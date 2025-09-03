3097c6e39389c86db7c1def376f7528e7334657d
fiflon <domanskibiznes@gmail.com>
2025-09-03 12:59:02 +0200

    refactor(runbotics-orchestrator-ui): :recycle: Refactor code after cr

--- runbotics/runbotics-orchestrator-ui/src/src-app/components/guards/GuestGuard.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/components/guards/GuestGuard.tsx
@@ -2,7 +2,6 @@ import { FC, VFC, useEffect } from 'react';
 
 import { useRouter } from 'next/router';
 
-import { Role } from 'runbotics-common';
 
 import useAuth from '#src-app/hooks/useAuth';
 import useTranslations from '#src-app/hooks/useTranslations';
@@ -12,10 +11,12 @@ import { Language } from '#src-app/translations/translations';
 import BlankPage from '#src-app/utils/BlankPage';
 import { redirectToWebsiteRoot } from '#src-app/utils/navigation';
 
+import { hasRoles } from '../utils/Secured';
 
 // eslint-disable-next-line react/display-name
 export const withGuestGuard = (Component: FC | VFC) => (props: any) => {
     const { isAuthenticated, isInitialized, user } = useAuth();
+    const { isAdmin, isGuest, isOnlyRoleUser } = hasRoles(user);
     const router = useRouter();
     const dispatch = useDispatch();
     const isBrowser = typeof window !== 'undefined';
@@ -25,35 +26,27 @@ export const withGuestGuard = (Component: FC | VFC) => (props: any) => {
         switchLanguage(router.locale as Language);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [router.locale]);
-    
+
     if (isBrowser && isInitialized && isAuthenticated) {
-        const isJustUser = user.roles.every((role) => role === Role.ROLE_USER);
-        if (user.roles.includes(Role.ROLE_ADMIN)) {
+        if (isAdmin) {
             router.replace('/app/tenants', null, { locale: user.langKey });
         }
 
-        if (isJustUser) {
+        if (isOnlyRoleUser) {
             router.replace('/app/assistant', null, { locale: user.langKey });
-        } else if (
-            ![Role.ROLE_GUEST, Role.ROLE_ADMIN].some((role) =>
-                user.roles.includes(role)
-            )
-        ) {
+        } else if (!isGuest && !isAdmin) {
             router.replace('/app/processes/collections', null, {
                 locale: user.langKey,
             });
         }
-        
-        if (
-            user.roles.includes(Role.ROLE_GUEST) &&
-            router.query.guest !== 'true'
-        ) {
+
+        if (isGuest && router.query.guest !== 'true') {
             dispatch(authActions.logout()).then(() => {
                 redirectToWebsiteRoot(user.langKey);
             });
         }
     }
-    
+
     if (isBrowser && isInitialized && !isAuthenticated) return <Component {...props} />;
 
     return <BlankPage />;
