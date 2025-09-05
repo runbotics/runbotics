238c4ece0e9b40c1de4899059e6776028c675d94
fiflon <domanskibiznes@gmail.com>
2025-09-05 14:58:00 +0200

    fix(runbotics-test): :bug: Fix endless waiting for processes category

--- runbotics-test/pages/processesCollectionsPage.ts
+++ runbotics-test/pages/processesCollectionsPage.ts
@@ -5,17 +5,27 @@ import { ProcessesPage } from "./processesPage";
 export class ProcessesCollectionsPage extends AppPage {
     readonly processesNavButton: Locator;
     readonly collectionsNavButton: Locator;
+    readonly processesNavButtonTestId = "procesTabs-tab-processes";
+    readonly collectionsNavButtonTestId = "procesTabs-tab-collections";
 
     constructor(page: Page) {
         super(page);
-        this.processesNavButton = page.getByTestId("procesTabs-tab-processes");
+        this.processesNavButton = page.getByTestId(
+            this.processesNavButtonTestId
+        );
         this.collectionsNavButton = page.getByTestId(
-            "procesTabs-tab-collections"
+            this.collectionsNavButtonTestId
         );
     }
 
     async goToProcessesPage() {
         await this.processesNavButton.click();
+
+        await this.page.waitForSelector(
+            `[data-testid=${this.processesNavButtonTestId}][aria-selected="true"]`
+        );
+
+        await this.page.waitForLoadState("load");
         return new ProcessesPage(this.page);
     }
 }
