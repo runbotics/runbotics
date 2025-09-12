ce5a9a7198eb45a784f41f8df19144c6c949f223
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2025-09-12 12:09:01 +0200

    feat: :sparkles: Changes user password policy

--- runbotics-orchestrator/src/main/java/com/runbotics/web/rest/vm/ManagedUserVM.java
+++ runbotics-orchestrator/src/main/java/com/runbotics/web/rest/vm/ManagedUserVM.java
@@ -1,20 +1,29 @@
 package com.runbotics.web.rest.vm;
 
 import com.runbotics.service.dto.AdminUserDTO;
-
-import javax.validation.constraints.Size;
 import java.util.UUID;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 /**
  * View Model extending the AdminUserDTO, which is meant to be used in the user management UI.
  */
 public class ManagedUserVM extends AdminUserDTO {
 
-    public static final int PASSWORD_MIN_LENGTH = 4;
-
+    public static final int PASSWORD_MIN_LENGTH = 14;
     public static final int PASSWORD_MAX_LENGTH = 100;
 
-    @Size(min = PASSWORD_MIN_LENGTH, max = PASSWORD_MAX_LENGTH)
+    private static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9])$";
+
+    @Size(
+        min = PASSWORD_MIN_LENGTH,
+        max = PASSWORD_MAX_LENGTH,
+        message = "Incorrect password. Must include at least 14 characters upper and lower cased, special character and digit"
+    )
+    @Pattern(
+        regexp = PASSWORD_REGEX,
+        message = "Incorrect password. Must include at least 14 characters upper and lower cased, special character and digit"
+    )
     private String password;
 
     private UUID inviteCode;
