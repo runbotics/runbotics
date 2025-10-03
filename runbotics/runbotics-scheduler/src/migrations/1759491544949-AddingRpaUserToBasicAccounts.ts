32c64785f30500a4f3024856a22d1c5e125abad9
fiflon <domanskibiznes@gmail.com>
2025-10-03 15:12:36 +0200

    feat(runbotics-scheduler): :sparkles: Add new basic RpaUser migration

---
+++ runbotics/runbotics-scheduler/src/migrations/1759491544949-AddingRpaUserToBasicAccounts.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddingRpaUserToBasicAccounts1759491544949
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+                INSERT INTO "jhi_user" (
+                    id,
+                    email,
+                    password_hash,
+                    first_name,
+                    last_name,
+                    activated,
+                    lang_key,
+                    tenant_id,
+                    created_by,
+                    created_date,
+                    last_modified_by,
+                    last_modified_date
+                ) VALUES (
+                    5,
+                    'rpa-user@localhost',
+                    '$2a$10$0vnKkp5IFbeEG3s.8GEzv.jZwq7hzHbfj3cFFNmHbdqwIc/ai2tn6',
+                    'RpaUser',
+                    'RpaUser',
+                    true,
+                    'en',
+                    'b7f9092f-5973-c781-08db-4d6e48f78e98',
+                    'system',
+                    NOW(),
+                    'system',
+                    NOW()
+                )
+            `);
+        await queryRunner.query(`
+                INSERT INTO "jhi_user_authority" (user_id, authority_name) VALUES (5, 'ROLE_RPA_USER')
+            `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+                DELETE FROM "jhi_user_authority" WHERE user_id = 5`);
+        await queryRunner.query('DELETE FROM "jhi_user" WHERE id = 5');
+    }
+}
