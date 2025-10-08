ccd9731ffec53797f36bd396e97543f111b4782e
Igor Spychała <62942349+sniperdev@users.noreply.github.com>
2025-10-08 11:53:43 +0200

    [RPA-2462] Add timeout to fetchAIAssistants API request (#1162)
    * fix(runbotics-orchestrator-ui): :bug: Add timeout to fetchAIAssistants API request
    
    * chore: :pushpin: bump app version to 3.5.0-SNAPSHOT.40
    
    ---------
    
    Co-authored-by: igorspychalaafo <210660167+igorspychalaafo@users.noreply.github.com>

--- runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/AIAssistants/AIAssistants.thunks.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/AIAssistants/AIAssistants.thunks.ts
@@ -6,7 +6,7 @@ import { AIAssistant, AIAssistantsResponse } from '#src-app/views/AIAssistantVie
 export const fetchAIAssistants = createAsyncThunk(
     'aiAssistants/fetchAll',
     async (): Promise<AIAssistant[]> => {
-        const response = await axios.get<AIAssistantsResponse>('/assistant/api/assistants');
+        const response = await axios.get<AIAssistantsResponse>('/assistant/api/assistants', {timeout: 10000});
         return response.data.data;
     }
 );
