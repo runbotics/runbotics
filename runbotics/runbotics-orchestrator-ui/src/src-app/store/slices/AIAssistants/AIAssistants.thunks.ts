62fa98af44e2b8edf0e8a105e5f1e4cee3ceec8d
fiflon <domanskibiznes@gmail.com>
2025-10-10 12:53:50 +0200

    feat(runbotics-orchestrator-ui): :sparkles: Add additional validation and Yup schemas

--- runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/AIAssistants/AIAssistants.thunks.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/AIAssistants/AIAssistants.thunks.ts
@@ -1,12 +1,36 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import axios from '#src-app/utils/axios';
-import { AIAssistant, AIAssistantsResponse } from '#src-app/views/AIAssistantView/types';
+import {
+    AIAssistant,
+    AIAssistantSchema,
+    AIAssistantsResponse,
+} from '#src-app/views/AIAssistantView/types';
 
 export const fetchAIAssistants = createAsyncThunk(
     'aiAssistants/fetchAll',
     async (): Promise<AIAssistant[]> => {
-        const response = await axios.get<AIAssistantsResponse>('/assistant/api/assistants', {timeout: 10000});
-        return response.data.data;
+        const response = await axios.get<AIAssistantsResponse>(
+            '/assistant/api/assistants',
+            { timeout: 10000 }
+        );
+        const unvalidatedData = response.data.data;
+        const validationPromises = unvalidatedData.map(
+            async (aiAssistant: any) => {
+                try {
+                    return await AIAssistantSchema.validate(aiAssistant, {
+                        abortEarly: false,
+                    });
+                } catch (error) {
+                    return null;
+                }
+            }
+        );
+
+        const validatedAssistants = (
+            await Promise.all(validationPromises)
+        ).filter(Boolean) as AIAssistant[];
+
+        return validatedAssistants;
     }
 );
