376ac4a7412070f49fa63c9de19af9fc9a78b73e
fiflon <domanskibiznes@gmail.com>
2025-10-14 13:18:55 +0200

    fix(runbotics-orchestrator-ui): :pencil2: Fix typo in translation

--- runbotics/runbotics-lp/src/src-app/translations/pl/scheduler/dialog.ts
+++ runbotics/runbotics-lp/src/src-app/translations/pl/scheduler/dialog.ts
@@ -2,6 +2,6 @@ import englishSchedulerDialogTranslations from '#src-app/translations/en/schedul
 
 const schedulerDialogTranslations: typeof englishSchedulerDialogTranslations = {
     'Scheduler.Dialog.NoServerToConnection': 'Aplikacja nie może nawiązać połączenia z serwerem',
-    'Scheduler.Dialog.ConnectionRestored': 'Połączenie z serwerem odzyskane. Jeżeli coś poszło nie tak odśwież karte.',
+    'Scheduler.Dialog.ConnectionRestored': 'Połączenie z serwerem odzyskane. Jeżeli coś poszło nie tak odśwież kartę.',
 };
 export default schedulerDialogTranslations;
