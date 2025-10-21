7f09ff7992716e9542258e2083e5aaf4d2af3813
jankowiakb <blazej.jankowiak@all-for-one.com>
2025-10-21 12:34:20 +0200

    [RPA-2503] eslint -build ui fix

--- runbotics/runbotics-orchestrator-ui/src/src-app/views/webhooks/WebhooksView/WebhooksView.tsx
+++ runbotics/runbotics-orchestrator-ui/src/src-app/views/webhooks/WebhooksView/WebhooksView.tsx
@@ -2,20 +2,20 @@ import React, { FC } from 'react';
 
 import InternalPage from '#src-app/components/pages/InternalPage';
 import useTranslations from '#src-app/hooks/useTranslations';
+import { useDispatch, useSelector } from '#src-app/store';
+import { webhookActions } from '#src-app/store/slices/Webhook';
+import WebhookDialog from '#src-app/views/webhooks/WebhooksView/WebhookDialog';
 import WebhookHeader from '#src-app/views/webhooks/WebhooksView/WebhookHeader';
 import WebhookList from '#src-app/views/webhooks/WebhooksView/WebhookList';
-import { useDispatch, useSelector } from '#src-app/store';
-import WebhookDialog from '#src-app/views/webhooks/WebhooksView/WebhookDialog';
-import { webhookActions } from '#src-app/store/slices/Webhook';
 
 const WebhooksView: FC = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const { translate } = useTranslations();
     const isModalOpen = useSelector((state) => state.webhook.isModalOpen); 
     
     const onModalClose = () => {
         dispatch(webhookActions.setIsModalOpen(false));
-    }
+    };
     return (
         <>
             <InternalPage
