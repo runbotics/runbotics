2c2fd7f46aeb1ba4fee93f8f27e39f2dec1f1698
teawithsand <teawithsand@gmail.com>
2025-11-13 09:03:34 +0100

    refactor(runbotics-scheduler): :recycle: Refactored logic to use more minimal and concise token validation for each bot message

--- runbotics/runbotics-scheduler/src/auth/guards/ws-bot-jwt.guard.ts
+++ runbotics/runbotics-scheduler/src/auth/guards/ws-bot-jwt.guard.ts
@@ -1,9 +1,12 @@
 import { BotAuthSocket } from '#/types/auth-socket';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { TaskQueue } from '#/utils/task-queue';
 
 @Injectable()
 export class WsBotJwtGuard implements CanActivate {
+    private static taskQueue = new TaskQueue(4);
+
     constructor(private authService: AuthService) {}
 
     async canActivate(context: ExecutionContext) {
@@ -18,6 +21,14 @@ export class WsBotJwtGuard implements CanActivate {
             return false;
         }
 
-        return true;
+        return WsBotJwtGuard.taskQueue.enqueue(async () => {
+            const user = await this.authService.validateToken(token);
+            
+            if (!user || !user.tenant?.active) {
+                return false;
+            }
+
+            return true;
+        });
     }
 }
