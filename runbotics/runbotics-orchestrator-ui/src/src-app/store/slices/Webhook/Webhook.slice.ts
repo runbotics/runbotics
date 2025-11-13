1312584f52e1993ccca540ae2ebaca6f7bc8cc32
Barbara Darowska <61866178+bhszewczyk@users.noreply.github.com>
2025-11-13 15:23:26 +0100

    feat(runbotics-orchestrator-ui): :sparkles: Updates store for delete webhook functonality

--- runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/Webhook/Webhook.slice.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/Webhook/Webhook.slice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { CreateClientRegistrationWebhookRequest } from 'runbotics-common';
+import { CreateClientRegistrationWebhookRequest, UnregisterClientWebhookRequest } from 'runbotics-common';
 
 import { RootState } from '#src-app/store';
 import buildWebhooksExtraReducers from '#src-app/store/slices/Webhook/Webhook.extraReducers';
@@ -16,7 +16,10 @@ const initialState: WebhookState = {
     triggers: null,
     tokenExpirationDate: null,
     registerWebhook: null,
+    unregisterWebhook: null,
     isModalOpen: false,
+    isUnregisterModalOpen: false,
+    modalWebhookId: null
 };
 
 export const slice = createSlice({
@@ -29,9 +32,18 @@ export const slice = createSlice({
         setIsModalOpen(state, action:PayloadAction<boolean>) {
             state.isModalOpen = action.payload;
         },
+        setIsUnregisterModalOpen(state, action:PayloadAction<boolean>) {
+            state.isUnregisterModalOpen = action.payload;
+        },
         setRegistrationForm(state, action:PayloadAction<CreateClientRegistrationWebhookRequest>) {
             state.registerWebhook = action.payload;
-        }
+        },
+        setUnregistrationForm(state, action:PayloadAction<UnregisterClientWebhookRequest>) {
+            state.unregisterWebhook = action.payload;
+        },
+        setModalWebhookId(state, action:PayloadAction<string>) {
+            state.modalWebhookId = action.payload;
+        },
     },
     extraReducers: builder => buildWebhooksExtraReducers(builder),
 });
--- runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/Webhook/Webhook.thunks.ts
+++ runbotics/runbotics-orchestrator-ui/src/src-app/store/slices/Webhook/Webhook.thunks.ts
@@ -1,7 +1,9 @@
 
-import { ClientRegistrationWebhook, CreateClientRegistrationWebhookRequest } from 'runbotics-common';
+import { ClientRegistrationWebhook, CreateClientRegistrationWebhookRequest, UnregisterClientWebhookRequest } from 'runbotics-common';
 
 import ApiTenantResource from '#src-app/utils/ApiTenantResource';
+
+type UnregisterWebhookBody = { data?: UnregisterClientWebhookRequest }
 
 export const createWebhookEntry = ApiTenantResource.post<ClientRegistrationWebhook, CreateClientRegistrationWebhookRequest>(
     'webhooks/createWebhookEntry',
@@ -16,4 +18,9 @@ export const getWebhooks = ApiTenantResource.get<ClientRegistrationWebhook[]>(
 export const getServiceToken = ApiTenantResource.get<{token: string, serviceTokenExpDate: string }>(
     'webhooks/getServiceToken',
     '/get-token'
+);
+
+export const deleteWebhookEntry = ApiTenantResource.delete<void, UnregisterWebhookBody>(
+    'webhooks/deleteWebhookEntry',
+    (id: string) => `webhook/${id}`
 );
